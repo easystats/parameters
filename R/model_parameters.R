6651b79282b5ac16425de83ff7c1a0a966c0ba92
Dominique Makowski <dom.mak19@gmail.com>
2019-02-20 10:20:00 +0800

    htest support

--- R/model_parameters.R
+++ R/model_parameters.R
@@ -2,6 +2,7 @@
 #'
 #' Compute and extract model parameters. See the documentation for your object's class:
 #' \itemize{
+#'  \item{\link[=model_parameters.htest]{Correlations and t-tests}}
 #'  \item{\link[=model_parameters.lm]{Frequentist regressions}}
 #'  \item{\link[=model_parameters.stanreg]{Bayesian regressions}}
 #'  }
---
+++ R/model_parameters.htest.R
@@ -0,0 +1,113 @@
+#' h-test Parameters
+#'
+#' Parameters of h-tests.
+#'
+#' @param model Object of class htest.
+#' @param bootstrap Should estimates be bootsrapped?
+#' @param ... Arguments passed to or from other methods (e.g., to \link[=standardize.lm]{standardize}).
+#'
+#' @examples
+#' model <- cor.test(mtcars$mpg, mtcars$cyl, method="pearson")
+#' model_parameters(model)
+#'
+#' model <- t.test(iris$Sepal.Width, iris$Sepal.Length)
+#' model <- t.test(mtcars$mpg ~ mtcars$vs)
+#' model <- t.test(iris$Sepal.Width, mu = 1)
+#' model_parameters(model)
+#' @export
+model_parameters.htest <- function(model, bootstrap = FALSE, ...) {
+
+  if(bootstrap){
+    stop("Bootstrapped h-tests are not yet implemented.")
+  } else{
+    return(.extract_parameters_htest(model))
+  }
+}
+
+
+
+#' @keywords internal
+.extract_parameters_htest <- function(model) {
+  if(insight::model_info(model)$is_correlation){
+    names <- unlist(strsplit(model$data.name, " and "))
+    out <- data.frame(
+      "Parameter1" = names[1],
+      "Parameter2" = names[2]
+    )
+
+    if (grepl("Pearson", model$method)) {
+      out$r <- model$estimate
+      out$t <- model$statistic
+      out$DoF = model$parameter
+      out$p <- model$p.value
+      out$CI_low <- model$conf.int[1]
+      out$CI_high <- model$conf.int[2]
+      out$CI_level <- attributes(model$conf.int)$conf.level
+      out$Method <- "Pearson"
+    } else if(grepl("Spearman", model$method)){
+      out$rho <- model$estimate
+      out$S <- model$statistic
+      out$DoF = model$parameter
+      out$p <- model$p.value
+      out$Method <- "Spearman"
+    } else {
+      out$tau <- model$estimate
+      out$z <- model$statistic
+      out$DoF = model$parameter
+      out$p <- model$p.value
+      out$Method <- "Kendall"
+    }
+  } else if(insight::model_info(model)$is_ttest){
+    if(grepl(" and ", model$data.name)){
+      names <- unlist(strsplit(model$data.name, " and "))
+      out <- data.frame(
+        "Parameter1" = names[1],
+        "Parameter2" = names[2],
+        "Mean_Parameter1" = model$estimate[1],
+        "Mean_Parameter2" = model$estimate[2],
+        "Difference" = model$estimate[2] - model$estimate[1],
+        "t" = model$statistic,
+        "DoF" = model$parameter,
+        "p" = model$p.value,
+        "CI_low" = model$conf.int[1],
+        "CI_high" = model$conf.int[2],
+        "CI_level" = attributes(model$conf.int)$conf.level,
+        "Method" = model$method
+      )
+    } else if(grepl(" by ", model$data.name)){
+      names <- unlist(strsplit(model$data.name, " by "))
+      out <- data.frame(
+        "Parameter" = names[1],
+        "Group" = names[2],
+        "Mean_Group1" = model$estimate[1],
+        "Mean_Group2" = model$estimate[2],
+        "Difference" = model$estimate[2] - model$estimate[1],
+        "t" = model$statistic,
+        "DoF" = model$parameter,
+        "p" = model$p.value,
+        "CI_low" = model$conf.int[1],
+        "CI_high" = model$conf.int[2],
+        "CI_level" = attributes(model$conf.int)$conf.level,
+        "Method" = model$method
+      )
+    } else {
+      out <- data.frame(
+        "Parameter" = model$data.name,
+        "Mean" = model$estimate,
+        "mu" = model$null.value,
+        "Difference" = model$estimate - model$null.value,
+        "t" = model$statistic,
+        "DoF" = model$parameter,
+        "p" = model$p.value,
+        "CI_low" = model$conf.int[1],
+        "CI_high" = model$conf.int[2],
+        "CI_level" = attributes(model$conf.int)$conf.level,
+        "Method" = model$method
+      )
+    }
+  } else{
+    stop("model_parameters not implemented for such h-tests yet.")
+  }
+
+  return(out)
+}
