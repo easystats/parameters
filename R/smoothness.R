cb6e6c217dc1471c01520ff37e11f13800de4a73
Dominique Makowski <dom.mak19@gmail.com>
2019-03-08 11:38:29 +0800

    added lag to smoothness

--- R/smoothness.R
+++ R/smoothness.R
@@ -2,18 +2,22 @@
 #'
 #' @param x Numeric vector (similar to a time series).
 #' @param method Can be "diff" (the standard deviation of the standardized differences) or "cor" (default, lag-one autocorrelation).
+#' @param lag An integer indicating which lag to use.
 #'
 #' @examples
-#' smoothness(exp(-10:10), method="cor")
-#' smoothness(exp(-10:10), method="diff")
+#' x <- (-10:10)^3+rnorm(21, 0, 100)
+#' plot(x)
+#' smoothness(x, method="cor")
+#' smoothness(x, method="diff")
 #'
 #' @references https://stats.stackexchange.com/questions/24607/how-to-measure-smoothness-of-a-time-series-in-r
 #' @importFrom stats cor
 #' @export
-smoothness <- function(x, method="cor"){
+smoothness <- function(x, method="cor", lag = 1){
   if(method == "cor"){
-    return(cor(x[-length(x)],x[-1]))
+    smooth <- cor(head(x, length(x)-lag), tail(x, length(x)-lag))
   } else{
-    return(sd(diff(x))/abs(mean(diff(x))))
+    smooth <- sd(diff(x, lag=lag))/abs(mean(diff(x, lag=lag)))
   }
+  return(smooth)
 }
