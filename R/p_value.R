79fce23e62c9699fd80b25babf303bab43ed6a6e
Daniel Lüdecke <mail@danielluedecke.de>
2019-04-29 12:27:07 +0200

    p_value() for more objects

--- R/p_value.R
+++ R/p_value.R
@@ -13,6 +13,8 @@
 #' model <- lme4::lmer(Petal.Length ~ Sepal.Length + (1 | Species), data = iris)
 #' p_value(model)
 #' }
+#'
+#' @importFrom stats coef vcov pnorm
 #' @export
 p_value <- function(model, ...) {
   UseMethod("p_value")
@@ -81,3 +83,98 @@ p_value.lmerMod <- function(model, method = "wald", ...) {
 p_value.merMod <- function(model, ...) {
   p_value_wald(model, ...)
 }
+
+
+
+#' @export
+p_value.vglm <- function(model, ...) {
+  if (!requireNamespace("VGAM", quietly = TRUE))
+    stop("Package `VGAM` required.", call. = FALSE)
+
+  cs <- VGAM::summary(model)@coef3
+  p <- cs[, 4]
+  # se <- cs[, 2]
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+p_value.svyglm.nb <- function(model, ...) {
+  if (!isNamespaceLoaded("survey"))
+    requireNamespace("survey", quietly = TRUE)
+
+  est <- stats::coef(model)
+  se <- sqrt(diag(stats::vcov(model, stderr = "robust")))
+  p <- 2 * stats::pnorm(abs(est / se), lower.tail = FALSE)
+
+  names(p) <- gsub("\\beta\\.", "", names(p), fixed = FALSE)
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+p_value.svyglm <- function(model, ...) {
+  cs <- stats::coef(summary(model))
+  p <- cs[, 4]
+  # se <- cs[, 2]
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+p_value.gmnl <- function(model, ...) {
+  cs <- summary(model)$CoefTable
+  p <- cs[, 4]
+  # se <- cs[, 2]
+
+  pv <- data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+
+  # rename intercepts
+  intercepts <- grepl(":(intercept)", pv$Parameter, fixed = TRUE)
+  pv$Parameter[intercepts] <- sprintf(
+    "(Intercept: %s)",
+    sub(":(intercept)", replacement = "", pv$Parameter[intercepts], fixed = TRUE)
+  )
+
+  pv
+}
+
+
+#' @export
+p_value.polr <- function(model, ...) {
+  smry <- suppressMessages(as.data.frame(stats::coef(summary(model))))
+  tstat <- smry[[3]]
+  # se <- smry[[2]]
+  p <- 2 * stats::pnorm(abs(tstat), lower.tail = FALSE)
+  names(p) <- rownames(smry)
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
