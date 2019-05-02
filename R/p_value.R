94c87b0f3a0f94a5d0fd8dcb36679c720afac24c
Daniel <mail@danielluedecke.de>
2019-05-02 21:02:20 +0200

    add p_value for lm,gam and standard_error()

--- R/p_value.R
+++ R/p_value.R
@@ -9,23 +9,13 @@
 #' @param ... Arguments passed to or from other methods.
 #'
 #' @examples
-#' \dontrun{
 #' model <- lme4::lmer(Petal.Length ~ Sepal.Length + (1 | Species), data = iris)
 #' p_value(model)
-#' }
 #'
 #' @importFrom stats coef vcov pnorm
 #' @export
 p_value <- function(model, ...) {
   UseMethod("p_value")
-}
-
-
-
-
-#' @export
-p_value.htest <- function(model, ...) {
-  model$p.value
 }
 
 
@@ -65,70 +55,11 @@ p_value.aovlist <- p_value.aov
 
 
 
-#' @rdname p_value
-#' @param method For mixed models, can be \link[=p_value_wald]{"wald"} (default) or \link[=p_value_kenward]{"kenward"}.
 #' @export
-p_value.lmerMod <- function(model, method = "wald", ...) {
-  method <- match.arg(method, c("wald", "kr", "kenward"))
-  if (method == "wald") {
-    p_value_wald(model, ...)
-  } else if (method == "kr" | method == "kenward") {
-    p_value_kenward(model, ...)
-  }
-}
-
-
-
-#' @export
-p_value.merMod <- function(model, ...) {
-  p_value_wald(model, ...)
-}
-
-
-
-#' @export
-p_value.vglm <- function(model, ...) {
-  if (!requireNamespace("VGAM", quietly = TRUE))
-    stop("Package `VGAM` required.", call. = FALSE)
-
-  cs <- VGAM::summary(model)@coef3
-  p <- cs[, 4]
-  # se <- cs[, 2]
-
-  data.frame(
-    Parameter = names(p),
-    p_value = as.vector(p),
-    stringsAsFactors = FALSE
-  )
-}
-
-
-
-#' @export
-p_value.svyglm.nb <- function(model, ...) {
-  if (!isNamespaceLoaded("survey"))
-    requireNamespace("survey", quietly = TRUE)
-
-  est <- stats::coef(model)
-  se <- sqrt(diag(stats::vcov(model, stderr = "robust")))
-  p <- 2 * stats::pnorm(abs(est / se), lower.tail = FALSE)
-
-  names(p) <- gsub("\\beta\\.", "", names(p), fixed = FALSE)
-
-  data.frame(
-    Parameter = names(p),
-    p_value = as.vector(p),
-    stringsAsFactors = FALSE
-  )
-}
-
-
-
-#' @export
-p_value.svyglm <- function(model, ...) {
-  cs <- stats::coef(summary(model))
-  p <- cs[, 4]
-  # se <- cs[, 2]
+p_value.gam <- function(fit, ...) {
+  sm <- summary(fit)
+  lc <- length(sm$p.coeff)
+  p <- sm$p.pv[1:lc]
 
   data.frame(
     Parameter = names(p),
@@ -162,6 +93,47 @@ p_value.gmnl <- function(model, ...) {
 }
 
 
+
+#' @export
+p_value.htest <- function(model, ...) {
+  model$p.value
+}
+
+
+
+#' @export
+p_value.lm <- function(model, ...) {
+  parms <- as.data.frame(stats::coef(summary(model)))
+  data.frame(
+    Parameter = rownames(parms),
+    p_value = as.vector(parms[, "Pr(>|t|)", drop = TRUE]),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @rdname p_value
+#' @param method For mixed models, can be \link[=p_value_wald]{"wald"} (default) or \link[=p_value_kenward]{"kenward"}.
+#' @export
+p_value.lmerMod <- function(model, method = "wald", ...) {
+  method <- match.arg(method, c("wald", "kr", "kenward"))
+  if (method == "wald") {
+    p_value_wald(model, ...)
+  } else if (method == "kr" | method == "kenward") {
+    p_value_kenward(model, ...)
+  }
+}
+
+
+
+#' @export
+p_value.merMod <- function(model, ...) {
+  p_value_wald(model, ...)
+}
+
+
+
 #' @export
 p_value.polr <- function(model, ...) {
   smry <- suppressMessages(as.data.frame(stats::coef(summary(model))))
@@ -178,3 +150,84 @@ p_value.polr <- function(model, ...) {
 }
 
 
+
+#' @export
+p_value.svyglm <- function(model, ...) {
+  cs <- stats::coef(summary(model))
+  p <- cs[, 4]
+  # se <- cs[, 2]
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+p_value.svyglm.nb <- function(model, ...) {
+  if (!isNamespaceLoaded("survey"))
+    requireNamespace("survey", quietly = TRUE)
+
+  est <- stats::coef(model)
+  se <- sqrt(diag(stats::vcov(model, stderr = "robust")))
+  p <- 2 * stats::pnorm(abs(est / se), lower.tail = FALSE)
+
+  names(p) <- gsub("\\beta\\.", "", names(p), fixed = FALSE)
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+p_value.vglm <- function(model, ...) {
+  if (!requireNamespace("VGAM", quietly = TRUE))
+    stop("Package `VGAM` required.", call. = FALSE)
+
+  cs <- VGAM::summary(model)@coef3
+  p <- cs[, 4]
+  # se <- cs[, 2]
+
+  data.frame(
+    Parameter = names(p),
+    p_value = as.vector(p),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @importFrom stats coef
+.get_pval_from_summary <- function(model) {
+  cs <- stats::coef(summary(model))
+  p <- NULL
+
+  if (ncol(cs) >= 4) {
+
+    # do we have a p-value column based on t?
+    pvcn <- which(colnames(cs) == "Pr(>|t|)")
+
+    # if not, do we have a p-value column based on z?
+    if (length(pvcn) == 0)
+      pvcn <- which(colnames(cs) == "Pr(>|z|)")
+
+    # if not, default to 4
+    if (length(pvcn) == 0) pvcn <- 4
+
+    p <- cs[, pvcn]
+
+    if (is.null(names(p))) {
+      coef_names <- rownames(cs)
+      if (length(coef_names) == length(p)) names(p) <- coef_names
+    }
+  }
+
+  p
+}
---
+++ R/standard_error.R
@@ -0,0 +1,174 @@
+#' Extract standard errors
+#'
+#' This function attempts to return standard errors of a model's parameters.
+#'
+#' @param model A model.
+#' @param ... Arguments passed to or from other methods.
+#'
+#' @examples
+#' model <- lme4::lmer(Petal.Length ~ Sepal.Length + (1 | Species), data = iris)
+#' standard_error(model)
+#'
+#' @importFrom stats coef vcov setNames
+#' @export
+standard_error <- function(model, ...) {
+  UseMethod("standard_error")
+}
+
+
+#' @rdname standard_error
+#' @export
+se <- standard_error
+
+
+#' @export
+standard_error.htest <- function(model, ...) {
+}
+
+
+#' @export
+standard_error.aov <- function(model, ...) {
+  params <- model_parameters(model)
+
+  data.frame(
+    Parameter = params$Parameter,
+    p_value = params$p,
+    stringsAsFactors = FALSE
+  )
+}
+
+
+#' @export
+standard_error.anova <- standard_error.aov
+
+#' @export
+standard_error.aovlist <- standard_error.aov
+
+
+#' @export
+standard_error.merMod <- function(model, ...) {
+  if (!requireNamespace("lme4", quietly = TRUE)) {
+    stop("Package `lme4` required for this function to work. Please install it.")
+  }
+
+  cc <- stats::coef(model)
+
+  # get names of intercepts
+  inames <- names(cc)
+
+  # variances of fixed effects
+  fixed.vars <- diag(as.matrix(stats::vcov(model)))
+
+  # extract variances of conditional modes
+  r1 <- lme4::ranef(model, condVar = TRUE)
+
+  # we may have multiple random intercepts, iterate all
+  se.merMod <- lapply(1:length(cc), function(i) {
+    cmode.vars <- t(apply(attr(r1[[i]], "postVar"), 3, diag))
+    seVals <- sqrt(sweep(cmode.vars, 2, fixed.vars[names(r1[[i]])], "+", check.margin = FALSE))
+
+    if (length(r1[[i]]) == 1) {
+      seVals <- as.data.frame(t(seVals))
+      stats::setNames(seVals, names(r1[[i]]))
+    } else {
+      seVals <- seVals[, 1:2]
+      stats::setNames(as.data.frame(seVals), names(r1[[i]]))
+    }
+  })
+
+  # set names of list
+  names(se.merMod) <- inames
+
+  se.merMod
+}
+
+
+
+#' @export
+standard_error.vglm <- function(model, ...) {
+  if (!requireNamespace("VGAM", quietly = TRUE))
+    stop("Package `VGAM` required.", call. = FALSE)
+
+  cs <- VGAM::summary(model)@coef3
+  se <- cs[, 2]
+
+  data.frame(
+    Parameter = names(se),
+    Std_Error = as.vector(se),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+standard_error.svyglm.nb <- function(model, ...) {
+  if (!isNamespaceLoaded("survey"))
+    requireNamespace("survey", quietly = TRUE)
+
+  se <- sqrt(diag(stats::vcov(model, stderr = "robust")))
+
+  data.frame(
+    Parameter = names(se),
+    Std_Error = as.vector(se),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+standard_error.svyglm <- function(model, ...) {
+  cs <- stats::coef(summary(model))
+  se <- cs[, 2]
+
+  data.frame(
+    Parameter = names(se),
+    Std_Error = as.vector(se),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+
+#' @export
+standard_error.gmnl <- function(model, ...) {
+  cs <- summary(model)$CoefTable
+  se <- cs[, 2]
+
+  pv <- data.frame(
+    Parameter = names(se),
+    Std_Error = as.vector(se),
+    stringsAsFactors = FALSE
+  )
+
+  # rename intercepts
+  intercepts <- grepl(":(intercept)", pv$Parameter, fixed = TRUE)
+  pv$Parameter[intercepts] <- sprintf(
+    "(Intercept: %s)",
+    sub(":(intercept)", replacement = "", pv$Parameter[intercepts], fixed = TRUE)
+  )
+
+  pv
+}
+
+
+#' @export
+standard_error.polr <- function(model, ...) {
+  smry <- suppressMessages(as.data.frame(stats::coef(summary(model))))
+  se <- smry[[2]]
+  names(se) <- rownames(smry)
+
+  data.frame(
+    Parameter = names(se),
+    p_value = as.vector(se),
+    stringsAsFactors = FALSE
+  )
+}
+
+
+#' @importFrom stats coef
+.get_se_from_summary <- function(model) {
+  cs <- stats::coef(summary(model))
+  cs[, 2]
+}
