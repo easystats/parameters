11de31df928617b94970c0f7accf549d01bf9728
Dominique Makowski <dom.mak19@gmail.com>
2019-05-23 16:17:20 +0800

    describe_distribution
    close #41

--- R/describe_distribution.R
+++ R/describe_distribution.R
@@ -1,44 +1,56 @@
-#' #' Classify the variable's distribution using machine learning
-#' #'
-#' #'
-#' #' This function uses an internal random forest model to classify the variable's distribution.
-#' #'
-#' #' @param x A numeric vector.
-#' #' @param probabilities Return a dataframe containing the probabilities of belonging to each distribution type.
-#' #'
-#' #' @examples
-#' #' find_distribution(rnorm(100))
-#' #' find_distribution(rpois(100, lambda = 4))
-#' #' find_distribution(runif(100))
-#' #' @importFrom bayestestR map_estimate
-#' #' @importFrom stats IQR density predict
-#' #' @export
-#' find_distribution <- function(x, probabilities = FALSE) {
-#'
-#'   if (!requireNamespace("randomForest", quietly = TRUE)) {
-#'     stop("Package `randomForest` required for distribution classification. Please install it.", call. = FALSE)
-#'   }
+#' Describe a Distribution
 #'
 #'
-#'   # Extract features
-#'   data <- data.frame(
-#'     "SD" = sd(x),
-#'     "MAD" = mad(x, constant=1),
-#'     "Mean_Median_Distance" = mean(x) - median(x),
-#'     "Mean_Mode_Distance" = mean(x) - as.numeric(bayestestR::map_estimate(x, bw = "nrd0")),
-#'     "SD_MAD_Distance" = sd(x) - mad(x, constant=1),
-#'     "Range" = diff(range(x)) / sd(x),
-#'     "IQR" = stats::IQR(x),
-#'     "Skewness" = skewness(x),
-#'     "Kurtosis" = kurtosis(x),
-#'     "Uniques" = length(unique(x)) / length(x)
-#'   )
+#' This function describes a distribution.
 #'
+#' @param x A numeric vector.
+#' @inheritParams bayestestR::point_estimate
 #'
-#'   # Predict
-#'   if (probabilities) {
-#'     return(as.data.frame(stats::predict(classify_distribution, data, type = "prob")))
-#'   } else {
-#'     return(as.character(stats::predict(classify_distribution, data)))
-#'   }
-#' }
+#' @examples
+#' describe_distribution(rnorm(100))
+#' describe_distribution(rpois(100, lambda = 4))
+#' describe_distribution(rbeta(100))
+#' @importFrom bayestestR map_estimate
+#' @importFrom stats IQR density predict
+#' @export
+describe_distribution <- function(x, estimate = "mean", dispersion = TRUE, range = TRUE, ...) {
+  UseMethod("describe_distribution")
+
+}
+
+
+#' @export
+describe_distribution.numeric <- function(x, estimate = "mean", dispersion = TRUE, range = TRUE, ...) {
+
+  # Missing
+  n_missing <- sum(is.na(x))
+  x <- na.omit(x)
+
+  # Distribution
+  type <- as.data.frame(t(find_distribution(x, probabilities = TRUE)))
+  type$Type <- row.names(type)
+
+  out <- data.frame(Type = type[which.max(type[, 1]), "Type"],
+                     Type_Confidence = type[which.max(type[, 1]), 1] * 100)
+
+  # Point estimates
+  out <- cbind(out,
+               bayestestR::point_estimate(x, estimate = estimate, dispersion = dispersion, ...))
+
+  # Range
+  if (range){
+    out <- cbind(out,
+                 data.frame(Min = min(x, na.rm = TRUE),
+                            Max = max(x, na.rm = TRUE)))
+  }
+
+  # Skewness
+  out <- cbind(out,
+               data.frame(Skewness = skewness(x),
+                          Kurtosis = kurtosis(x)))
+
+  out$n_Obs <- length(x)
+  out$n_Missing <- n_missing
+
+  out
+}
