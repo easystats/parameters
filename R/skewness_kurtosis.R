6205a44076a1407d1c73e288d4767669c135fb34
Daniel Lüdecke <mail@danielluedecke.de>
2019-06-03 11:11:54 +0200

    add details on interpretation

--- R/skewness_kurtosis.R
+++ R/skewness_kurtosis.R
@@ -3,6 +3,28 @@
 #' @param x A numeric vector or data.frame.
 #' @param na.rm Remove missing values.
 #'
+#' @details \strong{Skewness}
+#' \cr \cr
+#' Symmetric distributions have a \code{skewness} around zero, while
+#' a negative skewness values indicates a "left-skewed" distribution, and a
+#' positive skewness values indicates a "right-skewed" distribution. Examples
+#' for the relationship of skewness and distributions are:
+#' \itemize{
+#'   \item Normal distribution (and other symmetric distribution) has a skewness of 0
+#'   \item Half-normal distribution has a skewness just below 1
+#'   \item Exponential distribution has a skewness of 2
+#'   \item Lognormal distribution can have a skewness of any positive value, depending on its parameters
+#' }
+#' (\cite{https://en.wikipedia.org/wiki/Skewness})
+#' \cr \cr
+#' \strong{Kurtosis}
+#' \cr \cr
+#' The \code{kurtosis} is a measure of "tailedness" of a distribution. A distribution
+#' with a kurtosis valus of about zero is called "mesokurtic". A kurtosis value
+#' larger than zero indicates a "leptokurtic" distribution with \emph{fatter} tails.
+#' A kurtosis value below zero indicates a "platykurtic" distribution with \emph{thinner}
+#' tails (\cite{https://en.wikipedia.org/wiki/Kurtosis}).
+#'
 #' @examples
 #' skewness(rnorm(1000))
 #' kurtosis(rnorm(1000))
