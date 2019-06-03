66fabb6fb739140e029b157e3bb302dc0ccd6d08
Daniel Lüdecke <mail@danielluedecke.de>
2019-06-03 10:58:13 +0200

    use generics, fix bug in calculation of kurtosis

--- R/skewness_kurtosis.R
+++ R/skewness_kurtosis.R
@@ -1,46 +1,65 @@
 #' Compute Skewness and Kurtosis
 #'
-#'
 #' @param x A numeric vector or data.frame.
-#' @param na.rm Remove NaNs.
+#' @param na.rm Remove missing values.
 #'
 #' @examples
 #' skewness(rnorm(1000))
 #' kurtosis(rnorm(1000))
-#' @references All credits go to the \code{moments} package.
+#'
 #' @export
-skewness <- function(x, na.rm = TRUE) {
-  if (is.matrix(x)) {
-    apply(x, 2, skewness, na.rm = na.rm)
-  } else if (is.vector(x)) {
-    if (na.rm) x <- x[!is.na(x)]
-    n <- length(x)
-    (sum((x - mean(x))^3) / n) / (sum((x - mean(x))^2) / n)^(3 / 2)
-  }
-  else if (is.data.frame(x)) {
-    sapply(x, skewness, na.rm = na.rm)
-  } else {
-    skewness(as.vector(x), na.rm = na.rm)
-  }
+skewness <- function(x, ...) {
+  UseMethod("skewness")
 }
 
+#' @export
+skewness.numeric <- function(x, na.rm = TRUE, ...) {
+  if (na.rm) x <- x[!is.na(x)]
+  n <- length(x)
+  (sum((x - mean(x))^3) / n) / (sum((x - mean(x))^2) / n)^1.5
+}
 
+#' @export
+skewness.matrix <- function(x, na.rm = TRUE, ...) {
+  apply(x, 2, skewness, na.rm = na.rm)
+}
+
+#' @export
+skewness.data.frame <- function(x, na.rm = TRUE, ...) {
+  sapply(x, skewness, na.rm = na.rm)
+}
+
+#' @export
+skewness.default <- function(x, na.rm = TRUE, ...) {
+  skewness(as.vector(x), na.rm = na.rm)
+}
 
 
 
 #' @rdname skewness
 #' @export
-kurtosis <- function(x, na.rm = TRUE) {
-  if (is.matrix(x)) {
-    apply(x, 2, kurtosis, na.rm = na.rm)
-  } else if (is.vector(x)) {
-    if (na.rm) x <- x[!is.na(x)]
-    n <- length(x)
-    n * sum((x - mean(x))^4) / (sum((x - mean(x))^2)^2)
-  }
-  else if (is.data.frame(x)) {
-    sapply(x, kurtosis, na.rm = na.rm)
-  } else {
-    kurtosis(as.vector(x), na.rm = na.rm)
-  }
+kurtosis <- function(x, ...) {
+  UseMethod("kurtosis")
+}
+
+#' @export
+kurtosis.numeric <- function(x, na.rm = TRUE, ...) {
+  if (na.rm) x <- x[!is.na(x)]
+  n <- length(x)
+  n * sum((x - mean(x))^4) / (sum((x - mean(x))^2)^2) - 3
+}
+
+#' @export
+kurtosis.matrix <- function(x, na.rm = TRUE, ...) {
+  apply(x, 2, kurtosis, na.rm = na.rm)
+}
+
+#' @export
+kurtosis.data.frame <- function(x, na.rm = TRUE, ...) {
+  sapply(x, kurtosis, na.rm = na.rm)
+}
+
+#' @export
+kurtosis.default <- function(x, na.rm = TRUE, ...) {
+  kurtosis(as.vector(x), na.rm = na.rm)
 }
