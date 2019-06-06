122ee91448a7c7a8e3c36a18975a1c08692db9c6
Dominique Makowski <dom.mak19@gmail.com>
2019-06-06 10:53:36 +0800

    skip BF tests on travis?

--- tests/testthat/test-model_parameters.BFBayesFactor.R
+++ tests/testthat/test-model_parameters.BFBayesFactor.R
@@ -1,21 +1,29 @@
 context("model_parameters.BFBayesFactor")
 
 test_that("model_parameters.BFBayesFactor", {
+  testthat::skip_on_travis()
+
   library(BayesFactor)
   library(logspline)
 
   model <- BayesFactor::ttestBF(iris$Sepal.Width, iris$Petal.Length, paired = TRUE)
   testthat::expect_equal(parameters::model_parameters(model)$BF, 492.770, tolerance = 2)
-  model <- BayesFactor::ttestBF(formula = mpg ~ am, data = dplyr::mutate(mtcars, am = as.factor(am)))
-  testthat::expect_equal(parameters::model_parameters(model)$BF, 86, tolerance = 2)
+
   model <- BayesFactor::correlationBF(iris$Sepal.Width, iris$Petal.Length)
   testthat::expect_equal(parameters::model_parameters(model)$BF, 348853.6, tolerance = 10)
+
   model <- BayesFactor::anovaBF(Sepal.Length ~ Species, data = iris)
   testthat::expect_error(parameters::model_parameters(model))
+
+
 
   df <- mtcars
   df$gear <- as.factor(df$gear)
   df$am <- as.factor(df$am)
+
+  model <- BayesFactor::ttestBF(formula = mpg ~ am, data = df)
+  testthat::expect_equal(parameters::model_parameters(model)$BF, 86, tolerance = 2)
+
   model <- BayesFactor::anovaBF(mpg ~ gear * am, data = df)
   testthat::expect_error(parameters::model_parameters(model))
 })
