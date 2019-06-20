375c4465f2aa80dc4b604f9e0d3d38bd56005814
Dominique Makowski <dom.mak19@gmail.com>
2019-06-20 22:35:29 +0800

    n_parameters

--- R/n_parameters.R
+++ R/n_parameters.R
@@ -1,5 +1,10 @@
-# Count how many parameters in a model
-#
+#' Count how many parameters in a model
+#'
+#' Returns the number of parameters of a model.
+#'
+#' @param x A statistical model.
+#' @param ... Arguments passed to or from other methods.
+#'
 #' @export
 n_parameters <- function(x, ...) {
   length(insight::find_parameters(x, flatten = TRUE, ...))
