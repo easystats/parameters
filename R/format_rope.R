ad97d5ef9d1efa3121c981d838f7697341d661a6
Dominique Makowski <dom.mak19@gmail.com>
2019-07-21 14:41:28 +0800

    format_rope

--- R/format_rope.R
+++ R/format_rope.R
@@ -7,9 +7,12 @@
 #' format_rope(c(0.02, 0.12, 0.357, 0))
 #' format_rope(c(0.02, 0.12, 0.357, 0), name = NULL)
 #' @export
-format_rope <- function(rope_percentage, name = "ROPE") {
-  text <- ifelse(rope_percentage == 0, "= 0%",
-                        ifelse(rope_percentage == 1, "= 100%",
-                               paste0("= ", format_value(rope_percentage * 100), "%")))
-  .add_prefix_and_remove_stars(text, stars = FALSE, stars_only = FALSE, name)
+format_rope <- function(rope_percentage, name = "in ROPE") {
+  text <- ifelse(rope_percentage == 0, "0%",
+                        ifelse(rope_percentage == 1, "100%",
+                               paste0(format_value(rope_percentage * 100), "%")))
+  if(!is.null(name)){
+    text <- paste(text, name)
+  }
+  text
 }
