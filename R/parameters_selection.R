7b7275542f3925d94221170ec5b9804fab3b20e3
Dominique Makowski <dom.mak19@gmail.com>
2019-08-03 10:39:42 +0800

    format parameters fixed for nested models

--- R/parameters_selection.R
+++ R/parameters_selection.R
@@ -63,6 +63,8 @@ parameters_selection.lm <- function(model, direction = "both", steps = 1000, k =
 #' @rdname parameters_selection
 #' @export
 parameters_selection.merMod <- function(model, direction = "backward", steps = 1000, ...) {
+
+  # Using cAIC4's stepcAIC()
   if (!requireNamespace("cAIC4", quietly = TRUE)) {
     stop("Package 'cAIC4' required for this function to work. Please install it by running `install.packages('cAIC4')`.")
   }
@@ -85,6 +87,15 @@ parameters_selection.merMod <- function(model, direction = "backward", steps = 1
     steps = steps,
     allowUseAcross = TRUE
   )$finalModel
+
+
+  # Using MuMIn's dredge(): works nicely BUT throws unnecessary warnings and requires to set global options for na.action even tho no NaNs.
+  # The code is here: https://github.com/cran/MuMIn/blob/master/R/dredge.R Maybe it could be reimplemented?
+  # if (!requireNamespace("MuMIn", quietly = TRUE)) {
+  #   stop("Package 'MuMIn' required for this function to work. Please install it by running `install.packages('MuMIn')`.")
+  # }
+  # model <- lmer(Sepal.Width ~ Sepal.Length * Petal.Width * Petal.Length + (1 | Species), data = iris, na.action = na.fail)
+  # summary(MuMIn::get.models(MuMIn::dredge(model), 1)[[1]])
 
   best
 }
