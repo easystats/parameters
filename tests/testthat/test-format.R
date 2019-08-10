e7b241c7db3182e4f39ef2bea9794e74bb7f49bb
Dominique Makowski <dom.mak19@gmail.com>
2019-08-10 11:26:19 +0800

    improved polynomial clean names and new funs: format_order and format_under

--- tests/testthat/test-format.R
+++ tests/testthat/test-format.R
@@ -18,6 +18,19 @@ test_that("format_value", {
   testthat::expect_equal(nchar(format_value(4.0, protect_integers = TRUE)), 1)
 })
 
+test_that("format_number and format_order", {
+  testthat::expect_equal(format_number(2), "two")
+  testthat::expect_equal(format_number(45), "forty five")
+  testthat::expect_equal(format_number(2), "two")
+
+  testthat::expect_equal(format_order(2), "second")
+  testthat::expect_equal(format_order(45), "forty fifth")
+
+  testthat::expect_equal(format_order(2, textual = FALSE), "2nd")
+  testthat::expect_equal(format_order(45, textual = FALSE), "45th")
+})
+
+
 test_that("format others", {
   testthat::expect_true(is.character(format_pd(0.02)))
   testthat::expect_equal(nchar(format_bf(4)), 9)
