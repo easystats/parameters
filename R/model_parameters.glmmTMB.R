bf6e6785f13dfb86caa92a49de5dd9d03b020370
Daniel <mail@danielluedecke.de>
2019-08-29 17:46:56 +0200

    final fixed glmmTMB

--- R/model_parameters.glmmTMB.R
+++ R/model_parameters.glmmTMB.R
@@ -1,26 +1,20 @@
 #' @inheritParams model_simulate
 #' @rdname model_parameters.merMod
 #' @export
-model_parameters.glmmTMB <- function(model, ci = .95, standardize = "refit", standardize_robust = FALSE, bootstrap = FALSE, p_method = "wald", ci_method = "wald", iterations = 1000, component = c("all", "conditional", "zi", "zero_inflated"), ...) {
+model_parameters.glmmTMB <- function(model, ci = .95, bootstrap = FALSE, p_method = "wald", ci_method = "wald", iterations = 1000, component = c("all", "conditional", "zi", "zero_inflated"), ...) {
 
   component <- match.arg(component)
+
+  # fix argument, if model has no zi-part
+  if (!insight::model_info(model)$is_zero_inflated && component != "conditional") {
+    component <- "conditional"
+  }
 
   # Processing
   if (bootstrap) {
     parameters <- parameters_bootstrap(model, iterations = iterations, ci = ci, ...)
   } else {
     parameters <- .extract_parameters_glmmTMB(model, ci = ci, component = component, ...)
-  }
-
-
-  # Standardized
-  if (isTRUE(standardize)) {
-    warning("Please set the `standardize` method explicitly. Set to \"refit\" by default.")
-    standardize <- "refit"
-  }
-
-  if (!is.null(standardize) && !is.logical(standardize)) {
-    parameters <- cbind(parameters, parameters_standardize(model, method = standardize, robust = standardize_robust)[2])
   }
 
   attr(parameters, "pretty_names") <- format_parameters(model)
@@ -40,16 +34,13 @@ model_parameters.glmmTMB <- function(model, ci = .95, standardize = "refit", sta
   colnames(parameters) <- c("Parameter", "Estimate", "Component")
   original_order <- parameters$.id <- 1:nrow(parameters)
 
-  if (component == "all")
-    merge_by <- c("Parameter", "Component")
-  else
-    merge_by <- "Parameter"
+  merge_by <- c("Parameter", "Component")
 
   # CI
   if (!is.null(ci)) {
     ci_df <- ci(model, ci = ci, component = component)
     if (length(ci) > 1) ci_df <- bayestestR::reshape_ci(ci_df)
-    ci_cols <- names(ci_df)[!names(ci_df) %in% c("CI", "Parameter", "Component")]
+    ci_cols <- names(ci_df)[!names(ci_df) %in% c("CI", merge_by)]
     parameters <- merge(parameters, ci_df, by = merge_by)
   } else {
     ci_cols <- c()
@@ -75,6 +66,9 @@ model_parameters.glmmTMB <- function(model, ci = .95, standardize = "refit", sta
   # Reorder
   col_order <- c("Parameter", "Coefficient", "SE", ci_cols, "t", "z", "df", "df_residual", "p", "Component")
   parameters <- parameters[col_order[col_order %in% names(parameters)]]
+
+  # remove Component column if not needed
+  if (length(unique(parameters$Component)) == 1) parameters$Component <- NULL
 
   rownames(parameters) <- NULL
   parameters
