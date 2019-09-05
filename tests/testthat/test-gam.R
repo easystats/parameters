92fadd928db1482480c5db35f23c7bddf816e478
Daniel Lüdecke <mail@danielluedecke.de>
2019-09-05 14:24:33 +0200

    add test

--- tests/testthat/test-gam.R
+++ tests/testthat/test-gam.R
@@ -2,13 +2,14 @@ if (require("testthat") &&
   require("parameters") &&
   require("mgcv")) {
 
+  set.seed(123)
   dat <- gamSim(1, n = 400, dist = "normal", scale = 2)
   m1 <- mgcv::gam(y ~ s(x0) + s(x1) + s(x2) + s(x3), data = dat)
 
   test_that("ci", {
     expect_equal(
       ci(m1)$CI_low,
-      c(7.49622189706408, NA, NA, NA, NA),
+      c(7.771085, NA, NA, NA, NA),
       tolerance = 1e-4
     )
   })
@@ -16,7 +17,7 @@ if (require("testthat") &&
   test_that("se", {
     expect_equal(
       standard_error(m1)$SE,
-      c(0.103747577565001, NA, NA, NA, NA),
+      c(0.1020741, NA, NA, NA, NA),
       tolerance = 1e-4
     )
   })
@@ -24,7 +25,7 @@ if (require("testthat") &&
   test_that("p_value", {
     expect_equal(
       p_value(m1)$p,
-      c(0, 0, 0, 0, 0.96301),
+      c(0, 0, 0, 0, 0.00196),
       tolerance = 1e-4
     )
   })
@@ -32,7 +33,7 @@ if (require("testthat") &&
   test_that("model_parameters", {
     expect_equal(
       model_parameters(m1)$Coefficient,
-      c(7.70019, 2.51662, 2.01425, 8.32417, 1),
+      c(7.97176, 3.63421, 2.97192, 8.29867, 1.04607),
       tolerance = 1e-4
     )
   })
