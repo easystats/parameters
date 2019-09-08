3a49845585318fd4eaa5119c2cc632c90ee16bec
Dominique Makowski <dom.mak19@gmail.com>
2019-09-08 14:43:51 +0800

    renamed n_sims to iterations for consistency with model_boostrap

--- R/model_simulate.R
+++ R/model_simulate.R
@@ -1,14 +1,13 @@
 #' Simulated draws from model coefficients
 #'
-#' Simulate draws a statistical model \code{n_sims} times to return a data.frame of estimates.
+#' Simulate draws from a statistical model to return a data.frame of estimates.
 #'
 #' @param model Statistical model (no Bayesian models).
-#' @param n_sims The number of simulation draws to create.
 #' @param component Should all parameters, parameters for the conditional model,
 #'   or for the zero-inflated part of the model be returned? Applies to models
 #'   with zero-inflated component. \code{component} may be one of \code{"conditional"},
 #'   \code{"zi"}, \code{"zero-inflated"} or \code{"all"} (default). May be abbreviated.
-#' @param ... Arguments passed to or from other methods.
+#' @inheritParams model_bootstrap
 #'
 #' @return A data frame.
 #'
@@ -47,7 +46,7 @@
 #' head(model_simulate(model))
 #' head(model_simulate(model, component = "zero_inflated"))
 #' @export
-model_simulate <- function(model, n_sims = 1000, ...) {
+model_simulate <- function(model, iterations = 1000, ...) {
   UseMethod("model_simulate")
 }
 
@@ -58,8 +57,8 @@ model_simulate <- function(model, n_sims = 1000, ...) {
 #' @importFrom stats vcov setNames
 #' @importFrom insight get_parameters
 #' @export
-model_simulate.lm <- function(model, n_sims = 1000, ...) {
-  .model_simulate(model, n_sims, component = "conditional")
+model_simulate.lm <- function(model, iterations = 1000, ...) {
+  .model_simulate(model, iterations, component = "conditional")
 }
 
 
@@ -132,25 +131,25 @@ model_simulate.truncreg <- model_simulate.lm
 
 
 #' @export
-model_simulate.gam <- function(model, n_sims = 1000, ...) {
+model_simulate.gam <- function(model, iterations = 1000, ...) {
   if (!requireNamespace("MASS", quietly = TRUE)) {
     stop("Package 'MASS' needed for this function to work. Please install it.", call. = FALSE)
   }
 
-  if (is.null(n_sims)) n_sims <- 1000
+  if (is.null(iterations)) iterations <- 1000
 
   beta <- stats::coef(model)
   varcov <- .get_varcov(model, "all")
-  as.data.frame(MASS::mvrnorm(n = n_sims, mu = beta, Sigma = varcov))
+  as.data.frame(MASS::mvrnorm(n = iterations, mu = beta, Sigma = varcov))
 }
 
 
 
 #' @export
-model_simulate.gamm <- function(model, n_sims = 1000, ...) {
+model_simulate.gamm <- function(model, iterations = 1000, ...) {
   model <- model$gam
   class(model) <- c("gam", "lm", "glm")
-  model_simulate(model, n_sims = n_sims, ...)
+  model_simulate(model, iterations = iterations, ...)
 }
 
 
@@ -165,7 +164,7 @@ model_simulate.gamm <- function(model, n_sims = 1000, ...) {
 #' @importFrom insight get_parameters
 #' @rdname model_simulate
 #' @export
-model_simulate.glmmTMB <- function(model, n_sims = 1000, component = c("all", "conditional", "zi", "zero_inflated"), ...) {
+model_simulate.glmmTMB <- function(model, iterations = 1000, component = c("all", "conditional", "zi", "zero_inflated"), ...) {
   component <- match.arg(component)
 
   if (component %in% c("zi", "zero_inflated", "all") && !insight::model_info(model)$is_zero_inflated) {
@@ -173,17 +172,17 @@ model_simulate.glmmTMB <- function(model, n_sims = 1000, component = c("all", "c
     component <- "conditional"
   }
 
-  if (is.null(n_sims)) n_sims <- 1000
+  if (is.null(iterations)) iterations <- 1000
 
   if (component == "all") {
-    d1 <- .model_simulate(model, n_sims, component = "conditional")
-    d2 <- .model_simulate(model, n_sims, component = "zero_inflated")
+    d1 <- .model_simulate(model, iterations, component = "conditional")
+    d2 <- .model_simulate(model, iterations, component = "zero_inflated")
     colnames(d2) <- paste0(colnames(d2), "_zi")
     d <- cbind(d1, d2)
   } else if (component == "conditional") {
-    d <- .model_simulate(model, n_sims, component = "conditional")
+    d <- .model_simulate(model, iterations, component = "conditional")
   } else {
-    d <- .model_simulate(model, n_sims, component = "zero_inflated")
+    d <- .model_simulate(model, iterations, component = "zero_inflated")
   }
 
   d
@@ -211,27 +210,27 @@ model_simulate.zerocount <- model_simulate.zeroinfl
 # helper -----------------------------------------
 
 
-.model_simulate <- function(model, n_sims, component = "conditional") {
+.model_simulate <- function(model, iterations, component = "conditional") {
   if (!requireNamespace("MASS", quietly = TRUE)) {
     stop("Package 'MASS' needed for this function to work. Please install it.", call. = FALSE)
   }
 
-  if (is.null(n_sims)) n_sims <- 1000
+  if (is.null(iterations)) iterations <- 1000
 
   parms <- insight::get_parameters(model, effects = "fixed", component = component)
   beta <- stats::setNames(parms$estimate, parms$parameter)
 
   varcov <- .get_varcov(model, component)
-  as.data.frame(MASS::mvrnorm(n = n_sims, mu = beta, Sigma = varcov))
+  as.data.frame(MASS::mvrnorm(n = iterations, mu = beta, Sigma = varcov))
 
   ## Alternative approach, similar to arm::sim()
 
   # k <- length(insight::find_parameters(model, effects = "fixed", component = "conditional", flatten = TRUE))
   # n <- insight::n_obs(model)
   # beta.cov <- stats::vcov(model) / stats::sigma(model)
-  # s <- vector("double", n_sims)
+  # s <- vector("double", iterations)
   # b <- array(NA, c(100, k))
-  # for (i in 1:n_sims) {
+  # for (i in 1:iterations) {
   #   s[i] <- stats::sigma(model) * sqrt((n - k) / rchisq(1, n - k))
   #   b[i,] <- MASS::mvrnorm(n = 1, mu = beta, Sigma = beta.cov * s[i] ^ 2)
   # }
