7c073d8d7867ce6e96a044e29f50b5b986efde9b
Dominique Makowski <dom.mak19@gmail.com>
2019-09-25 14:56:12 +0800

    added data_partition function

---
+++ R/data_partition.R
@@ -0,0 +1,47 @@
+#' Partition data into a test and a training set
+#'
+#' Creates a training and a test set based on a dataframe. Can also be stratified (i.e., evenly spread a given factor) using the \code{group} argument.
+#'
+#' @param x A data.frame.
+#' @param training_proportion The proportion (between 0 and 1) of the training set. The remaining part will be used for the test set.
+#' @param group A character vector indicating the name(s) of the column(s) used for stratified partionning.
+#'
+#' @examples
+#' df <- iris
+#' df$Smell <- rep(c("Strong", "Light"), 75)
+#'
+#' data_partition(df)
+#' data_partition(df, group = "Species")
+#' data_partition(df, group = c("Species", "Smell"))
+#'
+#'  @export
+data_partition <- function(x, training_proportion = 0.7, group = NULL, ...){
+
+  training <- data.frame()
+  test <- data.frame()
+
+  if(!is.null(group)){
+    for(i in split(x, x[group])){
+      out <- .data_partition(i, training_proportion)
+      training <- rbind(training, i[out$training, ])
+      test <- rbind(test, i[out$test, ])
+    }
+  } else{
+    out <- .data_partition(x, training_proportion)
+    training <- rbind(training, x[out$training, ])
+    test <- rbind(test, x[out$test, ])
+  }
+
+  list(training = training,
+       test = test)
+
+}
+
+#' @keywords internal
+.data_partition <- function(x, training_proportion = 0.8){
+  training_indices = sample(1:nrow(x), size = training_proportion * nrow(x))
+  test_indices <- (1:nrow(x))[!1:nrow(x) %in% training_indices]
+
+  list(training = training_indices,
+       test = test_indices)
+}
