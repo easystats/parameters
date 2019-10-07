d57c19b87fd68836404343c265d8afdb87ae5ec0
Dominique Makowski <dom.mak19@gmail.com>
2019-10-07 16:52:02 +0800

    add odds_to_r

--- tests/testthat/test-convert.R
+++ tests/testthat/test-convert.R
@@ -31,4 +31,7 @@ test_that("odds_to_d", {
 test_that("d_to_r", {
   testthat::expect_equal(d_to_r(d = 1.1547), 0.5, tolerance = 0.01)
   testthat::expect_equal(r_to_d(r = 0.5), 1.1547, tolerance = 0.01)
+
+  testthat::expect_equal(odds_to_r(odds = d_to_odds(d = r_to_d(0.5))), 0.5, tol = 0.001)
+  testthat::expect_equal(odds_to_d(r_to_odds(d_to_r(d = 1), log = TRUE), log = TRUE), 1, tolerance = 0.001)
 })
