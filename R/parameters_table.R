8d47ac2f04d83a6e3915f1dac2ee050d8c57a96e
Daniel Lüdecke <mail@danielluedecke.de>
2019-10-30 09:42:47 +0100

    improve ci-printing

--- R/parameters_table.R
+++ R/parameters_table.R
@@ -63,9 +63,10 @@ parameters_table <- function(x, pretty_names = TRUE, stars = FALSE, ...) {
       ci_colname <- sprintf("%i%% CI", attributes(x)$ci * 100)
     }
     # Get characters to align the CI
-    max_len <- max(unlist(lapply(stats::na.omit(c(round(x[ci_low], 2), round(x[ci_high], 2))), function(.i) nchar(as.character(.i)))))
+    max_len_low <- max(unlist(lapply(stats::na.omit(round(x[ci_low], 2)), function(.i) nchar(as.character(.i)))))
+    max_len_high <- max(unlist(lapply(stats::na.omit(round(x[ci_high], 2)), function(.i) nchar(as.character(.i)))))
     for (i in 1:length(ci_colname)) {
-      x[ci_colname[i]] <- format_ci(x[[ci_low[i]]], x[[ci_high[i]]], ci = NULL, digits = ci_digits, width = max_len, brackets = TRUE)
+      x[ci_colname[i]] <- format_ci(x[[ci_low[i]]], x[[ci_high[i]]], ci = NULL, digits = ci_digits, width_low = max_len_low, width_high = max_len_high, brackets = TRUE)
     }
     # Replace at initial position
     ci_position <- which(names(x) == ci_low[1])
