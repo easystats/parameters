f820d7071ad2c8672b350538132638e1df4658d9
Daniel Lüdecke <mail@danielluedecke.de>
2019-11-01 12:25:34 +0100

    minor fixes, update website

--- R/model_parameters.bayesian.R
+++ R/model_parameters.bayesian.R
@@ -1,21 +1,3 @@
-#' @importFrom insight get_priors
-#' @keywords internal
-.model_parameters_bayesian <- function(model, centrality = "median", dispersion = FALSE, ci = .89, ci_method = "hdi", test = c("pd", "rope"), rope_range = "default", rope_ci = 1.0, bf_prior = NULL, diagnostic = c("ESS", "Rhat"), priors = TRUE, iterations = 1000, ...) {
-
-  # Processing
-  parameters <- .extract_parameters_bayesian(model, centrality = centrality, dispersion = dispersion, ci = ci, ci_method = ci_method, test = test, rope_range = rope_range, rope_ci = rope_ci, bf_prior = bf_prior, diagnostic = diagnostic, priors = priors, iterations = iterations, ...)
-
-  attr(parameters, "ci") <- ci
-  parameters <- .add_pretty_names(parameters, model, ...)
-  class(parameters) <- c("parameters_model", "see_parameters_model", class(parameters))
-
-  parameters
-}
-
-
-
-
-
 #' Bayesian Models Parameters
 #'
 #' Parameters of Bayesian models.
@@ -40,13 +22,36 @@
 #' }
 #'
 #' @return A data.frame of indices related to the model's parameters.
+#' @importFrom insight get_priors
 #' @export
-model_parameters.stanreg <- .model_parameters_bayesian
+model_parameters.stanreg <- function(model, centrality = "median", dispersion = FALSE, ci = .89, ci_method = "hdi", test = c("pd", "rope"), rope_range = "default", rope_ci = 1.0, bf_prior = NULL, diagnostic = c("ESS", "Rhat"), priors = TRUE, iterations = 1000, ...) {
+
+  # Processing
+  parameters <- .extract_parameters_bayesian(model, centrality = centrality, dispersion = dispersion, ci = ci, ci_method = ci_method, test = test, rope_range = rope_range, rope_ci = rope_ci, bf_prior = bf_prior, diagnostic = diagnostic, priors = priors, iterations = iterations, ...)
+
+  attr(parameters, "ci") <- ci
+  attr(parameters, "pretty_names") <- format_parameters(model)
+  class(parameters) <- c("parameters_model", "see_parameters_model", class(parameters))
+
+  parameters
+}
 
 
 #' @export
-model_parameters.brmsfit <- model_parameters.stanreg
+model_parameters.brmsfit <- function(model, centrality = "median", dispersion = FALSE, ci = .89, ci_method = "hdi", test = c("pd", "rope"), rope_range = "default", rope_ci = 1.0, bf_prior = NULL, diagnostic = c("ESS", "Rhat"), priors = TRUE, iterations = 1000, ...) {
+
+  # Processing
+  parameters <- .extract_parameters_bayesian(model, centrality = centrality, dispersion = dispersion, ci = ci, ci_method = ci_method, test = test, rope_range = rope_range, rope_ci = rope_ci, bf_prior = bf_prior, diagnostic = diagnostic, priors = priors, iterations = iterations, ...)
+
+  attr(parameters, "ci") <- ci
+  parameters <- .add_pretty_names(parameters, model, ...)
+  class(parameters) <- c("parameters_model", "see_parameters_model", class(parameters))
+
+  parameters
+}
 
 
 #' @export
 model_parameters.MCMCglmm <- model_parameters.stanreg
+
+
