8646ec13942cbd9be0a6077e2274a006362efa70
Dominique Makowski <dom.mak19@gmail.com>
2019-11-13 17:27:48 +0800

    refactor format_parameters
    don't try dealing with nested because it's quite complicated and the previous method was prone to errors for complex designs

--- R/format_parameters.R
+++ R/format_parameters.R
@@ -5,22 +5,28 @@
 #' @examples
 #' library(parameters)
 #'
+#' model <- lm(Sepal.Length ~ Species * Sepal.Width, data = iris)
+#' format_parameters(model)
+#'
+#' model <- lm(Sepal.Length ~ Sepal.Width * Species, data = iris)
+#' format_parameters(model)
+#'
 #' model <- lm(Sepal.Length ~ Species * Sepal.Width * Petal.Length, data = iris)
 #' format_parameters(model)
 #'
-#' model <- lm(Sepal.Length ~ Petal.Length + (Species / Sepal.Width), data = iris)
-#' format_parameters(model)
-#'
-#' model <- lm(Sepal.Length ~ Petal.Length + (Species / (Sepal.Width * Petal.Width)), data = iris)
-#' format_parameters(model)
-#'
 #' model <- lm(Sepal.Length ~ Species / Petal.Length, data = iris)
+#' format_parameters(model)
+#'
+#' model <- lm(Sepal.Length ~ Petal.Length + (Species / Sepal.Width), data = iris)
 #' format_parameters(model)
 #'
 #' model <- lm(Sepal.Length ~ Species / Petal.Length * Sepal.Width, data = iris)
 #' format_parameters(model)
 #'
 #' model <- lm(Sepal.Length ~ Species / (Petal.Length * Sepal.Width), data = iris)
+#' format_parameters(model)
+#'
+#' model <- lm(Sepal.Length ~ Petal.Length + (Species / (Sepal.Width * Petal.Width)), data = iris)
 #' format_parameters(model)
 #'
 #' model <- lm(Sepal.Length ~ Species + poly(Sepal.Width, 2), data = iris)
@@ -34,6 +40,7 @@
 format_parameters <- function(model) {
   UseMethod("format_parameters")
 }
+
 
 #' @export
 format_parameters.default <- function(model) {
@@ -56,45 +63,30 @@ format_parameters.default <- function(model) {
 
   # Type-specific changes
   types <- parameters_type(model)
-  parameters <- .clean_parameter_names(types$Parameter, full = TRUE)
+  types$Parameter <- .clean_parameter_names(types$Parameter, full = TRUE)
 
   for (i in 1:nrow(types)) {
 
-    # Factors
-    if (types$Type[i] == "factor") {
-      names[i] <- .format_factor(name = names[i], variable = types$Variable[i])
-    }
+    name <- types$Parameter[i]
 
-    # Polynomials
-    if (types$Type[i] %in% c("poly", "poly_raw")) {
-      names[i] <- .format_poly(name = names[i], variable = types$Variable[i], type = types$Type[i], degree = types$Level[i])
-    }
+    # No interaction
+    if (!types$Type[i] %in% c("interaction", "nested")) {
+      type <- types[types$Parameter == name, ]
+      names[i] <- .format_parameter(name, variable = type$Variable, type = type$Type, level = type$Level)
 
-    # Smooth
-    if (types$Type[i] == "smooth") {
-      names[i] <- gsub("^smooth_(.*)\\[(.*)\\]", "\\2", names[i])
-      names[i] <- gsub("s(", "Smooth term (", names[i], fixed = TRUE)
-    }
-
-    # Interactions
-    if (types$Type[i] %in% c("interaction", "nested")) {
-      sep <- ifelse(types$Type[i] == "interaction", " * ", " / ")
-
-      components <- unlist(strsplit(names[i], ":", fixed = TRUE))
-
-      for (j in 1:length(components)) {
-        component <- components[j]
-        if (component %in% parameters) {
-          if (types[parameters == component, "Type"] == "factor") {
-            components[j] <- .format_factor(component, types[parameters == component, "Variable"])
-          }
+    # Interaction or nesting
+    } else{
+      components <- unlist(strsplit(name, ":", fixed = TRUE))
+      for(j in 1:length(components)){
+        if(components[j] %in% types$Parameter){
+          type <- types[types$Parameter == components[j], ]
+          components[j] <- .format_parameter(components[j], variable = type$Variable, type = type$Type, level = type$Level)
+        } else if (components[j] %in% types$Secondary_Parameter){
+          type <- types[!is.na(types$Secondary_Parameter) & types$Secondary_Parameter == components[j], ]
+          components[j] <- .format_parameter(components[j], variable = type[1, ]$Secondary_Variable, type = type[1, ]$Secondary_Type, level = type[1, ]$Secondary_Level)
         }
       }
-      if (length(components) > 2) {
-        names[i] <- paste0("(", paste0(utils::head(components, -1), collapse = " * "), ")", sep, utils::tail(components, 1))
-      } else {
-        names[i] <- paste0(components, collapse = sep)
-      }
+      names[i] <- .format_interaction(components, type = types[i, "Type"])
     }
   }
   names(names) <- types$Parameter
@@ -119,13 +111,61 @@ format_parameters.parameters_model <- function(model) {
 }
 
 
+
+
+
+# Utilities ---------------------------------------------------------------
+
+
+
+
+#' @keywords internal
+.format_parameter <- function(name, variable, type, level){
+
+  # Factors
+  if (type == "factor") {
+    name <- .format_factor(name = name, variable = variable)
+  }
+
+  # Polynomials
+  if (type %in% c("poly", "poly_raw")) {
+    name <- .format_poly(name = name, variable = variable, type = type, degree = level)
+  }
+
+  # Smooth
+  if (type == "smooth") {
+    name <- gsub("^smooth_(.*)\\[(.*)\\]", "\\2", name)
+    name <- gsub("s(", "Smooth term (", name, fixed = TRUE)
+  }
+
+  name
+}
+
+
+#' @keywords internal
+.format_interaction <- function(components, type) {
+  # sep <- ifelse(type == "interaction", " * ", " / ")
+
+  if (length(components) > 2) {
+    if(type == "interaction"){
+      components <- paste0("(", paste0(utils::head(components, -1), collapse = " * "), ")", " * ", utils::tail(components, 1))
+    } else{
+      components <- paste0(components, collapse = " * ")
+    }
+  } else {
+    components <- paste0(components, collapse = " * ")
+  }
+  components
+}
+
+
 #' @keywords internal
 .format_factor <- function(name, variable) {
   level <- gsub(variable, "", name)
-  paste0(variable, " (", level, ")")
+  paste0(variable, " [", level, "]")
 }
 
 #' @keywords internal
 .format_poly <- function(name, variable, type, degree) {
-  paste0(variable, " (", format_order(as.numeric(degree), textual = FALSE), " degree)")
+  paste0(variable, " [", format_order(as.numeric(degree), textual = FALSE), " degree]")
 }
