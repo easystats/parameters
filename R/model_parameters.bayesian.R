7c383cc3546590c2475bacb5520852fe0146e10c
Daniel <mail@danielluedecke.de>
2019-11-15 21:44:02 +0100

    fix issues with MCMCglmm

--- R/model_parameters.bayesian.R
+++ R/model_parameters.bayesian.R
@@ -63,4 +63,14 @@ model_parameters.brmsfit <- function(model, centrality = "median", dispersion = 
 
 
 #' @export
-model_parameters.MCMCglmm <- model_parameters.stanreg
+model_parameters.MCMCglmm <- function(model, centrality = "median", dispersion = FALSE, ci = .89, ci_method = "hdi", test = c("pd", "rope"), rope_range = "default", rope_ci = 1.0, bf_prior = NULL, diagnostic = c("ESS", "Rhat"), priors = TRUE, iterations = 1000, ...) {
+
+  # Processing
+  parameters <- .extract_parameters_bayesian(model, centrality = centrality, dispersion = dispersion, ci = ci, ci_method = ci_method, test = test, rope_range = rope_range, rope_ci = rope_ci, bf_prior = bf_prior, diagnostic = diagnostic, priors = priors, iterations = iterations, ...)
+
+  attr(parameters, "pretty_names") <- format_parameters(model)
+  attr(parameters, "ci") <- ci
+  class(parameters) <- c("parameters_model", "see_parameters_model", class(parameters))
+
+  parameters
+}
