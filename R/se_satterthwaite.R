c09d0595d8f7261fa5b6bc351777c0342e81992d
Daniel <mail@danielluedecke.de>
2019-11-29 16:57:42 +0100

    update website

--- R/se_satterthwaite.R
+++ R/se_satterthwaite.R
@@ -3,9 +3,12 @@
 #' @importFrom insight get_parameters
 #' @export
 se_satterthwaite <- function(model) {
+  if (!requireNamespace("lmerTest", quietly = TRUE)) {
+    stop("Package `lmerTest` required for Kenward-Rogers approximation.", call. = FALSE)
+  }
+
   params <- insight::get_parameters(model)
   lmerTest_model <- lmerTest::as_lmerModLmerTest(model)
-  s <- summary(lmerTest_model)
 
   data.frame(
     Parameter = params$Parameter,
