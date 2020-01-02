9da590a11c11a726c4cb22ca9c86affc02a1e99c
Daniel <mail@danielluedecke.de>
2020-01-02 12:17:52 +0100

    update website

--- tests/testthat/test-model_parameters.cpglmm.R
+++ tests/testthat/test-model_parameters.cpglmm.R
@@ -2,7 +2,7 @@ if (require("testthat") && require("insight") && require("parameters") && requir
   data("FineRoot")
   model <- cpglmm(RLD ~ Stock + Spacing + (1 | Plant), data = FineRoot)
 
-  test_that("model_parameters.lme", {
+  test_that("model_parameters.cpglmm", {
     params <- model_parameters(model)
     testthat::expect_equal(params$SE, c(0.1308, 0.2514, 0.2, 0.1921), tolerance = 1e-3)
     testthat::expect_equal(
