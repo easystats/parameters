1db5ba52ec2e4f3bbe275f5d82b8ad9db4d686a5
Daniel <mail@danielluedecke.de>
2020-01-25 19:14:19 +0100

    fix test

--- tests/testthat/test-model_parameters.metafor.R
+++ tests/testthat/test-model_parameters.metafor.R
@@ -1,12 +1,13 @@
 if (require("testthat") && require("insight") && require("parameters") && require("metafor")) {
-  data <- data.frame(
+  test <- data.frame(
     estimate = c(0.111, 0.245, 0.8, 1.1, 0.03),
     std.error = c(0.05, 0.111, 0.001, 0.2, 0.01)
   )
-  model <- metafor::rma(yi = estimate, sei = std.error, data = data)
+  mydat <<- test
+  model <- metafor::rma(yi = estimate, sei = std.error, data = mydat)
   params <- model_parameters(model)
 
-  test_that("model_parameters.cpglmm", {
+  test_that("model_parameters.metafor", {
     expect_equal(params$Parameter, c("Study 1", "Study 2", "Study 3", "Study 4", "Study 5", "Overall"))
     expect_equal(
       colnames(params),
