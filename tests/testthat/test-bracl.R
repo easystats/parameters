1b3f3f2c5d5b984d50391dfeeded42729652c371
Daniel <mail@danielluedecke.de>
2020-01-27 21:13:00 +0100

    fix tests

--- tests/testthat/test-bracl.R
+++ tests/testthat/test-bracl.R
@@ -14,14 +14,14 @@ if (require("testthat") &&
       c("definitly", "alterly", "probably not", "definitly", "alterly", "probably not", "definitly", "alterly", "probably not")
     )
     expect_equal(
-      params$Response,
+      params$Parameter,
       c("definitly:(Intercept)", "alterly:(Intercept)", "probably not:(Intercept)",
         "definitly:as.numeric(religion)", "alterly:as.numeric(religion)",
         "probably not:as.numeric(religion)", "definitly:genderfemale",
         "alterly:genderfemale", "probably not:genderfemale")
     )
     expect_equal(
-      params$Response,
+      params$Coefficient,
       c(-1.24836, 0.47098, 0.42741, 0.4382, 0.25962, 0.01192, -0.13683, 0.18707, -0.16093),
       tolerance = 1e-3
     )
