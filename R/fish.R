d71c6287921d33ef3381162832b11745b69a7103
Daniel <mail@danielluedecke.de>
2020-03-28 11:29:13 +0100

    add sample data, update citation

--- R/fish.R
+++ R/fish.R
@@ -6,3 +6,12 @@
 #' @description A sample data set, used in tests and some examples.
 NULL
 
+
+#' @docType data
+#' @title Sample data set
+#' @name demean
+#' @keywords data
+#'
+#' @description A sample data set with longitudinal data, used in the vignette describing the \code{demean()} function.
+NULL
+
