6c357748cf6d5e8a69a912b9e7c361768e2d2a6d
Daniel <mail@danielluedecke.de>
2020-04-22 00:26:56 +0200

    hotfix

--- R/model_parameters_default.R
+++ R/model_parameters_default.R
@@ -70,7 +70,7 @@ model_parameters.default <- function(model, ci = .95, bootstrap = FALSE, iterati
   if (bootstrap) {
     params <- bootstrap_parameters(model, iterations = iterations, ci = ci, ...)
   } else {
-    .extract_parameters_generic(model, ci = ci, component = component, merge_by = merge_by, standardize = standardize, effects = effects, robust = robust, df_method = df_method, p_adjust = p_adjust, ...)
+    params <- .extract_parameters_generic(model, ci = ci, component = component, merge_by = merge_by, standardize = standardize, effects = effects, robust = robust, df_method = df_method, p_adjust = p_adjust, ...)
   }
 
   if (exponentiate) params <- .exponentiate_parameters(params)
