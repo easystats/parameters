65dc338fd674176f26c60fddabd738539c20e842
Daniel <mail@danielluedecke.de>
2020-04-23 09:08:36 +0200

    close #253

--- R/p_value.R
+++ R/p_value.R
@@ -150,6 +150,23 @@ p_value.tobit <- function(model, ...) {
 
 
 
+#' @export
+p_value.speedlm <- function(model, ...) {
+  p <- p_value.default(model, ...)
+  if (!is.numeric(p$p)) {
+    p$p <- tryCatch({
+      as.numeric(as.character(p$p))
+    },
+    error = function(e) {
+      p$p
+    })
+  }
+  p
+}
+
+
+
+
 
 
 
