3736894b52f5b5567d8081afcb352acda87c5c08
Daniel <mail@danielluedecke.de>
2020-05-08 11:54:26 +0200

    docs

--- R/robust_estimation.R
+++ R/robust_estimation.R
@@ -6,8 +6,8 @@
 #'
 #' @param model A model.
 #' @param vcov_estimation String, indicating the suffix of the \code{vcov*()}-function
-#'   from the \pkg{sandwich}-package, e.g. \code{vcov_estimation = "CL"} (which
-#'   calls \code{\link[sandwich]{vcovCL}} to compute clustered covariance matrix
+#'   from the \pkg{sandwich} or \pkg{clubSandwich} package, e.g. \code{vcov_estimation = "CL"}
+#'   (which calls \code{\link[sandwich]{vcovCL}} to compute clustered covariance matrix
 #'   estimators), or \code{vcov_estimation = "HC"} (which calls
 #'   \code{\link[sandwich:vcovHC]{vcovHC()}} to compute heteroskedasticity-consistent
 #'   covariance matrix estimators).
