f597ddf5ac639936e42b3efd5568ce3d61b9fa5e
Daniel <mail@danielluedecke.de>
2020-05-11 16:52:39 +0200

    fix tolerance for Linux

--- tests/testthat/test-gam.R
+++ tests/testthat/test-gam.R
@@ -9,7 +9,7 @@ if (require("testthat") &&
     expect_equal(
       ci(m1)$CI_low,
       c(7.771085, NA, NA, NA, NA),
-      tolerance = 1e-4
+      tolerance = 1e-3
     )
   })
 
@@ -17,7 +17,7 @@ if (require("testthat") &&
     expect_equal(
       standard_error(m1)$SE,
       c(0.1020741, NA, NA, NA, NA),
-      tolerance = 1e-4
+      tolerance = 1e-3
     )
   })
 
@@ -25,7 +25,7 @@ if (require("testthat") &&
     expect_equal(
       p_value(m1)$p,
       c(0, 0, 0, 0, 0.00196),
-      tolerance = 1e-4
+      tolerance = 1e-3
     )
   })
 
@@ -33,7 +33,7 @@ if (require("testthat") &&
     expect_equal(
       model_parameters(m1)$Coefficient,
       c(7.97176, 3.63421, 2.97192, 8.29867, 1.04607),
-      tolerance = 1e-4
+      tolerance = 1e-3
     )
   })
 }
