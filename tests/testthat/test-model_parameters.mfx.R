0be4e9cbf586f8d31e65a396f6194a6cd417ea9c
Daniel <mail@danielluedecke.de>
2020-06-03 07:42:48 +0200

    add tests

--- tests/testthat/test-model_parameters.mfx.R
+++ tests/testthat/test-model_parameters.mfx.R
@@ -21,15 +21,39 @@ if (require("testthat") && require("insight") && require("parameters") && requir
 
   test_that("model_parameters.betaor", {
     params <- model_parameters(model)
-    testthat::expect_equal(params$Parameter, c("x", "(Intercept)"))
+    testthat::expect_equal(params$Parameter, c("(Intercept)", "x"))
     testthat::expect_equal(params$Coefficient, c(1.35961, 0.13947), tolerance = 1e-2)
     testthat::expect_null(params$Component)
   })
 
   test_that("model_parameters.betaor", {
     params <- model_parameters(model, component = "all")
-    testthat::expect_equal(params$Parameter, c("x", "(Intercept)", "x", "(Intercept)"))
+    testthat::expect_equal(params$Parameter, c("(Intercept)", "x", "(Intercept)", "x"))
     testthat::expect_equal(params$Coefficient, unname(do.call(rbind, coef(summary(model$fit)))[, 1]), tolerance = 1e-2)
     testthat::expect_equal(params$Component, c("conditional", "conditional", "precision", "precision"))
+  })
+
+
+  set.seed(12345)
+  n = 1000
+  x = rnorm(n)
+  y = rnegbin(n, mu = exp(1 + 0.5 * x), theta = 0.5)
+
+  data = data.frame(y, x)
+
+  model <- poissonmfx(formula = y ~ x, data = data)
+
+  test_that("model_parameters.poissonmfx", {
+    params <- model_parameters(model)
+    testthat::expect_equal(params$Parameter, c("x", "(Intercept)", "x"))
+    testthat::expect_equal(params$Coefficient, c(1.46009, 0.96036, 0.54496), tolerance = 1e-2)
+    testthat::expect_equal(params$Component, c("marginal", "conditional", "conditional"))
+  })
+
+  test_that("model_parameters.poissonmfx", {
+    params <- model_parameters(model, component = "cond")
+    testthat::expect_equal(params$Parameter, c("(Intercept)", "x"))
+    testthat::expect_equal(params$Coefficient, c(0.96036, 0.54496), tolerance = 1e-2)
+    testthat::expect_null(params$Component)
   })
 }
