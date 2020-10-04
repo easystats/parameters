1ef177596282d5359e791dfa37742b02cabbcce9
Daniel <mail@danielluedecke.de>
2020-10-04 20:42:07 +0200

    test

--- tests/testthat/test-zeroinfl.R
+++ tests/testthat/test-zeroinfl.R
@@ -31,25 +31,52 @@ if (require("testthat") &&
   test_that("model_parameters", {
     expect_equal(
       model_parameters(m1)$Coefficient,
-      c(0.56167, -0.22945, 0.13656, -0.16725, 0.02132, -0.92959, 0.04599, -0.24912),
+      as.vector(coef(m1)),
       tolerance = 1e-4
     )
   })
 
 
-  ## TODO activate once insight update on CRAN
+  m2 <- zeroinfl(formula = art ~ . | 1, data = bioChemists, dist = "negbin")
+  test_that("model_parameters", {
+    expect_equal(
+      model_parameters(m2)$Coefficient,
+      as.vector(coef(m2)),
+      tolerance = 1e-4
+    )
+    expect_equal(
+      model_parameters(m2)$Coefficient,
+      c("conditional", "conditional", "conditional", "conditional",
+        "conditional", "conditional", "zero_inflated")
+    )
+  })
 
-  # m2 <- zeroinfl(formula = art ~ . | 1, data = bioChemists, dist = "negbin")
-  # test_that("model_parameters", {
-  #   expect_equal(
-  #     model_parameters(m2)$Coefficient,
-  #     c(0.25615, -0.21642, 0.15049, -0.17642, 0.01527, 0.02908, -11.95447),
-  #     tolerance = 1e-4
-  #   )
-  #   expect_equal(
-  #     model_parameters(m2)$Coefficient,
-  #     c("conditional", "conditional", "conditional", "conditional",
-  #       "conditional", "conditional", "zero_inflated")
-  #   )
-  # })
+  m3 <- zeroinfl(art ~ mar + kid5 * fem + ment | kid5 * fem + phd, data = bioChemists)
+
+  test_that("model_parameters", {
+    expect_equal(
+      model_parameters(m3)$Coefficient,
+      as.vector(coef(m3)),
+      tolerance = 1e-4
+    )
+  })
+
+  test_that("parameters_type", {
+    expect_equal(
+      parameters_type(m3)$Type,
+      c("intercept", "factor", "numeric", "factor", "numeric", "interaction",
+        "intercept", "numeric", "factor", "numeric", "interaction"),
+      tolerance = 1e-4
+    )
+  })
+
+  test_that("parameters_type", {
+    expect_equal(
+      parameters_type(m3)$Link,
+      c("Mean", "Difference", "Association", "Difference", "Association",
+        "Difference", "Mean", "Association", "Difference", "Association",
+        "Difference"),
+      tolerance = 1e-4
+    )
+  })
 }
