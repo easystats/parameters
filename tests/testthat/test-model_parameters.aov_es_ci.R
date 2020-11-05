c191430e08be8d1b890eb3316a019f9a1d29e291
Daniel <mail@danielluedecke.de>
2020-11-05 21:15:11 +0100

    tests

--- tests/testthat/test-model_parameters.aov_es_ci.R
+++ tests/testthat/test-model_parameters.aov_es_ci.R
@@ -1,82 +1,110 @@
-.runThisTest <- Sys.getenv("RunAllparametersTests") == "yes"
+if (require("insight") && require("effectsize") && require("testthat") && require("lme4") && require("parameters")) {
 
-if (.runThisTest) {
-  if (require("insight") && require("effectsize") && require("testthat") && require("lme4") && require("parameters")) {
+  unloadNamespace("lmerTest")
+  data(iris)
+  iris$Cat1 <- rep(c("X", "X", "Y"), length.out = nrow(iris))
+  iris$Cat2 <- rep(c("A", "B"), length.out = nrow(iris))
 
-    unloadNamespace("lmerTest")
-    data(iris)
-    iris$Cat1 <- rep(c("X", "X", "Y"), length.out = nrow(iris))
-    iris$Cat2 <- rep(c("A", "B"), length.out = nrow(iris))
+  # aov ----------------------------------
 
-    test_that("model_parameters.aov", {
-      model <- aov(Sepal.Width ~ Species, data = iris)
+  test_that("model_parameters.aov", {
+    model <- aov(Sepal.Width ~ Species, data = iris)
+    mp <- model_parameters(model, omega_squared = "partial", eta_squared = "partial", epsilon_squared = TRUE, ci = .9)
+    es <- effectsize::omega_squared(model, partial = TRUE, ci = .9)
+    expect_equivalent(na.omit(mp$Omega2_CI_low), es$CI_low, tolerance = 1e-3)
+    expect_equivalent(mp$Omega2_CI_low, c(0.29018, NA), tolerance = 1e-3)
+    expect_equivalent(na.omit(mp$Omega2_CI_high), es$CI_high, tolerance = 1e-3)
+
+    expect_equal(colnames(mp), c("Parameter", "Sum_Squares", "df", "Mean_Square", "F", "p",
+                                 "Omega2_partial", "Omega2_CI_low", "Omega2_CI_high", "Eta2_partial",
+                                 "Eta2_CI_low", "Eta2_CI_high", "Epsilon2_partial", "Epsilon2_CI_low",
+                                 "Epsilon2_CI_high"))
+
+    model <- aov(Sepal.Length ~ Species * Cat1 * Cat2, data = iris)
+    mp <- model_parameters(model, eta_squared = "raw", ci = .9)
+    es <- effectsize::eta_squared(model, partial = FALSE, ci = .9)
+    expect_equivalent(na.omit(mp$Eta2_CI_low), es$CI_low, tolerance = 1e-3)
+    expect_equivalent(mp$Eta2_CI_low, c(0.53866, 0, 0, 0, 0, 0, 0, NA), tolerance = 1e-3)
+    expect_equivalent(na.omit(mp$Eta2_CI_high), es$CI_high, tolerance = 1e-3)
+
+    expect_equal(colnames(mp), c("Parameter", "Sum_Squares", "df", "Mean_Square", "F", "p",
+                                 "Eta2", "Eta2_CI_low", "Eta2_CI_high"))
+  })
+
+
+  # anova ---------------------
+
+  data(mtcars)
+  test_that("model_parameters.anova", {
+    model <- anova(lm(Sepal.Length ~ Species * Cat1 * Cat2, data = iris))
+    mp <- model_parameters(model, omega_squared = "partial", eta_squared = "partial", epsilon_squared = TRUE, ci = .9)
+    es <- effectsize::omega_squared(model, partial = TRUE, ci = .9)
+    expect_equivalent(na.omit(mp$Omega2_CI_low), es$CI_low, tolerance = 1e-3)
+    expect_equivalent(na.omit(mp$Omega2_CI_high), es$CI_high, tolerance = 1e-3)
+
+    expect_equal(colnames(mp), c("Parameter", "Sum_Squares", "df", "Mean_Square", "F", "p",
+                                 "Omega2_partial", "Omega2_CI_low", "Omega2_CI_high", "Eta2_partial",
+                                 "Eta2_CI_low", "Eta2_CI_high", "Epsilon2_partial", "Epsilon2_CI_low",
+                                 "Epsilon2_CI_high"))
+  })
+
+  data(mtcars)
+  test_that("model_parameters.anova", {
+    model <- aov(wt ~ cyl + Error(gear), data = mtcars)
+    mp <- model_parameters(model, omega_squared = "partial", eta_squared = "partial", epsilon_squared = TRUE, ci = .9)
+    es <- effectsize::omega_squared(model, partial = TRUE, ci = .9)
+    expect_equivalent(na.omit(mp$Omega2_CI_low), es$CI_low, tolerance = 1e-3)
+    expect_equivalent(na.omit(mp$Omega2_CI_high), es$CI_high, tolerance = 1e-3)
+
+    expect_equal(colnames(mp), c("Group", "Parameter", "Sum_Squares", "df", "Mean_Square", "F", "p",
+                                 "Omega2_partial", "Omega2_CI_low", "Omega2_CI_high", "Eta2_partial",
+                                 "Eta2_CI_low", "Eta2_CI_high", "Epsilon2_partial", "Epsilon2_CI_low",
+                                 "Epsilon2_CI_high"))
+  })
+
+
+  # car anova ---------------------------------
+
+  if (require("car")) {
+    set.seed(123)
+    data(Moore)
+    mod <-
+      car::Anova(stats::lm(
+        formula = conformity ~ fcategory * partner.status,
+        data = Moore,
+        contrasts = list(fcategory = contr.sum, partner.status = contr.sum)
+      ))
+    test_that("model_parameters.car-anova", {
       mp <- model_parameters(model, omega_squared = "partial", eta_squared = "partial", epsilon_squared = TRUE, ci = .9)
       es <- effectsize::omega_squared(model, partial = TRUE, ci = .9)
       expect_equivalent(na.omit(mp$Omega2_CI_low), es$CI_low, tolerance = 1e-3)
+      expect_equivalent(mp$Omega2_CI_low, c(0.52133, 0, 0, 0, 0, 0, 0, NA), tolerance = 1e-3)
       expect_equivalent(na.omit(mp$Omega2_CI_high), es$CI_high, tolerance = 1e-3)
 
       expect_equal(colnames(mp), c("Parameter", "Sum_Squares", "df", "Mean_Square", "F", "p",
                                    "Omega2_partial", "Omega2_CI_low", "Omega2_CI_high", "Eta2_partial",
                                    "Eta2_CI_low", "Eta2_CI_high", "Epsilon2_partial", "Epsilon2_CI_low",
                                    "Epsilon2_CI_high"))
-
-      model <- aov(Sepal.Length ~ Species * Cat1 * Cat2, data = iris)
-      testthat::expect_equal(sum(model_parameters(model, omega_squared = "raw", eta_squared = "partial", epsilon_squared = TRUE)$df), 149)
-
-      model <- aov(Sepal.Length ~ Species / Cat1 * Cat2, data = iris)
-      testthat::expect_equal(sum(model_parameters(model)$df), 149)
-    })
-
-    data(mtcars)
-    test_that("model_parameters.anova", {
-      model <- anova(lm(Sepal.Width ~ Species, data = iris))
-      testthat::expect_equal(sum(model_parameters(model)$df), 149)
-
-      model <- anova(lm(Sepal.Length ~ Species * Cat1 * Cat2, data = iris))
-      testthat::expect_equal(sum(model_parameters(model)$df), 149)
-
-      model <- anova(lmer(wt ~ 1 + (1 | gear), data = mtcars))
-      testthat::expect_equal(nrow(model_parameters(model)), 0)
-
-      model <- anova(lmer(wt ~ cyl + (1 | gear), data = mtcars))
-      testthat::expect_equal(sum(model_parameters(model)$df), 1)
-
-      model <- anova(lmer(wt ~ drat + cyl + (1 | gear), data = mtcars))
-      testthat::expect_equal(sum(model_parameters(model)$df), 2)
-
-      model <- anova(lmer(wt ~ drat * cyl + (1 | gear), data = mtcars))
-      testthat::expect_equal(sum(model_parameters(model)$df), 3)
-
-      model <- anova(lmer(wt ~ drat/cyl + (1 | gear), data = mtcars))
-      testthat::expect_equal(sum(model_parameters(model)$df), 2)
-    })
-
-    if (.runThisTest) {
-      test_that("model_parameters.anova", {
-        model <- insight::download_model("anova_3")
-        testthat::expect_equal(sum(model_parameters(model)$df), 149)
-
-        model <- insight::download_model("anova_4")
-        testthat::expect_equal(sum(model_parameters(model)$df, na.rm = TRUE), 2)
-
-        model <- insight::download_model("anova_lmerMod_5")
-        testthat::expect_equal(sum(model_parameters(model)$df), 1)
-
-        model <- insight::download_model("anova_lmerMod_6")
-        testthat::expect_equal(sum(model_parameters(model)$df), 12)
-      })
-    }
-
-    data(mtcars)
-    test_that("model_parameters.anova", {
-      model <- aov(wt ~ cyl + Error(gear), data = mtcars)
-      testthat::expect_equal(sum(model_parameters(model)$df), 31)
-
-      model <- aov(Sepal.Length ~ Species * Cat1 + Error(Cat2), data = iris)
-      testthat::expect_equal(sum(model_parameters(model)$df), 149)
-
-      model <- aov(Sepal.Length ~ Species / Cat1 + Error(Cat2), data = iris)
-      testthat::expect_equal(sum(model_parameters(model)$df), 149)
     })
   }
+
+
+  # maov ----------------------------------
+
+  set.seed(123)
+  fit <- lm(cbind(mpg, disp, hp) ~ factor(cyl), data = mtcars)
+  model <- aov(fit)
+
+  test_that("model_parameters.maov", {
+    mp <- model_parameters(model, omega_squared = "partial", eta_squared = "partial", epsilon_squared = TRUE, ci = .9)
+    es <- effectsize::omega_squared(model, partial = TRUE, ci = .9)
+    expect_equivalent(na.omit(mp$Omega2_CI_low), es$CI_low, tolerance = 1e-3)
+    expect_equivalent(mp$Omega2_CI_low, c(0.71218, NA, 0.50841, NA, 0.53774, NA), tolerance = 1e-3)
+    expect_equivalent(na.omit(mp$Omega2_CI_high), es$CI_high, tolerance = 1e-3)
+
+    expect_equal(colnames(mp), c("Response", "Parameter", "Sum_Squares", "df", "Mean_Square", "F", "p",
+                                 "Omega2_partial", "Omega2_CI_low", "Omega2_CI_high", "Eta2_partial",
+                                 "Eta2_CI_low", "Eta2_CI_high", "Epsilon2_partial", "Epsilon2_CI_low",
+                                 "Epsilon2_CI_high"))
+  })
 }
