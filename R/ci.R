d515d1e406f5c200d201ff226495b4c068aa6bb7
Daniel <mail@danielluedecke.de>
2020-11-12 08:40:40 +0100

    #176

--- R/ci.R
+++ R/ci.R
@@ -831,23 +831,38 @@ ci.effectsize_table <- ci.effectsize_std_params
 #' @export
 ci.rma <- function(x, ci = .95, ...) {
   params <- insight::get_parameters(x)
-  tryCatch(
+  out <- tryCatch(
     {
-      out <- lapply(ci, function(i) {
+      tmp <- lapply(ci, function(i) {
         model <- stats::update(x, level = i)
         .data_frame(
-          Parameter = params[[1]],
+          Parameter = params$Parameter,
           CI = i * 100,
           CI_low = as.vector(model$ci.lb),
           CI_high = as.vector(model$ci.ub)
         )
       })
-      .remove_backticks_from_parameter_names(do.call(rbind, out))
+      .remove_backticks_from_parameter_names(do.call(rbind, tmp))
     },
     error = function(e) {
       NULL
     }
   )
+  if (is.null(out)) {
+    se <- standard_error(x)
+    out <- lapply(ci, function(i) {
+      alpha <- (1 + i) / 2
+      fac <- stats::qnorm(alpha)
+      .data_frame(
+        Parameter = params$Parameter,
+        CI = i * 100,
+        CI_low = params$Estimate - as.vector(se$SE) * fac,
+        CI_high = params$Estimate + as.vector(se$SE) * fac
+      )
+    })
+    out <- .remove_backticks_from_parameter_names(do.call(rbind, out))
+  }
+  out
 }
 
 
