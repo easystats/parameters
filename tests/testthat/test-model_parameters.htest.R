5f081d5176739c1b013c917d4f0054d6920f3a41
Daniel <mail@danielluedecke.de>
2020-11-14 20:00:33 +0100

    test

--- tests/testthat/test-model_parameters.htest.R
+++ tests/testthat/test-model_parameters.htest.R
@@ -22,20 +22,20 @@ if (require("testthat") && require("parameters")) {
   if (require("effectsize")) {
     data(mtcars)
     mp <- model_parameters(stats::chisq.test(table(mtcars$am)), cramers_v = "raw", phi = "raw", ci = 0.95)
-    test_that("model_parameters.htest", {
+    test_that("model_parameters-chisq-test raw", {
       expect_equal(mp$Chi2, 1.125, tolerance = 1e-3)
       expect_equal(mp$phi, 0.1875, tolerance = 1e-3)
       expect_equal(colnames(mp), c("Chi2", "df", "Cramers_v", "Cramers_CI_low", "Cramers_CI_high", "phi", "phi_CI_low", "phi_CI_high", "p"))
     })
 
     mp <- model_parameters(stats::chisq.test(table(mtcars$am)))
-    test_that("model_parameters.htest", {
+    test_that("model_parameters-chisq-test NULL", {
       expect_equal(mp$Chi2, 1.125, tolerance = 1e-3)
       expect_equal(colnames(mp), c("Chi2", "df", "p"))
     })
 
     mp <- model_parameters(stats::chisq.test(table(mtcars$am)), phi = "adjusted", ci = 0.95)
-    test_that("model_parameters.htest", {
+    test_that("model_parameters-chisq-test adjusted", {
       expect_equal(mp$Chi2, 1.125, tolerance = 1e-3)
       expect_equal(mp$phi_adjusted, 0.0538348, tolerance = 1e-3)
       expect_equal(colnames(mp), c("Chi2", "df", "phi_adjusted", "phi_CI_low", "phi_CI_high", "p"))
