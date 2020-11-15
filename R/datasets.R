2df72f7c0eb53c361d2bf108c4dd54e923da02d9
Daniel <mail@danielluedecke.de>
2020-11-15 11:32:40 +0100

    document sample data

---
+++ R/datasets.R
@@ -0,0 +1,27 @@
+#' @docType data
+#' @title Sample data set
+#' @name fish
+#' @keywords data
+#'
+#' @description A sample data set, used in tests and some examples.
+NULL
+
+
+#' @docType data
+#' @title Sample data set
+#' @name qol_cancer
+#' @keywords data
+#'
+#' @description A sample data set with longitudinal data, used in the vignette describing the \code{demean()} function. Health-related quality of life from cancer-patients was measured at three time points (pre-surgery, 6 and 12 months after surgery).
+#'
+#' @format A data frame with 564 rows and 7 variables:
+#' \describe{
+#'   \item{ID}{Patient ID}
+#'   \item{QoL}{Quality of Life Score}
+#'   \item{time}{Timepoint of measurement}
+#'   \item{age}{Age in years}
+#'   \item{phq4}{Patients' Health Questionnaire, 4-item version}
+#'   \item{hospital}{Hospital ID, where patient was treated}
+#'   \item{education}{Patients' educational level}
+#' }
+NULL
