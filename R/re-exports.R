ab51b0c3c8ab9f8d5d4a60733accd997ba58a1b9
Daniel <mail@danielluedecke.de>
2020-11-20 11:08:29 +0100

    remove non-used re-exports

--- R/re-exports.R
+++ R/re-exports.R
@@ -1,23 +1,3 @@
-#' @importFrom insight format_number
-#' @export
-insight::format_number
-
-#' @importFrom insight format_p
-#' @export
-insight::format_p
-
-#' @importFrom insight format_rope
-#' @export
-insight::format_rope
-
-#' @importFrom insight format_bf
-#' @export
-insight::format_bf
-
-#' @importFrom insight format_pd
-#' @export
-insight::format_pd
-
 #' @importFrom insight parameters_table
 #' @export
 insight::parameters_table
