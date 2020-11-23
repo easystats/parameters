fad90ea0696ec2df3f7696d394cd5c3a20daaf0a
Daniel <mail@danielluedecke.de>
2020-11-23 12:35:05 +0100

    close #346

--- R/model_parameters.BFBayesFactor.R
+++ R/model_parameters.BFBayesFactor.R
@@ -109,7 +109,7 @@ model_parameters.BFBayesFactor <- function(model,
   # ==== remove Component column if not needed
 
   if (.n_unique(out$Component) == 1) out$Component <- NULL
-  if (.n_unique(out$Effects) == 1 || effects == "fixed") out$Effects <- NULL
+  if (.n_unique(out$Effects) == 1) out$Effects <- NULL
 
   attr(out, "ci") <- ci
   attr(out, "object_name") <- deparse(substitute(model), width.cutoff = 500)
