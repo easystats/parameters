64ca5dce31a9c1701c92108608d2f18eb049e8c4
Daniel <mail@danielluedecke.de>
2020-12-01 07:54:46 +0100

    add back some still used reexports

--- R/re-exports.R
+++ R/re-exports.R
@@ -1,3 +1,11 @@
+#' @importFrom insight format_bf
+#' @export
+insight::format_bf
+
+#' @importFrom insight format_pd
+#' @export
+insight::format_pd
+
 #' @importFrom insight parameters_table
 #' @export
 insight::parameters_table
