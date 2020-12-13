914853e3cc3edd126ca70c3d39268f029734a4f8
Daniel <mail@danielluedecke.de>
2020-12-13 14:19:03 +0100

    fix check issues

--- R/p_value.R
+++ R/p_value.R
@@ -14,6 +14,7 @@
 #' @param adjust Character value naming the method used to adjust p-values or confidence intervals. See \code{?emmeans::summary.emmGrid} for details.
 #' @param verbose Toggle warnings and messages.
 #' @param ... Arguments passed down to \code{standard_error_robust()} when confidence intervals or p-values based on robust standard errors should be computed. Only available for models where \code{method = "robust"} is supported.
+#' @inheritParams ci.merMod
 #'
 #' @note \code{p_value_robust()} resp. \code{p_value(method = "robust")}
 #'   rely on the \pkg{sandwich} or \pkg{clubSandwich} package (the latter if
