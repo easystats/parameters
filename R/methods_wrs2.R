647bdc8baf17a4971eaa80c434f63071533b6fa0
Indrajeet Patil <inderonline1988@gmail.com>
2020-12-22 17:13:49 +0100

    method for yuen objects

--- R/methods_wrs2.R
+++ R/methods_wrs2.R
@@ -5,24 +5,23 @@
 #'
 #' @examples
 #' if (require("WRS2")) {
-#'   library(WRS2)
+#'   model1 <- t1way(libido ~ dose, data = viagra)
+#'   model_parameters(model1)
 #'
-#'   set.seed(123)
-#'   model <- t1way(libido ~ dose, data = viagra)
-#'   model_parameters(model)
-#'
-#'   set.seed(123)
-#'   model <- rmanova(WineTasting$Taste, WineTasting$Wine, WineTasting$Taster)
-#'   model_parameters(model)
+#'   model2 <- rmanova(WineTasting$Taste, WineTasting$Wine, WineTasting$Taster)
+#'   model_parameters(model2)
 #' }
 #' @return A data frame of indices related to the model's parameters.
 #' @export
 
 model_parameters.t1way <- function(model, verbose = TRUE, ...) {
-  parameters <- .extract_wrs2_t1way(model, ...)
+  parameters <- .extract_wrs2_t1way(model)
+  parameters <- .add_htest_parameters_attributes(parameters, model, ...)
   class(parameters) <- c("parameters_model", class(parameters))
   parameters
 }
+
+# extract WRS2 anova ----------------------
 
 .extract_wrs2_t1way <- function(model) {
   # effect sizes are by default contained for `t1way` but not `rmanova`
@@ -45,6 +44,64 @@ model_parameters.t1way <- function(model, verbose = TRUE, ...) {
       "df_error" = model$df2,
       "p" = model$p.value,
       "Method" = "A heteroscedastic one-way repeated measures ANOVA for trimmed means",
+      stringsAsFactors = FALSE
+    )
+  }
+}
+
+#' Parameters from \code{WRS2} objects
+#'
+#' @param model Object of class \code{yuen}.
+#' @param ... Arguments passed to or from other methods.
+#'
+#' @examples
+#' if (require("WRS2")) {
+#'   library(WRS2)
+#'
+#'   model1 <- yuen(Anxiety ~ Group, data = spider)
+#'   model_parameters(model1)
+#'
+#'   before <- c(190, 210, 300, 240, 280, 170, 280, 250, 240, 220)
+#'   after <- c(210, 210, 340, 190, 260, 180, 200, 220, 230, 200)
+#'   model2 <- yuend(before, after)
+#'   model_parameters(model2)
+#' }
+#' @return A data frame of indices related to the model's parameters.
+#' @export
+
+model_parameters.yuen <- function(model, verbose = TRUE, ...) {
+  parameters <- .extract_wrs2_yuen(model)
+  parameters <- .add_htest_parameters_attributes(parameters, model, ...)
+  class(parameters) <- c("parameters_model", class(parameters))
+  parameters
+}
+
+
+# extract WRS2 ttest ----------------------
+
+.extract_wrs2_yuen <- function(model) {
+  if ("yuen" %in% as.character(model$call)) {
+    out <- data.frame(
+      "Difference" = model$diff,
+      "Difference_CI_low" =  model$conf.int[1],
+      "Difference_CI_high" =  model$conf.int[2],
+      "t" = model$test,
+      "df_error" = model$df,
+      "p" = model$p.value,
+      "Effsize" = model$effsize,
+      "Method" = "Yuen's test on trimmed means for independent samples",
+      stringsAsFactors = FALSE
+    )
+  } else if ("yuend" %in% as.character(model$call)) {
+    out <- data.frame(
+      "Difference" = model$diff,
+      "Difference_CI_low" =  model$conf.int[1],
+      "Difference_CI_high" =  model$conf.int[2],
+      "t" = model$test,
+      "df_error" = model$df,
+      "p" = model$p.value,
+      "Effsize" = model$effsize,
+      "Method" = "Yuen's test on trimmed means for dependent samples",
       stringsAsFactors = FALSE
     )
   }
--- tests/testthat/test-wrs2.R
+++ tests/testthat/test-wrs2.R
@@ -25,7 +25,13 @@ if (require("testthat") && require("parameters") && require("WRS2")) {
         row.names = c(
           NA,
           -1L
-        )
+        ),
+        title = "A heteroscedastic one-way ANOVA for trimmed means",
+        model_class = "t1way",
+        digits = 2,
+        ci_digits = 2,
+        p_digits = 3,
+        ci = 0.95
       ),
       tolerance = 0.001
     )
@@ -45,7 +51,81 @@ if (require("testthat") && require("parameters") && require("WRS2")) {
         row.names = c(
           NA,
           -1L
-        )
+        ),
+        title = "A heteroscedastic one-way repeated measures ANOVA for trimmed means",
+        model_class = "t1way",
+        digits = 2,
+        ci_digits = 2,
+        p_digits = 3,
+        ci = 0.95
+      ),
+      tolerance = 0.001
+    )
+  })
+
+  test_that("model_parameters.yuen", {
+    set.seed(123)
+    df_b <- as.data.frame(model_parameters(yuen(Anxiety ~ Group, data = spider)))
+
+    before <- c(190, 210, 300, 240, 280, 170, 280, 250, 240, 220)
+    after <- c(210, 210, 340, 190, 260, 180, 200, 220, 230, 200)
+    set.seed(123)
+    df_w <- as.data.frame(model_parameters(yuend(before, after)))
+
+    # between-subjects
+    expect_equal(
+      df_b,
+      structure(
+        list(
+          Difference = -6.75,
+          Difference_CI_low = -17.9293607102093,
+          Difference_CI_high = 4.42936071020934,
+          t = 1.29575716084179,
+          df_error = 13.9137233889032,
+          p = 0.216143324927692,
+          Effsize = 0.378416737055775,
+          Method = "Yuen's test on trimmed means for independent samples"
+        ),
+        class = "data.frame",
+        row.names = c(
+          NA,
+          -1L
+        ),
+        title = "Yuen's test on trimmed means for independent samples",
+        model_class = "yuen",
+        digits = 2,
+        ci_digits = 2,
+        p_digits = 3,
+        ci = 0.95
+      ),
+      tolerance = 0.001
+    )
+
+    # within-subjects
+    expect_equal(
+      df_w,
+      structure(
+        list(
+          Difference = 28.3333333333333,
+          Difference_CI_low = -8.29182014978235,
+          Difference_CI_high = 64.958486816449,
+          t = 1.98861015130686,
+          df_error = 5,
+          p = 0.103433529948121,
+          Effsize = 0.519275279669972,
+          Method = "Yuen's test on trimmed means for dependent samples"
+        ),
+        class = "data.frame",
+        row.names = c(
+          NA,
+          -1L
+        ),
+        title = "Yuen's test on trimmed means for dependent samples",
+        model_class = "yuen",
+        digits = 2,
+        ci_digits = 2,
+        p_digits = 3,
+        ci = 0.95
       ),
       tolerance = 0.001
     )
