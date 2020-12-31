6f085a1ef40b737d7cdd16c6f41c5d2526d02234
Indrajeet Patil <inderonline1988@gmail.com>
2020-12-31 09:38:44 +0100

    adds support for PMCMR objects; #321

---
+++ R/methods_PMCMRplus.R
@@ -0,0 +1,41 @@
+#' Parameters from hypothesis tests from \code{PMCMRplus}
+#'
+#' @param model Object of class \code{PMCMR} or\code{trendPMCMR} or \code{osrt}.
+#' @param ... Arguments passed to or from other methods.
+#'
+#' @examples
+#' if (require("PMCMRplus")) {
+#'   model <- kwAllPairsConoverTest(count ~ spray, data = InsectSprays)
+#'   model_parameters(model)
+#' }
+#' @return A data frame of indices related to the model's parameters.
+#' @export
+
+model_parameters.PMCMR <- function(model, ...) {
+  if (!requireNamespace("PMCMRplus", quietly = TRUE)) {
+    stop("Package 'PMCMRplus' needed. Please install it by running `install.packages('PMCMRplus')`.")
+  }
+
+  parameters <- PMCMRplus::toTidy(model)
+  parameters <- standardize_names(parameters)
+
+  names(parameters) <- c(
+    "Group1", "Group2", "Statistic", "p", "alternative", "Method",
+    "Distribution", "p.adjust.method"
+  )
+
+  # TO DO: this is currently commented because it seems to print "Method" name
+  # multiple times and I am not sure why
+  #parameters <- .add_htest_parameters_attributes(parameters, model)
+
+  class(parameters) <- c("parameters_model", "see_parameters_model", class(parameters))
+  parameters
+}
+
+#' @export
+
+model_parameters.osrt <- model_parameters.PMCMR
+
+#' @export
+
+model_parameters.trendPMCMR <- model_parameters.PMCMR
