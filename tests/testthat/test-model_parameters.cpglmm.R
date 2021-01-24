12c7f16968ec7dee0727e3c99cb859a234276c8d
Daniel <mail@danielluedecke.de>
2021-01-24 14:56:16 +0100

    Update test-model_parameters.cpglmm.R

--- tests/testthat/test-model_parameters.cpglmm.R
+++ tests/testthat/test-model_parameters.cpglmm.R
@@ -7,7 +7,7 @@ if (require("testthat") && require("insight") && require("parameters") && requir
     testthat::expect_equal(params$SE, c(0.1308, 0.2514, 0.2, 0.1921), tolerance = 1e-3)
     testthat::expect_equal(
       colnames(params),
-      c("Parameter", "Coefficient", "SE", "CI_low", "CI_high", "t", "df_error", "p")
+      c("Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "t", "df_error", "p")
     )
   })
 }
