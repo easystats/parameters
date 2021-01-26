cb7d104508464105fe5caec2445a03e58ce3bc99
Daniel <mail@danielluedecke.de>
2021-01-26 11:49:23 +0100

    Update test-parameters_table.R

--- tests/testthat/test-parameters_table.R
+++ tests/testthat/test-parameters_table.R
@@ -2,13 +2,13 @@ if (require("testthat") && require("parameters") && require("insight") && requir
   test_that("parameters_table 1", {
     x <- model_parameters(lm(Sepal.Length ~ Species, data = iris), standardize = "refit")
     tab <- format_table(x)
-    expect_true(all(names(tab) == c("Parameter", "Coefficient", "SE", "CI", "95% CI", "t(147)", "p")))
+    expect_true(all(names(tab) == c("Parameter", "Coefficient", "SE", "95% CI", "t(147)", "p")))
   })
 
   test_that("parameters_table 2", {
     x <- model_parameters(lme4::lmer(Sepal.Length ~ Petal.Length + (1 | Species), data = iris))
     tab <- format_table(x)
-    expect_true(all(names(tab) == c("Parameter", "Coefficient", "SE", "CI", "95% CI", "t(146)", "p")))
+    expect_true(all(names(tab) == c("Parameter", "Coefficient", "SE", "95% CI", "t(146)", "p")))
   })
 
   test_that("parameters_table 3", {
@@ -20,7 +20,7 @@ if (require("testthat") && require("parameters") && require("insight") && requir
   test_that("parameters_table 4", {
     x <- model_parameters(lm(Sepal.Length ~ Species, data = iris), standardize = "posthoc")
     tab <- format_table(x)
-    expect_true(all(names(tab) == c("Parameter", "Std. Coef.", "SE", "CI", "95% CI", "t(147)", "p")))
+    expect_true(all(names(tab) == c("Parameter", "Std. Coef.", "SE", "95% CI", "t(147)", "p")))
   })
 
   # x <- report::report_table(lm(Sepal.Length ~ Species, data=iris))  # Once on CRAN
