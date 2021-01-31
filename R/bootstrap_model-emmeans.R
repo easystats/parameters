e234f223da092ce5423ba43cc51f42b9bf93abd2
mattansb <35330040+mattansb@users.noreply.github.com>
2021-01-31 12:38:36 +0200

    fix typo

--- R/bootstrap_model-emmeans.R
+++ R/bootstrap_model-emmeans.R
@@ -1,7 +1,7 @@
 #' @keywords emmeans_methods
 emm_basis.bootstrap_model <- function(object, trms, xlev, grid, ...) {
   if (!requireNamespace("emmeans", quietly = TRUE))
-    stop("Thin function requires the 'emmeans' package to work.")
+    stop("this function requires the 'emmeans' package to work.")
 
   model <- attr(object, "original_model")
   emb <- emmeans::emm_basis(model, trms, xlev, grid, ...)
@@ -16,7 +16,7 @@ emm_basis.bootstrap_model <- function(object, trms, xlev, grid, ...) {
 #' @keywords emmeans_methods
 recover_data.bootstrap_model <- function(object, ...) {
   if (!requireNamespace("emmeans", quietly = TRUE))
-    stop("Thin function requires the 'emmeans' package to work.")
+    stop("this function requires the 'emmeans' package to work.")
 
   model <- attr(object, "original_model")
   emmeans::recover_data(model, ...)
@@ -26,7 +26,7 @@ recover_data.bootstrap_model <- function(object, ...) {
 #' @keywords emmeans_methods
 emm_basis.bootstrap_parameters <- function(object, trms, xlev, grid, ...) {
   if (!requireNamespace("emmeans", quietly = TRUE))
-    stop("Thin function requires the 'emmeans' package to work.")
+    stop("this function requires the 'emmeans' package to work.")
 
   model <- attr(object, "boot_samples")
   emmeans::emm_basis(model, trms, xlev, grid, ...)
@@ -35,7 +35,7 @@ emm_basis.bootstrap_parameters <- function(object, trms, xlev, grid, ...) {
 #' @keywords emmeans_methods
 recover_data.bootstrap_parameters <- function(object, ...) {
   if (!requireNamespace("emmeans", quietly = TRUE))
-    stop("Thin function requires the 'emmeans' package to work.")
+    stop("this function requires the 'emmeans' package to work.")
 
   model <- attr(object, "boot_samples")
   emmeans::recover_data(model, ...)
