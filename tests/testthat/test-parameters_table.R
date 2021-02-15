92da5a3ebeecdeed77557ae00679ae83310861c0
Daniel <mail@danielluedecke.de>
2021-02-15 18:35:40 +0100

    Update test-parameters_table.R

--- tests/testthat/test-parameters_table.R
+++ tests/testthat/test-parameters_table.R
@@ -2,7 +2,7 @@ if (require("testthat") && require("parameters") && require("insight") && requir
   test_that("parameters_table 1", {
     x <- model_parameters(lm(Sepal.Length ~ Species, data = iris), standardize = "refit")
     tab <- format_table(x)
-    expect_true(all(names(tab) == c("Parameter", "Coefficient", "SE", "95% CI", "t(147)", "p")))
+    expect_equal(colnames(tab), c("Parameter", "Coefficient", "SE", "95% CI", "t(147)", "p"))
   })
 
   test_that("parameters_table 2", {
@@ -14,13 +14,13 @@ if (require("testthat") && require("parameters") && require("insight") && requir
   test_that("parameters_table 3", {
     x <- effectsize::effectsize(lm(Sepal.Length ~ Species, data = iris))
     tab <- format_table(x)
-    expect_true(all(names(tab) == c("Parameter", "Std. Coef.", "95% CI")))
+    expect_equal(colnames(tab), c("Parameter", "Std. Coef.", "95% CI"))
   })
 
   test_that("parameters_table 4", {
     x <- model_parameters(lm(Sepal.Length ~ Species, data = iris), standardize = "posthoc")
     tab <- format_table(x)
-    expect_true(all(names(tab) == c("Parameter", "Std. Coef.", "SE", "95% CI", "t(147)", "p")))
+    expect_equal(colnames(tab), c("Parameter", "Std. Coef.", "SE", "95% CI", "t(147)", "p"))
   })
 
   # x <- report::report_table(lm(Sepal.Length ~ Species, data=iris))  # Once on CRAN
