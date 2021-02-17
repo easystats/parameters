adfca3d3660a8c8bf61dbdc267d4275b2b944d15
Daniel <mail@danielluedecke.de>
2021-02-17 20:04:20 +0100

    Update test-rstanarm.R

--- tests/testthat/test-rstanarm.R
+++ tests/testthat/test-rstanarm.R
@@ -1,13 +1,18 @@
 .runThisTest <- Sys.getenv("RunAllparametersTests") == "yes"
 
-osx <- tryCatch({
-  si <- Sys.info()
-  if (!is.null(si["sysname"])) {
-    si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
-  } else {
+osx <- tryCatch(
+  {
+    si <- Sys.info()
+    if (!is.null(si["sysname"])) {
+      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+    } else {
+      FALSE
+    }
+  },
+  error = function(e) {
     FALSE
   }
-})
+)
 
 if (.runThisTest && !osx && require("testthat") && require("parameters") && require("rstanarm")) {
   data(mtcars)
