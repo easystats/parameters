b4a6a70233b6279c4b76544aa3ec644a15c8fcc7
Daniel <mail@danielluedecke.de>
2021-02-27 21:10:37 +0100

    Update testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -5,4 +5,22 @@ if (length(strsplit(packageDescription("parameters")$Version, "\\.")[[1]]) > 3) 
 } else {
   Sys.setenv("RunAllparametersTests" = "no")
 }
-test_check("parameters")
+
+osx <- tryCatch(
+  {
+    si <- Sys.info()
+    if (!is.null(si["sysname"])) {
+      si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
+    } else {
+      FALSE
+    }
+  },
+  error = function(e) {
+    FALSE
+  }
+)
+
+if (!osx) {
+  test_check("parameters")
+}
+
