7056a0e37a2820e97262c0f1e8c62c924af36224
Daniel <mail@danielluedecke.de>
2021-03-05 11:10:22 +0100

    Update methods_panelr.R

--- R/methods_panelr.R
+++ R/methods_panelr.R
@@ -7,6 +7,8 @@
 #' @export
 model_parameters.wbm <- function(model,
                                  ci = .95,
+                                 effects = "fixed",
+                                 group_level = FALSE,
                                  bootstrap = FALSE,
                                  iterations = 1000,
                                  exponentiate = FALSE,
@@ -14,26 +16,64 @@ model_parameters.wbm <- function(model,
                                  p_adjust = NULL,
                                  verbose = TRUE,
                                  ...) {
-  out <- .model_parameters_generic(
-    model = model,
-    ci = ci,
-    bootstrap = bootstrap,
-    iterations = iterations,
-    merge_by = c("Parameter", "Component"),
-    standardize = NULL,
-    exponentiate = exponentiate,
-    robust = FALSE,
-    p_adjust = p_adjust,
-    ...
-  )
 
-  attr(out, "object_name") <- deparse(substitute(model), width.cutoff = 500)
+  effects <- match.arg(effects, choices = c("fixed", "random", "all"))
+  params <- params_random <- params_variance <- att <- NULL
 
-  if (isTRUE(details)) {
-    attr(out, "details") <- .randomeffects_summary(model)
+  if (effects %in% c("fixed", "all")) {
+    params <- .model_parameters_generic(
+      model = model,
+      ci = ci,
+      bootstrap = bootstrap,
+      iterations = iterations,
+      merge_by = c("Parameter", "Component"),
+      standardize = NULL,
+      exponentiate = exponentiate,
+      robust = FALSE,
+      p_adjust = p_adjust,
+      ...
+    )
+    params$Effects <- "fixed"
+    att <- attributes(params)
   }
 
-  out
+  if (effects %in% c("random", "all") && isTRUE(group_level)) {
+    params_random <- .extract_random_parameters(model, ci = ci, effects = effects)
+  }
+
+  if (effects %in% c("random", "all") && isFALSE(group_level)) {
+    params_variance <- .extract_random_variances(model, ci = ci, effects = effects)
+  }
+
+
+  # merge random and fixed effects, if necessary
+  if (!is.null(params) && (!is.null(params_random) || !is.null(params_variance))) {
+    params$Level <- NA
+    params$Group <- ""
+
+    # reorder
+    if (!is.null(params_random)) {
+      params <- params[match(colnames(params_random), colnames(params))]
+    } else {
+      params <- params[match(colnames(params_variance), colnames(params))]
+    }
+  }
+
+  params <- rbind(params, params_random, params_variance)
+  if (!is.null(att)) {
+    attributes(params) <- utils::modifyList(att, attributes(params))
+  }
+
+  attr(params, "object_name") <- deparse(substitute(model), width.cutoff = 500)
+
+  if (isTRUE(details)) {
+    attr(params, "details") <- .randomeffects_summary(model)
+    if (verbose) {
+      message("Argument 'details' is deprecated. Please use 'group_level'.")
+    }
+  }
+
+  params
 }
 
 
