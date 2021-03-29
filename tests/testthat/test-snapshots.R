84046617a52175439b6fecf38250ca87b13c06c7
Indrajeet Patil <inderonline1988@gmail.com>
2021-03-29 15:40:41 +0200

    even stricter

--- tests/testthat/test-snapshots.R
+++ tests/testthat/test-snapshots.R
@@ -1,4 +1,8 @@
 if (require("testthat") && require("parameters")) {
+
+  test_that("model_parameters output as expected", {
   set.seed(123)
-  expect_snapshot(parameters(lm(formula = wt ~ am * cyl, data = mtcars)))
+  mod <- lm(formula = wt ~ am * cyl, data = mtcars)
+  expect_snapshot(as.data.frame(parameters(mod)))
+  })
 }
