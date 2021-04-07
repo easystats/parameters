efb3f7f6464ecc25062cf459f4edaa4a443cec5c
Daniel <mail@danielluedecke.de>
2021-04-07 11:40:52 +0200

    Delete test-snapshot.R

--- tests/testthat/test-snapshot.R
+++
@@ -1,6 +0,0 @@
-test_that("model_parameters output as expected", {
-  skip_if(getRversion() < "3.5")
-  set.seed(123)
-  mod <- lm(formula = wt ~ am * cyl, data = mtcars)
-  expect_snapshot(as.data.frame(parameters(mod)))
-})
