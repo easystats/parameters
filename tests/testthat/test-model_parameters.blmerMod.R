a09c0562a05bad252a24bd290202901c2d585ed5
Daniel <mail@danielluedecke.de>
2021-06-15 16:06:57 +0200

    Update test-model_parameters.blmerMod.R

--- tests/testthat/test-model_parameters.blmerMod.R
+++ tests/testthat/test-model_parameters.blmerMod.R
@@ -15,7 +15,7 @@ if (require("testthat") && require("parameters") && require("blme")) {
   test_that("model_parameters.blmerMod-all", {
     params <- model_parameters(model, effects = "all")
     expect_equal(params$SE, c(6.8246, 1.54579, NA, NA, NA, NA), tolerance = 1e-3)
-    expect_equal(params$Coefficient, c(251.4051, 10.46729, 24.74066, 5.92214, 0.06555, 5.05883), tolerance = 1e-3)
+    expect_equal(params$Coefficient, c(251.4051, 10.46729, 24.74066, 5.92214, 0.06555, 25.5918), tolerance = 1e-3)
     expect_equal(
       colnames(params),
       c("Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "t", "df_error", "p", "Effects", "Group")
