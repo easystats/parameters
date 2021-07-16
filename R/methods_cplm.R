5fa90600422cf0fd951abeafdc1c76c9842c9012
Daniel <mail@danielluedecke.de>
2021-07-16 11:06:33 +0200

    add residual option

--- R/methods_cplm.R
+++ R/methods_cplm.R
@@ -261,9 +261,11 @@ model_parameters.cpglmm <- function(model,
 #' @rdname p_value.lmerMod
 #' @export
 p_value.cpglmm <- function(model, method = "wald", ...) {
-  method <- match.arg(tolower(method), c("wald", "betwithin", "ml1"))
+  method <- match.arg(tolower(method), c("wald", "betwithin", "ml1", "residual"))
   if (method == "wald") {
     dof <- Inf
+  } else if (method == "residual") {
+    dof <- degrees_of_freedom(model, method = "residual")
   } else if (method == "ml1") {
     dof <- dof_ml1(model)
   } else {
