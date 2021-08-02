14905a11531a161d5c3e02f893bd38aee23b2e3d
Daniel <mail@danielluedecke.de>
2021-08-02 08:48:36 +0200

    Update methods_systemfit.R

--- R/methods_systemfit.R
+++ R/methods_systemfit.R
@@ -1,46 +1,45 @@
-#' @title Parameters from Linear Equation System Estimation Models
-#' @name model_parameters.systemfit
-#'
-#' @description
-#' Parameters from Linear Equation System Estimation models (from
-#' \pkg{systemfit} package).
-#'
-#' @param model A model from \pkg{systemfit} package.
-#' @inheritParams model_parameters.default
 #' @export
+standard_error.systemfit <- function(model, ...) {
+  cf <- stats::coef(summary(model))
+  f <- insight::find_formula(model)
 
-model_parameters.systemfit <- function(model, ci = 0.95, verbose = TRUE, ...) {
-  if (!missing(ci)) {
-    if (isTRUE(verbose)) {
-      message(insight::format_message("'systemfit' models do not support other levels for confidence intervals than 0.95. Argument 'ci' is ignored."))
-    }
-    ci <- 0.95
-  }
+  system_names <- names(f)
+  parameter_names <- row.names(cf)
 
-  # estimates
-  params <- as.data.frame(summary(model)$coefficients)
-  params$Parameter <- c(attributes(params)$row.names)
-  colnames(params) <- c("Coefficient", "SE", "t", "p", "Parameter")
-  rownames(params) <- NULL
+  out <- lapply(system_names, function(i) {
+    pattern <- paste0("^", i, "_(.*)")
+    params <- grepl(pattern, parameter_names)
+    data.frame(
+      Parameter = gsub(pattern, "\\1", parameter_names[params]),
+      SE = as.vector(cf[params, 2]),
+      Component = i,
+      stringsAsFactors = FALSE
+    )
+  })
 
-  # confidence intervals
-  ci_data <- as.data.frame(unclass(stats::confint(model, level = ci)))
-  colnames(ci_data) <- c("CI_low", "CI_high")
-  ci_data$Parameter <- c(attributes(ci_data)$row.names)
-  rownames(ci_data) <- NULL
+  do.call(rbind, out)
+}
 
-  # merge estimates with CIs
-  params <- merge(params, ci_data, by = "Parameter", all.x = TRUE)
 
-  # add CI level column
-  params$CI <- ci
 
-  # reorder columns
-  col_order <- c("Parameter", "Coefficient", "CI", "CI_low", "CI_high", "SE", "t", "p")
-  params <- params[col_order[col_order %in% names(params)]]
+#' @export
+p_value.systemfit <- function(model, ...) {
+  cf <- stats::coef(summary(model))
+  f <- insight::find_formula(model)
 
-  # add attributes
-  class(params) <- c("parameters_model", "see_parameters_model", class(params))
+  system_names <- names(f)
+  parameter_names <- row.names(cf)
 
-  params
+  out <- lapply(system_names, function(i) {
+    pattern <- paste0("^", i, "_(.*)")
+    params <- grepl(pattern, parameter_names)
+    data.frame(
+      Parameter = gsub(pattern, "\\1", parameter_names[params]),
+      p = as.vector(cf[params, 4]),
+      Component = i,
+      stringsAsFactors = FALSE
+    )
+  })
+
+  do.call(rbind, out)
 }
