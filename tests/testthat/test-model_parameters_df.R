9e9fd7e2b9d046596c5ae6f15432e5d4ac1f0e6d
Daniel <mail@danielluedecke.de>
2021-10-17 12:06:03 +0200

    test

--- tests/testthat/test-model_parameters_df.R
+++ tests/testthat/test-model_parameters_df.R
@@ -110,5 +110,34 @@ if (.runThisTest && requiet("testthat") && requiet("parameters")) {
                                0.00794, 0.14296, 0.36269, 0.6949), tolerance = 1e-3)
     })
   }
+
+
+
+
+
+  # ivprobit ---------------------------
+
+  if (requiet("ivprobit")) {
+    set.seed(123)
+    data(eco)
+
+    # model
+    model <- ivprobit::ivprobit(
+      formula = d2 ~ ltass + roe + div | eqrat + bonus | ltass + roe + div + gap + cfa,
+      data = eco
+    )
+
+    test_that("model_parameters.ivprobit", {
+      params <- suppressWarnings(model_parameters(model))
+      expect_equal(params$df_error, c(789L, 789L, 789L, 789L, 789L, 789L), tolerance = 1e-3)
+      expect_equal(params$CI_low, c(-35.96484, -0.27082, -0.15557, -1e-05, -15.95755, -1e-05), tolerance = 1e-3)
+      expect_equal(params$p, c(0.08355, 0.12724, 0.55684, 0.63368, 0.46593, 0.61493), tolerance = 1e-3)
+
+      params <- suppressWarnings(model_parameters(model, ci_method = "normal"))
+      expect_equal(params$df_error, c(Inf, Inf, Inf, Inf, Inf, Inf), tolerance = 1e-3)
+      expect_equal(params$CI_low, c(-35.93553, -0.26895, -0.15522, -1e-05, -15.91859, -1e-05), tolerance = 1e-3)
+      expect_equal(params$p, c(0.08355, 0.12724, 0.55684, 0.63368, 0.46593, 0.61493), tolerance = 1e-3)
+    })
+  }
 }
 
