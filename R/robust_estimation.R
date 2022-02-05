0095584f15cd1fad0a188aee8d8293af15ed9dd5
Daniel <mail@danielluedecke.de>
2022-02-05 15:07:44 +0100

    https://github.com/easystats/insight/issues/505

--- R/robust_estimation.R
+++ R/robust_estimation.R
@@ -184,11 +184,19 @@ ci_robust <- function(model,
   # check if required package is available
   if (vcov_fun == "vcovCR") {
     insight::check_if_installed("clubSandwich", reason = "to get cluster-robust standard errors")
-    .vcov <- do.call(clubSandwich::vcovCR, c(list(obj = x, type = vcov_type), vcov_args))
+    if (is.null(vcov_type)) {
+      .vcov <- do.call(clubSandwich::vcovCR, c(list(obj = x), vcov_args))
+    } else {
+      .vcov <- do.call(clubSandwich::vcovCR, c(list(obj = x, type = vcov_type), vcov_args))
+    }
   } else {
     insight::check_if_installed("sandwich", reason = "to get robust standard errors")
     vcov_fun <- get(vcov_fun, asNamespace("sandwich"))
-    .vcov <- do.call(vcov_fun, c(list(x = x, type = vcov_type), vcov_args))
+    if (is.null(vcov_type)) {
+      .vcov <- do.call(vcov_fun, c(list(x = x), vcov_args))
+    } else {
+      .vcov <- do.call(vcov_fun, c(list(x = x, type = vcov_type), vcov_args))
+    }
   }
 
   # get coefficients
