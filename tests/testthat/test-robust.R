371438ef3462c594a7b92fc265967b29a7fca657
Daniel <mail@danielluedecke.de>
2022-03-07 12:52:15 +0100

    Update test-robust.R

--- tests/testthat/test-robust.R
+++ tests/testthat/test-robust.R
@@ -5,6 +5,7 @@ if (.runThisTest &&
     requiet("parameters") &&
     requiet("MASS") &&
     requiet("pscl") &&
+    requiet("survial") &&
     requiet("sandwich")) {
 
   data(iris)
@@ -20,13 +21,46 @@ if (.runThisTest &&
   m <- polr(Sat ~ Infl + Type + Cont, weights = Freq, data = housing)
 
   test_that("robust-se polr", {
-    # MASS::polr doesn't work with sandwich?
+    se1 <- standard_error(m, vcov = "vcovCL")
+    se2 <- sqrt(diag(sandwich::vcovCL(m)))
+    expect_equal(se1$SE, se2, ignore_attr = TRUE)
+
+    se1 <- standard_error(m, vcov = "vcovOPG")
+    se2 <- sqrt(diag(sandwich::vcovOPG(m)))
+    expect_equal(se1$SE, se2, ignore_attr = TRUE)
   })
 
   data("bioChemists")
   m <- zeroinfl(art ~ fem + mar + kid5 + ment | kid5 + phd, data = bioChemists)
 
   test_that("robust-se zeroinfl", {
-    # pscl::zeroinfl doesn't work with sandwich?
+    se1 <- standard_error(m, vcov = "vcovCL")
+    se2 <- sqrt(diag(sandwich::vcovCL(m)))
+    expect_equal(se1$SE, se2, ignore_attr = TRUE)
+
+    set.seed(123)
+    se1 <- standard_error(m, vcov = "vcovBS", vcov_args = list(R = 20))
+    set.seed(123)
+    se2 <- sqrt(diag(sandwich::vcovBS(m, R = 20)))
+    expect_equal(se1$SE, se2, ignore_attr = TRUE)
+
+    se1 <- standard_error(m, vcov = "vcovOPG")
+    se2 <- sqrt(diag(sandwich::vcovOPG(m)))
+    expect_equal(se1$SE, se2, ignore_attr = TRUE)
+  })
+
+  set.seed(123)
+  m <- survreg(
+    formula = Surv(futime, fustat) ~ ecog.ps + rx,
+    data = ovarian,
+    dist = "logistic"
+  )
+
+  test_that("robust-se survival", {
+    set.seed(123)
+    se1 <- standard_error(m, vcov = "vcovBS")
+    set.seed(123)
+    se2 <- sqrt(diag(sandwich::vcovBS(m)))
+    expect_equal(se1$SE, se2, ignore_attr = TRUE)
   })
 }
