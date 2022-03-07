f8e194dfe4d1a78b12b8ca105e65ecd4ced6b406
Daniel <mail@danielluedecke.de>
2022-03-07 10:02:24 +0100

    Update test-zeroinfl.R

--- tests/testthat/test-zeroinfl.R
+++ tests/testthat/test-zeroinfl.R
@@ -29,6 +29,8 @@ if (requiet("testthat") &&
     expect_s3_class(p_value(m1, method = "robust"), "data.frame")
     expect_s3_class(p_value(m1, method = "robust", vcov = NULL), "data.frame")
     expect_s3_class(p_value(m1, vcov = NULL), "data.frame")
+    expect_s3_class(p_value(m1, vcov = "HC"), "data.frame")
+    expect_s3_class(p_value(m1, method = "robust", vcov = "HC"), "data.frame")
   })
 
   test_that("model_parameters", {
