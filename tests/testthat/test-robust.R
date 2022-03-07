33380dc9af2dab6e6f23c3c1bb089f8d2da67e88
Daniel <mail@danielluedecke.de>
2022-03-07 16:05:31 +0100

    fix tests

--- tests/testthat/test-robust.R
+++ tests/testthat/test-robust.R
@@ -392,12 +392,58 @@ if (.runThisTest &&
     dof <- degrees_of_freedom(m, method = "wald", verbose = FALSE)
     fac <- suppressWarnings(stats::qt(.975, df = dof))
     ci2 <- as.data.frame(cbind(
-      CI_low = coef(m) - se * fac,
-      CI_high = coef(m) + se * fac
+      CI_low = insight::get_parameters(m)$Estimate - se * fac,
+      CI_high = insight::get_parameters(m)$Estimate + se * fac
     ))
     expect_equal(ci1$CI_low, ci2$CI_low, tolerance = 1e-4, ignore_attr = TRUE)
     expect_equal(ci1$CI_high, ci2$CI_high, tolerance = 1e-4, ignore_attr = TRUE)
   })
 
+
+
+
+  # mixed models ----------------------
+
+  if (requiet("clubSandwich") && requiet("lme4")) {
+    data(iris)
+    set.seed(1234)
+    iris$grp <- as.factor(sample(1:3, nrow(iris), replace = TRUE))
+
+    m <- lme4::lmer(
+      Sepal.Length ~ Species * Sepal.Width + Petal.Length + (1 | grp),
+      data = iris
+    )
+
+    test_that("robust-se lmer", {
+      se1 <- standard_error(m, vcov = "vcovCR", vcov_args = list(type = "CR1", cluster = iris$grp))
+      se2 <- sqrt(diag(clubSandwich::vcovCR(m, type = "CR1", cluster = iris$grp)))
+      expect_equal(se1$SE, se2, tolerance = 1e-4, ignore_attr = TRUE)
+    })
+
+    test_that("robust-p lmer", {
+      p1 <- p_value(m, vcov = "vcovCR", vcov_args = list(type = "CR1", cluster = iris$grp))
+      se <- sqrt(diag(clubSandwich::vcovCR(m, type = "CR1", cluster = iris$grp)))
+      dof <- degrees_of_freedom(m, method = "wald", verbose = FALSE)
+      stat <- fixef(m) / se
+      p2 <- 2 * pt(abs(stat), df = dof, lower.tail = FALSE)
+      expect_equal(p1$p, p2, tolerance = 1e-4, ignore_attr = TRUE)
+    })
+
+    test_that("robust-ci lmer", {
+      ci1 <- ci(m, vcov = "vcovCR", vcov_args = list(type = "CR1", cluster = iris$grp))
+      # robust CI manually
+      params <- insight::get_parameters(m)
+      se <- sqrt(diag(clubSandwich::vcovCR(m, type = "CR1", cluster = iris$grp)))
+      dof <- degrees_of_freedom(m, method = "wald", verbose = FALSE)
+      fac <- suppressWarnings(stats::qt(.975, df = dof))
+      ci2 <- as.data.frame(cbind(
+        CI_low = params$Estimate - se * fac,
+        CI_high = params$Estimate + se * fac
+      ))
+      expect_equal(ci1$CI_low, ci2$CI_low, tolerance = 1e-4, ignore_attr = TRUE)
+      expect_equal(ci1$CI_high, ci2$CI_high, tolerance = 1e-4, ignore_attr = TRUE)
+    })
+
+  }
 }
 
