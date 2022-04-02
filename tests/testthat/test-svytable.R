5b77c2532c3334f9676222cc08a502be30b41f7b
Daniel <mail@danielluedecke.de>
2022-04-02 23:44:01 +0200

    Create test-svytable.R

---
+++ tests/testthat/test-svytable.R
@@ -0,0 +1,13 @@
+if (requiet("testthat") &&
+  requiet("parameters") &&
+  requiet("survey")) {
+
+  data(api)
+  dclus1 <- svydesign(id = ~dnum, weights = ~pw, data = apiclus1, fpc = ~fpc)
+  m <- svytable(~ sch.wide + stype, dclus1)
+  mp <- model_parameters(m)
+  test_that("model_parameters", {
+    expect_equal(colnames(mp), c("F", "df", "df_error", "p", "Method"))
+    expect_equal(mp$p, 0.02174746, tolerance = 1e-3)
+  })
+}
