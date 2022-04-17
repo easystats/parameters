cdb93e0546ec18ab71f3caa177992afefd742f8d
Indrajeet Patil <patilindrajeet.science@gmail.com>
2022-04-17 09:44:40 +0200

    fix docs

--- R/sort_parameters.R
+++ R/sort_parameters.R
@@ -1,13 +1,7 @@
 #' Sort parameters by coefficient values
 #'
 #' @param x A data frame or a `parameters_model` object.
-#' @param sort If `"none"` (default) do not sort, `"ascending"` sort by
-#'   increasing coefficient value, or `"descending"` sort by decreasing
-#'   coefficient value.
-#' @param style Naming conventions the given object follows. This can either be
-#'   based on the naming conventions from the
-#'   [easystats-project](https://easystats.github.io/easystats/), or on
-#'   \pkg{broom}'s naming conventions.
+#' @param ... Arguments passed to or from other methods.
 #'
 #' @examples
 #' # creating object to sort (can also be a regular data frame)
@@ -28,6 +22,15 @@ sort_parameters <- function(x, ...) {
 }
 
 #' @rdname sort_parameters
+#'
+#' @param sort If `"none"` (default) do not sort, `"ascending"` sort by
+#'   increasing coefficient value, or `"descending"` sort by decreasing
+#'   coefficient value.
+#' @param style Naming conventions the given object follows. This can either be
+#'   based on the naming conventions from the
+#'   [easystats-project](https://easystats.github.io/easystats/), or on
+#'   \pkg{broom}'s naming conventions.
+#'
 #' @export
 sort_parameters.default <- function(x, sort = "none", style = "easystats") {
   sort <- match.arg(tolower(sort), choices = c("none", "ascending", "descending"))
