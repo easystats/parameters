fade7f608757b881eb090e80a58eeec31a5c902b
Daniel <mail@danielluedecke.de>
2022-06-07 21:38:31 +0200

    remove old code, acticate new workhorse

--- R/extract_random_variances.R
+++ R/extract_random_variances.R
@@ -119,7 +119,9 @@
                                              verbose = FALSE,
                                              ...) {
   varcorr <- .get_variance_information(model, component)
-  class(varcorr) <- "VarCorr.merMod"
+  if (!inherits(model, "lme")) {
+    class(varcorr) <- "VarCorr.merMod"
+  }
 
   # return varcorr matrix
   re_data <- as.data.frame(varcorr, order = "lower.tri")
@@ -225,199 +227,58 @@
 
 
 
+#' @export
+as.data.frame.VarCorr.lme <- function(x, row.names = NULL, optional = FALSE, ...) {
+  stddevs <- sapply(x[, "StdDev"], as.numeric)
+  corrs <- suppressWarnings(sapply(x[, "Corr"], as.numeric))
+  grps <- grepl(" =$", names(stddevs))
 
-# old workhorse ------------------------------------
-
-# this used to be the old way to extract random effect variances,
-# which might be less stable than just using "as.data.frame(VarCorr())".
-# we still might need this for nlme::lme
-
-.extract_random_variances_other <- function(model,
-                                            ci = .95,
-                                            effects = "random",
-                                            component = "conditional",
-                                            ci_method = NULL,
-                                            verbose = FALSE,
-                                            ...) {
-
-  varcorr <- .get_variance_information(model, component)
-
-  ran_intercept <- tryCatch(data.frame(.random_intercept_variance(varcorr)),
-                            error = function(e) NULL)
-
-  ran_slope <- tryCatch(data.frame(.random_slope_variance(model, varcorr)),
-                        error = function(e) NULL)
-
-  ran_corr <- tryCatch(data.frame(.random_slope_intercept_corr(model, varcorr)),
-                       error = function(e) NULL)
-
-  ran_slopes_corr <- tryCatch(data.frame(.random_slopes_corr(model, varcorr)),
-                              error = function(e) NULL)
-
-
-  # sigma/dispersion only once,
-  if (component == "conditional") {
-    ran_sigma <- data.frame(insight::get_sigma(model, ci = NULL, verbose = FALSE))
+  if (any(grps)) {
+    from <- which(grps)
+    to <- c(which(grps) - 1, length(grps))[-1]
+    out_sd <- do.call(rbind, lapply(1:length(from), function(i) {
+      values <- stddevs[from[i]:to[i]]
+      data.frame(
+        grp = gsub("(.*) =$", "\\1", names(values[1])),
+        var1 = names(values[-1]),
+        var2 = NA_character_,
+        sdcor = unname(values[-1]),
+        stringsAsFactors = FALSE
+      )
+    }))
+    out_cor <- do.call(rbind, lapply(1:length(from), function(i) {
+      values <- corrs[from[i]:to[i]]
+      data.frame(
+        grp = gsub("(.*) =$", "\\1", names(values[1])),
+        var1 = "(Intercept)",
+        var2 = names(values[-1]),
+        sdcor = unname(values[-1]),
+        stringsAsFactors = FALSE
+      )
+    }))
   } else {
-    ran_sigma <- NULL
+    out_sd <- data.frame(
+      grp = gsub("(.*) =(.*)", "\\1", attributes(x)$title),
+      var1 = names(stddevs),
+      var2 = NA_character_,
+      sdcor = unname(stddevs),
+      stringsAsFactors = FALSE
+    )
+    out_cor <- data.frame(
+      grp = gsub("(.*) =(.*)", "\\1", attributes(x)$title),
+      var1 = "(Intercept)",
+      var2 = names(corrs),
+      sdcor = unname(corrs),
+      stringsAsFactors = FALSE
+    )
   }
 
+  out_sd$grp[out_sd$var1 == "Residual"] <- "Residual"
+  out_sd$var1[out_sd$grp == "Residual"] <- NA_character_
+  out_sd$var2[out_sd$grp == "Residual"] <- NA_character_
+  out_cor <- out_cor[!is.na(out_cor$sdcor), ]
 
-  # random intercept - tau00
-  if (!is.null(ran_intercept) && nrow(ran_intercept) > 0) {
-    colnames(ran_intercept) <- "Coefficient"
-    ran_intercept$Group <- rownames(ran_intercept)
-    ran_intercept$Parameter <- "SD (Intercept)"
-  }
-  ran_groups_int <- ran_intercept$Group
-
-
-  # random slope - tau11
-  if (!is.null(ran_slope) && nrow(ran_slope) > 0) {
-    colnames(ran_slope) <- "Coefficient"
-    ran_slope$Group <- rownames(ran_slope)
-    ran_groups_slp <- gsub("\\..*", "", ran_slope$Group)
-    ran_slope$Parameter <- NA
-    for (i in unique(ran_groups_slp)) {
-      slopes <- which(grepl(paste0("^\\Q", i, "\\E"), ran_slope$Group))
-      if (length(slopes)) {
-        ran_slope$Parameter[slopes] <- paste0(
-          "SD (", gsub("^\\.", "", gsub(i, "", ran_slope$Group[slopes], fixed = TRUE)), ")"
-        )
-        ran_slope$Group[slopes] <- i
-      }
-    }
-  } else {
-    ran_groups_slp <- NULL
-  }
-
-  ran_groups <- unique(c(ran_groups_int, ran_groups_slp))
-
-
-  # random slope-intercept correlation - rho01
-  if (!is.null(ran_corr) && nrow(ran_corr) > 0) {
-    if (ncol(ran_corr) > 1 && all(colnames(ran_corr) %in% ran_groups)) {
-      ran_corr <- datawizard::reshape_longer(ran_corr, colnames_to = "Group", values_to = "Coefficient", rows_to = "Slope")
-      ran_corr$Parameter <- paste0("Cor (Intercept~", ran_corr$Slope, ": ", ran_intercept$Group, ")")
-      ran_corr <- datawizard::data_reorder(ran_corr, select = c("Parameter", "Coefficient", "Group"))
-      ran_corr$Slope <- NULL
-    } else if (!is.null(ran_intercept$Group) && colnames(ran_corr)[1] == ran_intercept$Group[1]) {
-      colnames(ran_corr)[1] <- "Coefficient"
-      ran_corr$Parameter <- paste0("Cor (Intercept~", row.names(ran_corr), ": ", ran_intercept$Group[1], ")")
-      ran_corr$Group <- ran_intercept$Group[1]
-    } else if (!is.null(ran_groups) && colnames(ran_corr)[1] == ran_groups[1]) {
-      # colnames(ran_corr)[1] <- "Coefficient"
-      # ran_corr$Parameter <- paste0("Cor (Reference~", row.names(ran_corr), ")")
-      # ran_corr$Group <- ran_groups[1]
-      # this occurs when model has no random intercept
-      # will be captures by random slope-slope-correlation
-      ran_corr <- NULL
-    } else {
-      colnames(ran_corr) <- "Coefficient"
-      ran_corr$Group <- rownames(ran_corr)
-      ran_corr$Parameter <- NA
-      for (i in unique(ran_groups)) {
-        corrs <- which(grepl(paste0("^\\Q", i, "\\E"), ran_corr$Group))
-        if (length(corrs)) {
-          param_name <- i
-          cor_slopes <- which(grepl(paste0("^\\Q", i, "\\E"), ran_slope$Group))
-          if (length(cor_slopes)) {
-            param_name <- paste0(
-              gsub("SD \\((.*)\\)", "\\1", ran_slope$Parameter[cor_slopes]),
-              ": ",
-              i
-            )
-          }
-          ran_corr$Parameter[corrs] <- paste0("Cor (Intercept~", param_name, ")")
-          ran_corr$Group[corrs] <- i
-        }
-      }
-    }
-  }
-
-
-  # random slope correlation - rho00
-  if (!is.null(ran_slopes_corr) && nrow(ran_slopes_corr) > 0) {
-    term <- rownames(ran_slopes_corr)
-    colnames(ran_slopes_corr) <- "Coefficient"
-    rg <- paste0("(", paste0(c(ran_groups, paste0(ran_groups, "\\.\\d+")), collapse = "|"), ")")
-    grp <- gsub(paste0(rg, "\\.(.*)-(.*)"), "\\1", term)
-    slope1 <- gsub(paste0(rg, "\\.(.*)-(.*)"), "\\2", term)
-    slope2 <- gsub(paste0(rg, "\\.(.*)-(.*)"), "\\3", term)
-    ran_slopes_corr$Parameter <- paste0("Cor (", slope1, "~", slope2, ": ", grp, ")")
-    ran_slopes_corr$Group <- grp
-  }
-
-
-  # residuals - sigma
-  if (!is.null(ran_sigma) && nrow(ran_sigma) > 0) {
-    colnames(ran_sigma) <- "Coefficient"
-    ran_sigma$Group <- "Residual"
-    ran_sigma$Parameter <- "SD (Observations)"
-  }
-
-  # row bind all random effect variances, if possible
-  out <- tryCatch(
-    {
-      out_list <- insight::compact_list(list(ran_intercept, ran_slope, ran_corr, ran_slopes_corr, ran_sigma))
-      do.call(rbind, out_list)
-    },
-    error = function(e) {
-      NULL
-    }
-  )
-
-  if (is.null(out)) {
-    return(NULL)
-  }
-
-  rownames(out) <- NULL
-
-  # variances to SD (sqrt), except correlations and Sigma
-  corr_param <- grepl("^Cor (.*)", out$Parameter)
-  sigma_param <- out$Parameter == "SD (Observations)"
-  not_cor_and_sigma <- !corr_param & !sigma_param
-  if (any(not_cor_and_sigma)) {
-    out$Coefficient[not_cor_and_sigma] <- sqrt(out$Coefficient[not_cor_and_sigma])
-  }
-
-  stat_column <- gsub("-statistic", "", insight::find_statistic(model), fixed = TRUE)
-
-  # to match rbind
-  out[[stat_column]] <- NA
-  out$SE <- NA
-  out$df_error <- NA
-  out$p <- NA
-  out$Level <- NA
-  out$CI <- NA
-
-  out$Effects <- "random"
-
-  if (length(ci) == 1) {
-    ci_cols <- c("CI_low", "CI_high")
-  } else {
-    ci_cols <- c()
-    for (i in ci) {
-      ci_low <- paste0("CI_low_", i)
-      ci_high <- paste0("CI_high_", i)
-      ci_cols <- c(ci_cols, ci_low, ci_high)
-    }
-  }
-  out[ci_cols] <- NA
-
-  # add confidence intervals?
-  if (!is.null(ci) && !all(is.na(ci)) && length(ci) == 1) {
-    out <- .random_sd_ci(model, out, ci_method, ci, corr_param, sigma_param, component, verbose = verbose)
-  }
-
-  out <- out[c("Parameter", "Level", "Coefficient", "SE", ci_cols, stat_column, "df_error", "p", "Effects", "Group")]
-
-  if (effects == "random") {
-    out[c(stat_column, "df_error", "p", "CI")] <- NULL
-  }
-
-  rownames(out) <- NULL
-  out
+  rbind(out_sd, out_cor)
 }
 
 
@@ -814,18 +675,13 @@
     # nlme
     # ---------------------------
   } else if (inherits(model, "lme")) {
-    re_names <- insight::find_random(model, split_nested = TRUE, flatten = TRUE)
-    if (.is_nested_lme(model)) {
-      varcorr <- .get_nested_lme_varcorr(model)
-    } else {
-      varcorr <- list(nlme::getVarCov(model))
-    }
-    names(varcorr) <- re_names
+    varcorr <- lme4::VarCorr(model)
 
     # ordinal
     # ---------------------------
   } else if (inherits(model, "clmm")) {
     varcorr <- ordinal::VarCorr(model)
+    attr(varcorr, "useSc") <- FALSE
 
     # glmmadmb
     # ---------------------------
@@ -859,6 +715,7 @@
     # ---------------------------
   } else if (inherits(model, "cpglmm")) {
     varcorr <- cplm::VarCorr(model)
+    attr(varcorr, "useSc") <- FALSE
 
     # lme4 / glmmTMB
     # ---------------------------
@@ -882,64 +739,6 @@
 
 
 
-# Caution! this is somewhat experimental...
-# It retrieves the variance-covariance matrix of random effects
-# from nested lme-models.
-.get_nested_lme_varcorr <- function(model) {
-  # installed?
-  insight::check_if_installed("lme4")
-
-  vcor <- lme4::VarCorr(model)
-  class(vcor) <- "matrix"
-
-  re_index <- (which(rownames(vcor) == "(Intercept)") - 1)[-1]
-  vc_list <- split(data.frame(vcor, stringsAsFactors = FALSE), findInterval(1:nrow(vcor), re_index))
-  vc_rownames <- split(rownames(vcor), findInterval(1:nrow(vcor), re_index))
-  re_pars <- unique(unlist(insight::find_parameters(model)["random"]))
-  re_names <- insight::find_random(model, split_nested = TRUE, flatten = TRUE)
-
-  names(vc_list) <- re_names
-
-  mapply(
-    function(x, y) {
-      if ("Corr" %in% colnames(x)) {
-        g_cor <- suppressWarnings(stats::na.omit(as.numeric(x[, "Corr"])))
-      } else {
-        g_cor <- NULL
-      }
-      row.names(x) <- as.vector(y)
-      vl <- rownames(x) %in% re_pars
-      x <- suppressWarnings(apply(x[vl, vl, drop = FALSE], MARGIN = c(1, 2), FUN = as.numeric))
-      m1 <- matrix(, nrow = nrow(x), ncol = ncol(x))
-      m1[1:nrow(m1), 1:ncol(m1)] <- as.vector(x[, 1])
-      rownames(m1) <- rownames(x)
-      colnames(m1) <- rownames(x)
-
-      if (!is.null(g_cor)) {
-        m1_cov <- sqrt(prod(diag(m1))) * g_cor
-        for (j in 1:ncol(m1)) {
-          m1[j, nrow(m1) - j + 1] <- m1_cov[1]
-        }
-      }
-
-      attr(m1, "cor_slope_intercept") <- g_cor
-      m1
-    },
-    vc_list,
-    vc_rownames,
-    SIMPLIFY = FALSE
-  )
-}
-
-
-.is_nested_lme <- function(model) {
-  re <- insight::find_random(model)
-  if (is.null(re)) {
-    return(FALSE)
-  }
-  sapply(re, function(i) any(grepl(":", i, fixed = TRUE)))
-}
-
 
 
 # glmmTMB returns a list of model information, one for conditional
@@ -960,168 +759,4 @@
   } else {
     x
   }
-}
-
-
-
-
-#### helper to extract various random effect variances -----------------------
-
-## NOTE these may be no longer needed, once we switch to the new workhorse function
-
-# random slope-variances (tau 11) ----
-# ----------------------------------------------
-.random_slope_variance <- function(model, varcorr) {
-  if (inherits(model, "lme")) {
-    unlist(lapply(varcorr, function(x) diag(x)[-1]))
-  } else {
-    # random slopes for correlated slope-intercept
-    out <- unlist(lapply(varcorr, function(x) diag(x)[-1]))
-    # check for uncorrelated random slopes-intercept
-    non_intercepts <- which(sapply(varcorr, function(i) !grepl("^\\(Intercept\\)", dimnames(i)[[1]][1])))
-    if (length(non_intercepts)) {
-      if (length(non_intercepts) == length(varcorr)) {
-        out <- unlist(lapply(varcorr, function(x) diag(x)))
-      } else {
-        dn <- unlist(lapply(varcorr, function(i) dimnames(i)[1])[non_intercepts])
-        rndslopes <- unlist(lapply(varcorr, function(i) {
-          if (is.null(dim(i)) || identical(dim(i), c(1, 1))) {
-            as.vector(i)
-          } else {
-            as.vector(diag(i))
-          }
-        })[non_intercepts])
-        # random slopes for uncorrelated slope-intercept
-        names(rndslopes) <- gsub("(.*)\\.\\d+$", "\\1", names(rndslopes))
-        rndslopes <- stats::setNames(rndslopes, paste0(names(rndslopes), ".", dn))
-        # anything missing? (i.e. correlated slope-intercept slopes)
-        missig_rnd_slope <- setdiff(names(out), names(rndslopes))
-        if (length(missig_rnd_slope)) {
-          # sanity check
-          to_remove <- c()
-          for (j in 1:length(out)) {
-            # identical random slopes might have different names, so
-            # we here check if random slopes from correlated and uncorrelated
-            # are duplicated (i.e. their difference is 0 - including a tolerance)
-            # and then remove duplicated elements
-            the_same <- which(abs(outer(out[j], rndslopes, `-`)) < .0001)
-            if (length(the_same) && grepl(dn[the_same], names(out[j]), fixed = TRUE)) {
-              to_remove <- c(to_remove, j)
-            }
-          }
-          if (length(to_remove)) {
-            out <- out[-to_remove]
-          }
-          out <- c(out, rndslopes)
-        } else {
-          out <- rndslopes
-        }
-      }
-    }
-    out
-  }
-}
-
-
-
-# random intercept-variances, i.e.
-# between-subject-variance (tau 00) ----
-# ----------------------------------------------
-.random_intercept_variance <- function(varcorr) {
-  vars <- lapply(varcorr, function(i) i[1])
-  # check for uncorrelated random slopes-intercept
-  non_intercepts <- which(sapply(varcorr, function(i) !grepl("^\\(Intercept\\)", dimnames(i)[[1]][1])))
-  if (length(non_intercepts)) {
-    vars <- vars[-non_intercepts]
-  }
-
-  sapply(vars, function(i) i)
-}
-
-
-
-# slope-intercept-correlations (rho 01) ----
-# ----------------------------------------------
-.random_slope_intercept_corr <- function(model, varcorr) {
-  if (inherits(model, "lme")) {
-    rho01 <- unlist(sapply(varcorr, function(i) attr(i, "cor_slope_intercept")))
-    if (is.null(rho01)) {
-      vc <- lme4::VarCorr(model)
-      if ("Corr" %in% colnames(vc)) {
-        re_name <- insight::find_random(model, split_nested = FALSE, flatten = TRUE)
-        rho01 <- as.vector(suppressWarnings(stats::na.omit(as.numeric(vc[, "Corr"]))))
-        if (length(re_name) == length(rho01)) {
-          names(rho01) <- re_name
-        }
-      }
-    }
-    rho01
-  } else {
-    corrs <- lapply(varcorr, attr, "correlation")
-    rho01 <- sapply(corrs, function(i) {
-      if (!is.null(i) && colnames(i)[1] == "(Intercept)") {
-        i[-1, 1]
-      } else {
-        NULL
-      }
-    })
-    unlist(rho01)
-  }
-}
-
-
-
-# slope-slope-correlations (rho 00) ----
-# ----------------------------------------------
-.random_slopes_corr <- function(model, varcorr) {
-  corrs <- lapply(varcorr, attr, "correlation")
-  rnd_slopes <- unlist(insight::find_random_slopes(model))
-
-  # check if any categorical random slopes. we then have
-  # correlation among factor levels
-  cat_random_slopes <- tryCatch(
-    {
-      d <- insight::get_data(model)[rnd_slopes]
-      any(sapply(d, is.factor))
-    },
-    error = function(e) {
-      NULL
-    }
-  )
-
-  # check if any polynomial / I term in random slopes.
-  # we then have correlation among levels
-  rs_names <- unique(unlist(lapply(corrs, colnames)))
-  pattern <- paste0("(I|poly)(.*)(", paste0(rnd_slopes, collapse = "|"), ")")
-  poly_random_slopes <- any(grepl(pattern, rs_names))
-
-  if (length(rnd_slopes) < 2 && !isTRUE(cat_random_slopes) && !isTRUE(poly_random_slopes)) {
-    return(NULL)
-  }
-
-  rho00 <- tryCatch(
-    {
-      insight::compact_list(lapply(corrs, function(d) {
-        d[upper.tri(d, diag = TRUE)] <- NA
-        d <- as.data.frame(d)
-
-        d <- datawizard::reshape_longer(d, colnames_to = "Parameter1", rows_to = "Parameter2", verbose = FALSE)
-        d <- d[stats::complete.cases(d), ]
-        d <- d[!d$Parameter1 %in% c("Intercept", "(Intercept)"), ]
-
-        if (nrow(d) == 0) {
-          return(NULL)
-        }
-
-        d$Parameter <- paste0(d$Parameter1, "-", d$Parameter2)
-        d$Parameter1 <- d$Parameter2 <- NULL
-        stats::setNames(d$Value, d$Parameter)
-      }))
-    },
-    error = function(e) {
-      NULL
-    }
-  )
-
-  unlist(rho00)
 }
