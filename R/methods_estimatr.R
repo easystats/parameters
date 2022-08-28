753aa0b347a00db663c38523c5430a0dd61a64a4
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2022-08-28 09:14:28 -0400

    fix method signature

--- R/methods_estimatr.R
+++ R/methods_estimatr.R
@@ -1,38 +1,37 @@
 #' @export
-standard_error.lm_robust <- function(x, ...) {
-  if (insight::is_multivariate(x)) {
-    standard_error.mlm(x, ...)
+standard_error.lm_robust <- function(model, ...) {
+  if (insight::is_multivariate(model)) {
+    standard_error.mlm(model, ...)
   } else {
-    standard_error.default(x, ...)
+    standard_error.default(model, ...)
   }
 }
 
 
 #' @export
-p_value.lm_robust <- function(x, ...) {
-  if (insight::is_multivariate(x)) {
-    p_value.mlm(x, ...)
+p_value.lm_robust <- function(model, ...) {
+  if (insight::is_multivariate(model)) {
+    p_value.mlm(model, ...)
   } else {
-    p_value.default(x, ...)
+    p_value.default(model, ...)
   }
 }
 
 
 #' @export
-ci.lm_robust <- function(x, ...) {
-  if (insight::is_multivariate(x)) {
-    ci.mlm(x, ...)
+ci.lm_robust <- function(model, ...) {
+  if (insight::is_multivariate(model)) {
+    ci.mlm(model, ...)
   } else {
-    ci.default(x, ...)
+    ci.default(model, ...)
   }
 }
 
-
 #' @export
-model_parameters.lm_robust <- function(x, ...) {
-  if (insight::is_multivariate(x)) {
-    model_parameters.mlm(x, ...)
+model_parameters.lm_robust <- function(model, ...) {
+  if (insight::is_multivariate(model)) {
+    model_parameters.mlm(model, ...)
   } else {
-    model_parameters.default(x, ...)
+    model_parameters.default(model, ...)
   }
 }
