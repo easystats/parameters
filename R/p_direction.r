ad0f0fdb1b543b280e3102637790d06e543f1f8b
Daniel <mail@danielluedecke.de>
2022-08-31 17:00:29 +0200

    update namespace

--- R/p_direction.r
+++ R/p_direction.r
@@ -1,9 +1,18 @@
 #' @export
-p_direction.lm <- function(x, ...) {
+p_direction.lm <- function(x, dof = NULL, ...) {
   # check for valid input
   .is_model_valid(x)
   # required parameters
   stats <- insight::get_statistic(x)
-  dof <- degrees_of_freedom(x)
-  stats::pt(abs(stats$Statistic), df = dof, lower.tail = TRUE)
+  if (is.null(dof)) {
+    dof <- degrees_of_freedom(x)
+  }
+  out <- .data_frame(
+    Parameter = stats$Parameter,
+    pd = stats::pt(abs(stats$Statistic), df = dof, lower.tail = TRUE)
+  )
+  if (!is.null(stats$Component)) {
+    out$Component <- stats$Component
+  }
+  out
 }
