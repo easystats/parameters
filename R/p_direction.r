fc4511c4cf3beaf77f5483ae00640356c0fb5631
Daniel <mail@danielluedecke.de>
2022-08-31 17:14:04 +0200

    check for < 0.5 not needed

--- R/p_direction.r
+++ R/p_direction.r
@@ -8,9 +8,6 @@ p_direction.lm <- function(x, dof = NULL, ...) {
     dof <- degrees_of_freedom(x)
   }
   pd <- stats::pt(abs(stats$Statistic), df = dof, lower.tail = TRUE)
-  if (any(pd < 0.5)) {
-    pd[pd < 0.5] <- 1 - pd[pd < 0.5]
-  }
   out <- .data_frame(Parameter = stats$Parameter, pd = pd)
   if (!is.null(stats$Component)) {
     out$Component <- stats$Component
