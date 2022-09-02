c7608ae361e1866034f6609b873476f0436ae0e7
Dominique Makowski <dom.mak19@gmail.com>
2022-09-02 10:28:02 +0800

    move get_scores.R to utils_pca_efa.R + fixes to predict.efa

--- R/utils_pca_efa.R
+++ R/utils_pca_efa.R
@@ -1,3 +1,68 @@
+#' Get Scores from Principal Component Analysis (PCA)
+#'
+#' `get_scores()` takes `n_items` amount of items that load the most
+#' (either by loading cutoff or number) on a component, and then computes their
+#' average.
+#'
+#' @param x An object returned by [principal_components()].
+#' @param n_items Number of required (i.e. non-missing) items to build the sum
+#'   score. If `NULL`, the value is chosen to match half of the number of
+#'   columns in a data frame.
+#'
+#' @details `get_scores()` takes the results from
+#'   [principal_components()] and extracts the variables for each
+#'   component found by the PCA. Then, for each of these "subscales", row means
+#'   are calculated (which equals adding up the single items and dividing by the
+#'   number of items). This results in a sum score for each component from the
+#'   PCA, which is on the same scale as the original, single items that were
+#'   used to compute the PCA.
+#'
+#' @examples
+#' if (require("psych")) {
+#'   pca <- principal_components(mtcars[, 1:7], n = 2, rotation = "varimax")
+#'
+#'   # PCA extracted two components
+#'   pca
+#'
+#'   # assignment of items to each component
+#'   closest_component(pca)
+#'
+#'   # now we want to have sum scores for each component
+#'   get_scores(pca)
+#'
+#'   # compare to manually computed sum score for 2nd component, which
+#'   # consists of items "hp" and "qsec"
+#'   (mtcars$hp + mtcars$qsec) / 2
+#' }
+#' @return A data frame with subscales, which are average sum scores for all
+#'   items from each component.
+#' @export
+get_scores <- function(x, n_items = NULL) {
+  subscales <- closest_component(x)
+  data_set <- attributes(x)$data_set
+
+  out <- lapply(sort(unique(subscales)), function(.subscale) {
+    columns <- names(subscales)[subscales == .subscale]
+    items <- data_set[columns]
+
+    if (is.null(n_items)) {
+      .n_items <- round(ncol(items) / 2)
+    } else {
+      .n_items <- n_items
+    }
+
+    apply(items, 1, function(i) ifelse(sum(!is.na(i)) >= .n_items, mean(i, na.rm = TRUE), NA))
+  })
+
+  out <- as.data.frame(do.call(cbind, out))
+  colnames(out) <- sprintf("Component_%i", 1:ncol(out))
+
+  out
+}
+
+
+
+
 # model parameters -----------------------------------------------------------------
 
 
@@ -76,23 +141,30 @@ predict.parameters_efa <- function(object,
                                    names = NULL,
                                    keep_na = TRUE,
                                    ...) {
+  attri <- attributes(object)
   if (is.null(newdata)) {
-    out <- as.data.frame(attributes(object)$scores)
-    if (isTRUE(keep_na)) {
-      out <- .merge_na(object, out)
+    if("scores" %in% names(attri)) {
+      out <- as.data.frame(attri$scores)
+      if (isTRUE(keep_na)) {
+        out <- .merge_na(object, out)
+      }
+    } else {
+      if("data_set" %in% names(attri)) {
+        out <- as.data.frame(stats::predict(attri$model, data = attri$data_set))
+      } else {
+        stop("Could not retrieve data nor model... Please report an issue on github.com/easystats/parameters.")
+      }
     }
   } else {
-    if (inherits(attributes(object)$model, c("psych", "fa"))) {
+    if (inherits(attri$model, c("psych", "fa"))) {
       # psych:::predict.fa(object, data)
-      out <- as.data.frame(stats::predict(attributes(object)$model, data = newdata))
+      out <- as.data.frame(stats::predict(attri$model, data = newdata))
     } else {
-      out <- as.data.frame(stats::predict(attributes(object)$model, newdata = newdata, ...))
+      out <- as.data.frame(stats::predict(attri$model, newdata = newdata, ...))
     }
   }
   if (!is.null(names)) {
     names(out)[1:length(c(names))] <- names
-  } else {
-    names(out) <- names(get_scores(object))
   }
   row.names(out) <- NULL
   out
