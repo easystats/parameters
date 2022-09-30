c568ecedf8e7e4e441915743e2c113f8e4a9cc1c
Daniel <mail@danielluedecke.de>
2022-09-30 10:51:22 +0200

    code style

--- R/methods_rstanarm.R
+++ R/methods_rstanarm.R
@@ -101,10 +101,11 @@ model_parameters.stanreg <- function(model,
 
   if (effects != "fixed") {
     random_effect_levels <- which(
-      params$Effects %in% "random" &
-        grepl("^(?!Sigma\\[)(.*)", params$Parameter, perl = TRUE)
+      params$Effects %in% "random" & grepl("^(?!Sigma\\[)(.*)", params$Parameter, perl = TRUE)
     )
-    if (length(random_effect_levels) && isFALSE(group_level)) params <- params[-random_effect_levels, ]
+    if (length(random_effect_levels) && isFALSE(group_level)) {
+      params <- params[-random_effect_levels, , drop = FALSE]
+    }
   }
 
   params <- .add_pretty_names(params, model)
