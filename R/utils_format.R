26d93593f99316a57f4711449117cb080fffab30
Daniel <mail@danielluedecke.de>
2022-10-04 10:31:50 +0200

    revise printing components for compare_parameters

--- R/utils_format.R
+++ R/utils_format.R
@@ -506,6 +506,11 @@
 
 
 .prepare_splitby_for_print <- function(x) {
+  # fix colnames for compare_parameters
+  if (inherits(x, "compare_parameters")) {
+    colnames(x) <- gsub("(.*)\\.(.*)$", "\\1", colnames(x))
+  }
+
   if (!is.null(attributes(x)$model_class) && any(attributes(x)$model_class == "mvord")) {
     x$Response <- NULL
   }
