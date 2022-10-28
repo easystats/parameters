fc274ce2caceae08ef05038f7cb2b11700c6074c
Daniel <mail@danielluedecke.de>
2022-10-28 10:44:17 +0200

    fix

--- R/utils_cleaners.R
+++ R/utils_cleaners.R
@@ -31,7 +31,7 @@
       # some exceptions here...
     } else if (full && pattern[j] == "scale" && any(grepl("scale(", x, fixed = TRUE))) {
       x[grepl("scale(", x, fixed = TRUE)] <- insight::clean_names(x[grepl("scale(", x, fixed = TRUE)])
-    } else if (any(grepl(pattern, x, fixed = TRUE))) {
+    } else if (any(grepl(pattern[j], x, fixed = TRUE))) {
       p <- paste0(pattern[j], "\\(((\\w|\\.)*)\\)(.*)")
       x <- insight::trim_ws(sub(p, "\\1\\3", x))
     }
