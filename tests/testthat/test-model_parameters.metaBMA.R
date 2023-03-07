0ec098420e6c06dee7389f24f1bbdb7eecf01331
Daniel <mail@danielluedecke.de>
2023-03-07 10:56:11 +0100

    lintr

--- tests/testthat/test-model_parameters.metaBMA.R
+++ tests/testthat/test-model_parameters.metaBMA.R
@@ -1,25 +1,21 @@
-if (
-
-
-  requiet("metaBMA")) {
+if (requiet("metaBMA")) {
   data(towels)
 
   # default
   set.seed(1234)
-  m <-
-    suppressWarnings(meta_random(
-      logOR,
-      SE,
-      study,
-      data = towels,
-      ci = 0.95,
-      iter = 100,
-      logml_iter = 200
-    ))
+  m <- suppressWarnings(meta_random(
+    logOR,
+    SE,
+    study,
+    data = towels,
+    ci = 0.95,
+    iter = 100,
+    logml_iter = 200
+  ))
 
   test_that("model_parameters.meta_random", {
     params <- model_parameters(m)
-    expect_equal(params$Parameter, c(
+    expect_identical(params$Parameter, c(
       "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
       "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
       "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
@@ -35,7 +31,7 @@ if (
       c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.02744, 0.02641),
       tolerance = 1e-3
     )
-    expect_equal(
+    expect_identical(
       colnames(params),
       c(
         "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
@@ -45,18 +41,17 @@ if (
   })
 
   set.seed(1234)
-  m2 <-
-    meta_fixed(
-      logOR,
-      SE,
-      study,
-      data = towels,
-      ci = 0.95
-    )
+  m2 <- meta_fixed(
+    logOR,
+    SE,
+    study,
+    data = towels,
+    ci = 0.95
+  )
 
   test_that("model_parameters.meta_fixed", {
     params <- model_parameters(m2)
-    expect_equal(params$Parameter, c(
+    expect_identical(params$Parameter, c(
       "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
       "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
       "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
@@ -71,7 +66,7 @@ if (
       c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, 0.06839),
       tolerance = 1e-3
     )
-    expect_equal(
+    expect_identical(
       colnames(params),
       c(
         "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
@@ -81,28 +76,35 @@ if (
   })
 
   set.seed(1234)
-  m3 <-
-    suppressWarnings(meta_random(
-      logOR,
-      SE,
-      study,
-      data = towels,
-      ci = 0.99,
-      iter = 100,
-      logml_iter = 200
-    ))
+  m3 <- suppressWarnings(meta_random(
+    logOR,
+    SE,
+    study,
+    data = towels,
+    ci = 0.99,
+    iter = 100,
+    logml_iter = 200
+  ))
 
   test_that("model_parameters.meta_random", {
     params <- model_parameters(m3)
-    expect_equal(params$Parameter, c(
+    expect_identical(params$Parameter, c(
       "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
       "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
       "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
       "Bohner & Schluter (2014), Exp. 2", "Overall", "tau"
     ))
-    expect_equal(params$Coefficient, c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.2004, 0.12107), tolerance = 1e-3)
-    expect_equal(params$CI_low, c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.15494, 0.01993), tolerance = 1e-3)
     expect_equal(
+      params$Coefficient,
+      c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.2004, 0.12107),
+      tolerance = 1e-3
+    )
+    expect_equal(
+      params$CI_low,
+      c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.15494, 0.01993),
+      tolerance = 1e-3
+    )
+    expect_identical(
       colnames(params),
       c(
         "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
