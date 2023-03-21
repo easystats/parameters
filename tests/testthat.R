8dc3f42e425061fb4a0729ca0a1e19618e90f295
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-21 17:42:40 +0100

    clean expect_equal lints
    https://github.com/easystats/easystats/issues/334

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,41 +1,12 @@
 if (require("testthat")) {
   library(parameters)
-  if (length(strsplit(packageDescription("parameters")$Version, ".", fixed = TRUE)[[1]]) > 3) {
+
+  is_dev_version <- length(strsplit(packageDescription("parameters")$Version, ".", fixed = TRUE)[[1]]) > 3L
+  if (is_dev_version) {
     Sys.setenv("RunAllparametersTests" = "yes")
   } else {
     Sys.setenv("RunAllparametersTests" = "no")
   }
 
-  si <- Sys.info()
-
-  osx <- tryCatch(
-    {
-      if (!is.null(si["sysname"])) {
-        si["sysname"] == "Darwin" || grepl("^darwin", R.version$os)
-      } else {
-        FALSE
-      }
-    },
-    error = function(e) {
-      FALSE
-    }
-  )
-
-  solaris <- tryCatch(
-    {
-      if (!is.null(si["sysname"])) {
-        grepl("SunOS", si["sysname"], ignore.case = TRUE)
-      } else {
-        FALSE
-      }
-    },
-    error = function(e) {
-      FALSE
-    }
-  )
-
-  # if (!osx && !solaris) {
-  #   test_check("parameters")
-  # }
   test_check("parameters")
 }
--- tests/testthat/test-model_parameters.efa_cfa.R
+++ tests/testthat/test-model_parameters.efa_cfa.R
@@ -1,35 +1,29 @@
-linux <- tryCatch({
-  si <- Sys.info()
-  if (!is.null(si["sysname"])) {
-    si["sysname"] == "Linux" || grepl("^linux", R.version$os)
-  } else {
-    FALSE
-  }
-})
-
-
-if (
-
-  requiet("psych") &&
-    requiet("lavaan") &&
-    requiet("BayesFM") &&
-    requiet("FactoMineR")) {
+if (requiet("psych") &&
+  requiet("lavaan") &&
+  requiet("BayesFM") &&
+  requiet("FactoMineR")) {
   test_that("principal_components", {
     set.seed(333)
-
     x <- principal_components(mtcars[, 1:7], n = "all", threshold = 0.2)
-    expect_equal(c(ncol(x), nrow(x)), c(8, 7))
-    x <- suppressMessages(principal_components(mtcars[, 1:7], n = 2, rotation = "oblimin", threshold = "max", sort = TRUE))
-    expect_equal(c(ncol(x), nrow(x)), c(6, 7))
+    expect_identical(c(ncol(x), nrow(x)), c(8L, 7L))
+
+    x <- suppressMessages(principal_components(
+      mtcars[, 1:7],
+      n = 2,
+      rotation = "oblimin",
+      threshold = "max",
+      sort = TRUE
+    ))
+    expect_identical(c(ncol(x), nrow(x)), c(6L, 7L))
 
     pca <- principal_components(mtcars[, 1:5], n = 2)
-    expect_equal(c(ncol(pca), nrow(pca)), c(4, 5))
+    expect_identical(c(ncol(pca), nrow(pca)), c(4L, 5L))
     x <- summary(pca)
-    expect_equal(c(ncol(x), nrow(x)), c(3, 4))
+    expect_identical(c(ncol(x), nrow(x)), c(3L, 4L))
     x <- model_parameters(pca)
-    expect_equal(c(ncol(x), nrow(x)), c(5, 2))
+    expect_identical(c(ncol(x), nrow(x)), c(5L, 2L))
     x <- predict(pca)
-    expect_equal(c(ncol(x), nrow(x)), c(2, 32))
+    expect_identical(c(ncol(x), nrow(x)), c(2L, 32L))
   })
 
 
@@ -37,44 +31,40 @@ if (
   test_that("efa-cfa", {
     efa <- psych::fa(attitude, nfactors = 3)
     params <- parameters::model_parameters(efa)
-    expect_equal(c(nrow(params), ncol(params)), c(7, 6))
+    expect_identical(c(nrow(params), ncol(params)), c(7L, 6L))
 
     model1 <- efa_to_cfa(efa)
     model2 <- efa_to_cfa(efa, threshold = 0.3)
 
-    expect_equal(nchar(model1), 109)
+    expect_identical(nchar(model1), 109L)
 
     m1 <- suppressWarnings(lavaan::cfa(model1, data = attitude))
     params <- parameters::model_parameters(m1)
-    expect_equal(c(nrow(params), ncol(params)), c(10, 10))
+    expect_identical(c(nrow(params), ncol(params)), c(10L, 10L))
 
     expect_message(parameters::model_parameters(m1, ci = c(0.8, 0.9)))
 
     params <- parameters::model_parameters(m1, standardize = TRUE, component = "all")
-    expect_equal(c(nrow(params), ncol(params)), c(20, 10))
+    expect_identical(c(nrow(params), ncol(params)), c(20L, 10L))
 
     x <- lavaan::anova(m1, lavaan::cfa(model2, data = attitude))
     params <- parameters::model_parameters(x)
-    expect_equal(c(nrow(params), ncol(params)), c(2, 6))
+    expect_identical(c(nrow(params), ncol(params)), c(2L, 6L))
   })
 
+  test_that("FactoMineR", {
+    x <- suppressWarnings(model_parameters(FactoMineR::PCA(mtcars, ncp = 3), threshold = 0.2, sort = TRUE))
+    expect_identical(c(ncol(x), nrow(x)), c(5L, 11L))
 
-  # if (!linux) {
-  #   test_that("FactoMineR", {
-  #     x <- suppressWarnings(model_parameters(FactoMineR::PCA(mtcars, ncp = 3), threshold = 0.2, sort = TRUE))
-  #     expect_equal(c(ncol(x), nrow(x)), c(5, 11))
-  #
-  #     x <- suppressWarnings(model_parameters(FactoMineR::FAMD(iris, ncp = 3), threshold = 0.2, sort = TRUE))
-  #     expect_equal(c(ncol(x), nrow(x)), c(5, 5))
-  #   })
-  # }
-
+    x <- suppressWarnings(model_parameters(FactoMineR::FAMD(iris, ncp = 3), threshold = 0.2, sort = TRUE))
+    expect_identical(c(ncol(x), nrow(x)), c(5L, 5L))
+  })
 
   set.seed(333)
   befa <- BayesFM::befa(mtcars, iter = 1000, verbose = FALSE)
   params <- suppressWarnings(parameters::model_parameters(befa, sort = TRUE))
 
   test_that("BayesFM", {
-    expect_equal(nrow(params), 11)
+    expect_identical(nrow(params), 11L)
   })
 }
