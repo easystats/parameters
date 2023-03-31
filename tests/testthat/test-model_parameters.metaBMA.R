59f4bc983b7bba6e5bef24dbbcfb733f04b4bcca
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-03-31 19:07:22 +0200

    more silence in tests

--- tests/testthat/test-model_parameters.metaBMA.R
+++ tests/testthat/test-model_parameters.metaBMA.R
@@ -1,9 +1,9 @@
-if (requiet("metaBMA")) {
-  data(towels)
+skip_if_not_installed("metaBMA")
+data(towels, package = "metaBMA")
 
-  # default
-  set.seed(1234)
-  m <- suppressWarnings(meta_random(
+set.seed(1234)
+m <- suppressWarnings(
+  metaBMA::meta_random(
     logOR,
     SE,
     study,
@@ -11,72 +11,77 @@ if (requiet("metaBMA")) {
     ci = 0.95,
     iter = 100,
     logml_iter = 200
-  ))
+  )
+)
 
-  test_that("model_parameters.meta_random", {
-    params <- model_parameters(m)
-    expect_identical(params$Parameter, c(
+test_that("model_parameters.meta_random", {
+  params <- model_parameters(m)
+  expect_identical(
+    params$Parameter,
+    c(
       "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
       "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
       "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
       "Bohner & Schluter (2014), Exp. 2", "Overall", "tau"
-    ))
-    expect_equal(
-      params$Coefficient,
-      c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.2004, 0.12107),
-      tolerance = 1e-3
     )
-    expect_equal(
-      params$CI_low,
-      c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.02744, 0.02641),
-      tolerance = 1e-3
-    )
-    expect_identical(
-      colnames(params),
-      c(
-        "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
-        "BF", "Rhat", "ESS", "Component", "Prior_Distribution", "Prior_Location", "Prior_Scale", "Method"
-      )
-    )
-  })
-
-  set.seed(1234)
-  m2 <- meta_fixed(
-    logOR,
-    SE,
-    study,
-    data = towels,
-    ci = 0.95
   )
+  expect_equal(
+    params$Coefficient,
+    c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.2004, 0.12107),
+    tolerance = 1e-3
+  )
+  expect_equal(
+    params$CI_low,
+    c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.02744, 0.02641),
+    tolerance = 1e-3
+  )
+  expect_identical(
+    colnames(params),
+    c(
+      "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
+      "BF", "Rhat", "ESS", "Component", "Prior_Distribution", "Prior_Location", "Prior_Scale", "Method"
+    )
+  )
+})
 
-  test_that("model_parameters.meta_fixed", {
-    params <- model_parameters(m2)
-    expect_identical(params$Parameter, c(
-      "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
-      "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
-      "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
-      "Bohner & Schluter (2014), Exp. 2", "Overall"
-    ))
-    expect_equal(params$Coefficient,
-      c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.22141),
-      tolerance = 1e-3
-    )
-    expect_equal(
-      params$CI_low,
-      c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, 0.06839),
-      tolerance = 1e-3
-    )
-    expect_identical(
-      colnames(params),
-      c(
-        "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
-        "BF", "Rhat", "ESS", "Component", "Prior_Distribution", "Prior_Location", "Prior_Scale", "Method"
-      )
-    )
-  })
+set.seed(1234)
+m2 <- metaBMA::meta_fixed(
+  logOR,
+  SE,
+  study,
+  data = towels,
+  ci = 0.95
+)
 
-  set.seed(1234)
-  m3 <- suppressWarnings(meta_random(
+test_that("model_parameters.meta_fixed", {
+  params <- model_parameters(m2)
+  expect_identical(params$Parameter, c(
+    "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
+    "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
+    "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
+    "Bohner & Schluter (2014), Exp. 2", "Overall"
+  ))
+  expect_equal(params$Coefficient,
+    c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.22141),
+    tolerance = 1e-3
+  )
+  expect_equal(
+    params$CI_low,
+    c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, 0.06839),
+    tolerance = 1e-3
+  )
+  expect_identical(
+    colnames(params),
+    c(
+      "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
+      "BF", "Rhat", "ESS", "Component", "Prior_Distribution", "Prior_Location", "Prior_Scale", "Method"
+    )
+  )
+})
+
+set.seed(1234)
+m3 <- suppressWarnings(
+  metaBMA::meta_random(
     logOR,
     SE,
     study,
@@ -84,32 +89,35 @@ if (requiet("metaBMA")) {
     ci = 0.99,
     iter = 100,
     logml_iter = 200
-  ))
+  )
+)
 
-  test_that("model_parameters.meta_random", {
-    params <- model_parameters(m3)
-    expect_identical(params$Parameter, c(
+test_that("model_parameters.meta_random", {
+  params <- model_parameters(m3)
+  expect_identical(
+    params$Parameter,
+    c(
       "Goldstein, Cialdini, & Griskevicius (2008), Exp. 1", "Goldstein, Cialdini, & Griskevicius  (2008), Exp. 2",
       "Schultz, Khazian, & Zaleski (2008), Exp. 2", "Schultz, Khazian, & Zaleski (2008), Exp. 3",
       "Mair & Bergin-Seers (2010), Exp. 1", "Bohner & Schluter (2014), Exp. 1",
       "Bohner & Schluter (2014), Exp. 2", "Overall", "tau"
-    ))
-    expect_equal(
-      params$Coefficient,
-      c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.2004, 0.12107),
-      tolerance = 1e-3
     )
-    expect_equal(
-      params$CI_low,
-      c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.15494, 0.01993),
-      tolerance = 1e-3
+  )
+  expect_equal(
+    params$Coefficient,
+    c(0.3806, 0.30494, 0.20554, 0.25084, 0.28768, -0.12154, -1.45792, 0.2004, 0.12107),
+    tolerance = 1e-3
+  )
+  expect_equal(
+    params$CI_low,
+    c(-0.00686, 0.03816, -0.16998, -0.0825, -1.32685, -0.60772, -2.94785, -0.15494, 0.01993),
+    tolerance = 1e-3
+  )
+  expect_identical(
+    colnames(params),
+    c(
+      "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
+      "BF", "Rhat", "ESS", "Component", "Prior_Distribution", "Prior_Location", "Prior_Scale", "Method"
     )
-    expect_identical(
-      colnames(params),
-      c(
-        "Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "Weight",
-        "BF", "Rhat", "ESS", "Component", "Prior_Distribution", "Prior_Location", "Prior_Scale", "Method"
-      )
-    )
-  })
-}
+  )
+})
--- tests/testthat/test-pca.R
+++ tests/testthat/test-pca.R
@@ -1,74 +1,78 @@
-if (requiet("psych") && requiet("nFactors")) {
-  test_that("principal_components", {
-    x <- parameters::principal_components(mtcars[, 1:7], rotation = "varimax")
+skip_if_not_installed("psych")
+skip_if_not_installed("nFactors")
+skip_if_not_installed("GPArotation")
 
-    expect_equal(
-      x$RC1,
-      c(
-        -0.836114674884308,
-        0.766808147590597,
-        0.85441780762136,
-        0.548502661888057,
-        -0.889046093964722,
-        0.931879020871552,
-        -0.030485507571411
-      ),
-      tolerance = 0.01
-    )
+test_that("principal_components", {
+  x <- parameters::principal_components(mtcars[, 1:7], rotation = "varimax")
 
-    expect_equal(
-      colnames(x),
-      c("Variable", "RC1", "RC2", "Complexity", "Uniqueness", "MSA")
-    )
-  })
+  expect_equal(
+    x$RC1,
+    c(
+      -0.836114674884308,
+      0.766808147590597,
+      0.85441780762136,
+      0.548502661888057,
+      -0.889046093964722,
+      0.931879020871552,
+      -0.030485507571411
+    ),
+    tolerance = 0.01
+  )
 
-
-  test_that("principal_components", {
-    x <- parameters::principal_components(mtcars[, 1:7])
-
-    expect_equal(
-      x$PC1,
-      c(
-        -0.930866058535747,
-        0.9578708009312,
-        0.952846253483008,
-        0.874493647245971,
-        -0.746868056938478,
-        0.882509152331738,
-        -0.541093678419456
-      ),
-      tolerance = 0.01
-    )
-
-    expect_equal(
-      colnames(x),
-      c("Variable", "PC1", "PC2", "Complexity")
-    )
-  })
+  expect_equal(
+    colnames(x),
+    c("Variable", "RC1", "RC2", "Complexity", "Uniqueness", "MSA")
+  )
+})
 
 
-  # predict ----------------------
-  # N.B tests will fail if `GPArotation` package is not installed
+test_that("principal_components", {
+  x <- parameters::principal_components(mtcars[, 1:7])
 
-  data(bfi)
-  d <- na.omit(bfi[, 1:25])
-  model <- psych::fa(d, nfactors = 5)
-  mp <- model_parameters(model, sort = TRUE, threshold = "max")
+  expect_equal(
+    x$PC1,
+    c(
+      -0.930866058535747,
+      0.9578708009312,
+      0.952846253483008,
+      0.874493647245971,
+      -0.746868056938478,
+      0.882509152331738,
+      -0.541093678419456
+    ),
+    tolerance = 0.01
+  )
 
-  test_that("predict model_parameters fa", {
-    pr <- suppressMessages(
-      predict(mp, names = c("Neuroticism", "Conscientiousness", "Extraversion", "Agreeableness", "Opennness"))
-    )
-    out <- head(pr, 5)
-    expect_equal(
-      out$Neuroticism,
-      c(-0.22242, 0.1618, 0.61907, -0.11692, -0.17372),
-      tolerance = 0.01
-    )
-    expect_equal(
-      out$Opennness,
-      c(-1.6092, -0.17222, 0.23341, -1.06152, -0.66086),
-      tolerance = 0.01
-    )
-  })
-}
+  expect_equal(
+    colnames(x),
+    c("Variable", "PC1", "PC2", "Complexity")
+  )
+})
+
+
+# predict ----------------------
+# N.B tests will fail if `GPArotation` package is not installed
+
+require("GPArotation", quietly = TRUE)
+d <- na.omit(psych::bfi[, 1:25])
+model <- psych::fa(d, nfactors = 5)
+mp <- model_parameters(model, sort = TRUE, threshold = "max")
+
+test_that("predict model_parameters fa", {
+  pr <- suppressMessages(
+    predict(mp, names = c("Neuroticism", "Conscientiousness", "Extraversion", "Agreeableness", "Opennness"))
+  )
+  out <- head(pr, 5)
+  expect_equal(
+    out$Neuroticism,
+    c(-0.22242, 0.1618, 0.61907, -0.11692, -0.17372),
+    tolerance = 0.01
+  )
+  expect_equal(
+    out$Opennness,
+    c(-1.6092, -0.17222, 0.23341, -1.06152, -0.66086),
+    tolerance = 0.01
+  )
+})
+
+unloadNamespace("GPArotation")
