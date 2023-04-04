8dec9e9e84acf16d9e0257be8bc015908e48db5a
Daniel <mail@danielluedecke.de>
2023-04-04 16:18:48 +0200

    "Unknown argument(s) specified to be passed to tmbprofile" (#872)
    * "Unknown argument(s) specified to be passed to tmbprofile"
    Fixes #871
    
    * fix, test
    
    * news, version
    
    * add tests

---
+++ tests/testthat/test-glmmTMB-profile_CI.R
@@ -0,0 +1,15 @@
+if (requiet("glmmTMB") && requiet("TMB") && requiet("lme4")) {
+  test_that("glmmTMB profiled and uniroot CI work", {
+    skip_on_cran()
+    data(sleepstudy, package = "lme4")
+    m <- glmmTMB(Reaction ~ Days + (Days | Subject), data = sleepstudy)
+    expect_silent({
+      mp1 <- model_parameters(m, ci_method = "uniroot")
+    })
+    expect_silent({
+      mp2 <- model_parameters(m, ci_method = "profile")
+    })
+    expect_snapshot(print(mp1))
+    expect_snapshot(print(mp2))
+  })
+}
