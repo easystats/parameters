56a4c0a5dc53794b400d1dd39c45967151326e33
Daniel <mail@danielluedecke.de>
2023-04-11 17:48:16 +0200

    Update test-mipo.R

--- tests/testthat/test-mipo.R
+++ tests/testthat/test-mipo.R
@@ -4,8 +4,8 @@ skip_if_not_installed("nnet")
 
 test_that("param", {
   set.seed(1234)
-  d <- mice::ampute(mtcars) ## Ampute mtcars and impute two data sets
-  imp <- mice::mice(d$amp, m = 2)
+  d <- suppressWarnings(mice::ampute(mtcars)) ## Ampute mtcars and impute two data sets
+  imp <- suppressWarnings(mice::mice(d$amp, m = 2, printFlag = FALSE))
   imp.l <- mice::complete(imp, action = "long")
   model <- list() ## Fit and pool models
   for (i in 1:2) model[[i]] <- nnet::multinom(cyl ~ disp + hp, data = imp.l, subset = .imp == i)
