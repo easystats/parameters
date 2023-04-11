74ea34b72cd044780a868aad8c8a60da701d0551
Daniel <mail@danielluedecke.de>
2023-04-11 17:41:10 +0200

    `parameters()` does not extract needed columns for `mipo` objects Fixes #874

---
+++ tests/testthat/test-mipo.R
@@ -0,0 +1,16 @@
+skip_on_cran()
+skip_if_not_installed("mice")
+skip_if_not_installed("nnet")
+
+test_that("param", {
+  set.seed(1234)
+  d <- mice::ampute(mtcars) ## Ampute mtcars and impute two data sets
+  imp <- mice::mice(d$amp, m = 2)
+  imp.l <- mice::complete(imp, action = "long")
+  model <- list() ## Fit and pool models
+  for (i in 1:2) model[[i]] <- nnet::multinom(cyl ~ disp + hp, data = imp.l, subset = .imp == i)
+  pooled <- mice::pool(model)
+
+  mp <- model_parameters(pooled)
+  expect_snapshot(print(mp))
+})
