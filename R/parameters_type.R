8c97d635bf1a7716d9df4e31306939973de45b7e
Daniel <mail@danielluedecke.de>
2023-05-03 23:35:46 +0200

    fix

--- R/parameters_type.R
+++ R/parameters_type.R
@@ -323,11 +323,13 @@ parameters_type <- function(model, ...) {
   contrast_coding <- .safe(model$contrasts)
 
   # clean names from on-the-fly conversion, like "as.ordered(x)"
-  names(contrast_coding) <- gsub(
-    "(as\\.ordered|ordered|as\\.factor|factor)\\((.*)\\)",
-    "\\2",
-    names(contrast_coding)
-  )
+  if (!is.null(contrast_coding) && !is.null(names(contrast_coding))) {
+    names(contrast_coding) <- gsub(
+      "(as\\.ordered|ordered|as\\.factor|factor)\\((.*)\\)",
+      "\\2",
+      names(contrast_coding)
+    )
+  }
 
   # if contrasts are given as matrix, find related contrast name
   if (!is.null(contrast_coding)) {
