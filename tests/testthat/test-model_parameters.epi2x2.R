63382f26d9a460db79f91ab23193c17ada471a17
Daniel <mail@danielluedecke.de>
2024-02-07 17:26:07 +0100

    typo

--- tests/testthat/test-model_parameters.epi2x2.R
+++ tests/testthat/test-model_parameters.epi2x2.R
@@ -5,8 +5,8 @@ test_that("model_parameters.epi2x2", {
   data(mtacrs)
   tab <- xtabs(~ am + vs, data = mtcars)
   out <- model_parameters(epiR::epi.2by2(tab))
-  expect_identicl(out$Parameter, c("RR", "OR", "ARisk", "AFRisk", "PARisk", "PAFRisk"))
-  expect_identicl(
+  expect_identical(out$Parameter, c("RR", "OR", "ARisk", "AFRisk", "PARisk", "PAFRisk"))
+  expect_identical(
     attributes(out)$pretty_names,
     c(
       RR = "Risk Ratio", OR = "Odds Ratio", ARisk = "Attributable Risk",
