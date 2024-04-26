b76df8fd2f692e48b62d407cd2c35129db78bdc4
Daniel <mail@danielluedecke.de>
2024-04-26 08:18:54 +0200

    coxme fails (#965)
    Fixes #964

---
+++ tests/testthat/test-model_parameters.coxme.R
@@ -0,0 +1,18 @@
+skip_on_cran()
+skip_if_not_installed("coxme")
+skip_if_not_installed("survival")
+
+# modelparameters ----------------------------------
+
+test_that("model_parameters.coxme", {
+  data(eortc, package = "coxme")
+  d <- coxme::eortc
+  d$surv <- survival::Surv(d$y, d$uncens)
+  m1 <- coxme::coxme(surv ~ trt + (1 | center), data = d)
+  out <- model_parameters(m1)
+  expect_named(
+    out,
+    c("Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "z", "df_error", "p")
+  )
+  expect_equal(out$Coefficient, 0.7086127, tolerance = 1e-4)
+})
