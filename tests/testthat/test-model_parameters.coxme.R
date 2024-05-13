cdf896fbf11cd7637eca405f77a01fef05c99d68
Daniel <mail@danielluedecke.de>
2024-05-13 08:36:23 +0200

    update test

--- tests/testthat/test-model_parameters.coxme.R
+++ tests/testthat/test-model_parameters.coxme.R
@@ -1,18 +1,22 @@
 skip_on_cran()
 skip_if_not_installed("coxme")
 skip_if_not_installed("survival")
+skip_if_not_installed("withr")
 
 # modelparameters ----------------------------------
 
-test_that("model_parameters.coxme", {
-  data(eortc, package = "coxme")
-  d <- coxme::eortc
-  d$surv <- survival::Surv(d$y, d$uncens)
-  m1 <- coxme::coxme(surv ~ trt + (1 | center), data = d)
-  out <- model_parameters(m1)
-  expect_named(
-    out,
-    c("Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "z", "df_error", "p")
-  )
-  expect_equal(out$Coefficient, 0.7086127, tolerance = 1e-4)
-})
+withr::with_environment(
+  new.env(),
+  test_that("model_parameters.coxme", {
+    data(eortc, package = "coxme")
+    d <- coxme::eortc
+    d$surv <- survival::Surv(d$y, d$uncens)
+    m1 <- coxme::coxme(surv ~ trt + (1 | center), data = d)
+    out <- model_parameters(m1)
+    expect_named(
+      out,
+      c("Parameter", "Coefficient", "SE", "CI", "CI_low", "CI_high", "z", "df_error", "p")
+    )
+    expect_equal(out$Coefficient, 0.7086127, tolerance = 1e-4)
+  })
+)
