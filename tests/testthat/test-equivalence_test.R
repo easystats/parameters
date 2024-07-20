eeaaae6a023dc4987ec47ffce7e0d792c77d624c
Daniel <mail@danielluedecke.de>
2024-07-20 18:05:17 +0200

    skip_on_cran

--- tests/testthat/test-equivalence_test.R
+++ tests/testthat/test-equivalence_test.R
@@ -42,6 +42,7 @@ test_that("equivalence_test, unequal rope-range", {
 })
 
 test_that("equivalence_test, unequal rope-range, plots", {
+  skip_on_cran()
   skip_if_not_installed("vdiffr")
   data(iris)
   m <- lm(Sepal.Length ~ Species, data=iris)
