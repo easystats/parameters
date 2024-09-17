86597e27e9ca8e4898cc1e7c5f507e49966d34bd
Daniel <mail@danielluedecke.de>
2024-09-17 20:23:33 +0200

    test of dev of bayestestR (#1016)
    * test of dev of bayestestR
    
    * fix?
    
    * test
    
    * fix
    
    * fix

--- tests/testthat/test-p_significance.R
+++ tests/testthat/test-p_significance.R
@@ -1,33 +1,38 @@
 skip_on_cran()
-skip_if_not_installed("bayestestR", minimum_version = "0.14.1")
+skip_if_not_installed("bayestestR", minimum_version = "0.14.0.10")
+skip_if_not_installed("distributional")
+skip_if_not_installed("withr")
 
-test_that("p_significance", {
-  data(mtcars)
-  m <- lm(mpg ~ gear + wt + cyl + hp, data = mtcars)
-  set.seed(123)
-  x <- p_significance(m)
-  expect_identical(c(nrow(x), ncol(x)), c(5L, 5L))
-  expect_named(x, c("Parameter", "CI", "CI_low", "CI_high", "ps"))
-  expect_snapshot(print(x))
+withr::with_environment(
+  new.env(),
+  test_that("p_significance", {
+    data(mtcars)
+    m <<- lm(mpg ~ gear + wt + cyl + hp, data = mtcars)
+    set.seed(123)
+    x <- p_significance(m)
+    expect_identical(c(nrow(x), ncol(x)), c(5L, 5L))
+    expect_named(x, c("Parameter", "CI", "CI_low", "CI_high", "ps"))
+    expect_snapshot(print(x))
 
-  mp <- model_parameters(m)
-  set.seed(123)
-  x2 <- p_significance(mp)
-  expect_equal(x$ps, x2$ps, tolerance = 1e-4)
+    mp <- model_parameters(m)
+    set.seed(123)
+    x2 <- p_significance(mp)
+    expect_equal(x$ps, x2$ps, tolerance = 1e-4)
 
-  set.seed(123)
-  x <- p_significance(m, ci = 0.8)
-  expect_equal(x$ps, c(1, 0.3983, 0.9959, 0.6188, 0), tolerance = 1e-3)
+    set.seed(123)
+    x <- p_significance(m, ci = 0.8)
+    expect_equal(x$ps, c(1, 0.3983, 0.9959, 0.6188, 0), tolerance = 1e-3)
 
-  set.seed(123)
-  x <- p_significance(m, threshold = 0.5)
-  expect_equal(x$ps, c(1, 0.4393, 0.9969, 0.6803, 0), tolerance = 1e-4)
+    set.seed(123)
+    x <- p_significance(m, threshold = 0.5)
+    expect_equal(x$ps, c(1, 0.4393, 0.9969, 0.6803, 0), tolerance = 1e-4)
 
-  set.seed(123)
-  # Test p_significance with custom thresholds for specific parameters
-  x <- p_significance(m, threshold = list(cyl = 0.5, wt = 0.7))
-  expect_equal(x$ps, c(1, 0.6002, 0.995, 0.6805, 0), tolerance = 1e-4)
-})
+    set.seed(123)
+    # Test p_significance with custom thresholds for specific parameters
+    x <- p_significance(m, threshold = list(cyl = 0.5, wt = 0.7))
+    expect_equal(x$ps, c(1, 0.5982, 0.9955, 0.6803, 1e-04), tolerance = 1e-4)
+  })
+)
 
 test_that("p_significance, glmmTMB", {
   skip_if_not_installed("glmmTMB")
