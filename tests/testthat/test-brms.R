22cc3456a41ad37658195be818c62b397163cc0b
Daniel <mail@danielluedecke.de>
2024-09-24 11:17:00 +0200

    lintr

--- tests/testthat/test-brms.R
+++ tests/testthat/test-brms.R
@@ -9,13 +9,11 @@ skip_if_not_installed("rstan")
 
 withr::with_options(
   list(parameters_warning_exponentiate = TRUE),
-  {
-    test_that("mp, footer exp", {
-      m <- suppressWarnings(insight::download_model("brms_bernoulli_1"))
-      out <- parameters::model_parameters(m, exponentiate = FALSE)
-      expect_snapshot(print(out))
-      out <- parameters::model_parameters(m, exponentiate = TRUE)
-      expect_snapshot(print(out))
-    })
-  }
+  test_that("mp, footer exp", {
+    m <- suppressWarnings(insight::download_model("brms_bernoulli_1"))
+    out <- parameters::model_parameters(m, exponentiate = FALSE)
+    expect_snapshot(print(out))
+    out <- parameters::model_parameters(m, exponentiate = TRUE)
+    expect_snapshot(print(out))
+  })
 )
