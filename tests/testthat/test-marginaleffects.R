546073754fd064c735ee8ae5427deb20e58eb034
Daniel <mail@danielluedecke.de>
2025-02-09 20:30:07 +0100

    add test

--- tests/testthat/test-marginaleffects.R
+++ tests/testthat/test-marginaleffects.R
@@ -111,3 +111,20 @@ test_that("digits and ci_digits for marginaleffects", {
     model_parameters(digits = 1)
   expect_snapshot(out)
 })
+
+
+test_that("preserve columns with same name as reserved words", {
+  data(mtcars)
+  skip_if(getRversion() < "4.2.0")
+  skip_if_not_installed("modelbased")
+
+  set.seed(1234)
+  x <- rnorm(200)
+  z <- rnorm(200)
+  # quadratic relationship
+  y <- 2 * x + x^2 + 4 * z + rnorm(200)
+  d <- data.frame(x, y, z)
+  model <- lm(y ~ x + z, data = d)
+  pred <- modelbased::estimate_means(model, c("x", "z"))
+  expect_named(pred, c("x", "z", "Mean", "SE", "CI_low", "CI_high", "t", "df"))
+})
