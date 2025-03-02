f969ad8085c714656167ea4eb5c6f8ae78943ab4
Daniel <mail@danielluedecke.de>
2025-03-02 20:31:25 +0100

    fix

--- R/group_level_total.R
+++ R/group_level_total.R
@@ -1,22 +1,20 @@
-#' @keywords internal
-group_level_total <- function(model, ...) {
-  UseMethod("group_level_total")
+.group_level_total <- function(x, ...) {
+  UseMethod(".group_level_total")
 }
 
 
-#' @keywords internal
-group_level_total.glmmTMB <- function(model, ...) {
-  params <- suppressWarnings(insight::compact_list(stats::coef(model)))
+.group_level_total.glmmTMB <- function(x, ...) {
+  params <- suppressWarnings(insight::compact_list(stats::coef(x)))
   params_cond <- params$cond
   params_zi <- params$zi
 
   if (!is.null(params_cond)) {
     group_levels <- insight::compact_list(lapply(
-      model$modelInfo$reTrms$cond$flist,
+      x$modelInfo$reTrms$cond$flist,
       levels
     ))
     params_cond <- .reshape_group_level_coefficients(
-      model,
+      x,
       params_cond,
       group_levels = group_levels
     )
@@ -24,11 +22,11 @@ group_level_total.glmmTMB <- function(model, ...) {
   }
   if (!is.null(params_zi)) {
     group_levels <- insight::compact_list(lapply(
-      model$modelInfo$reTrms$zi$flist,
+      x$modelInfo$reTrms$zi$flist,
       levels
     ))
     params_zi <- .reshape_group_level_coefficients(
-      model,
+      x,
       params_zi,
       group_levels = group_levels,
       component = "zero_inflated_random"
@@ -42,12 +40,12 @@ group_level_total.glmmTMB <- function(model, ...) {
 
 # helper ----------------------------------------------------------------------
 
-.reshape_group_level_coefficients <- function(model,
+.reshape_group_level_coefficients <- function(x,
                                               params,
                                               group_levels,
                                               component = "random") {
-  group_factors <- insight::find_random(model)
-  random_slopes <- insight::find_random_slopes(model)
+  group_factors <- insight::find_random(x)
+  random_slopes <- insight::find_random_slopes(x)
 
   # find all columns for which we can add fixed and random effects
   cols <- c(random_slopes[[component]], "(Intercept)")
