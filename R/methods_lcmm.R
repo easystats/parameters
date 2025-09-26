00e64c3d62ce4f476f5a692603b31274de336d3e
Daniel <mail@danielluedecke.de>
2025-09-26 09:06:05 +0200

    rfeactor

--- R/methods_lcmm.R
+++ R/methods_lcmm.R
@@ -66,6 +66,28 @@ model_parameters.externX <- model_parameters.externVar
 
 # p-values ----------------------------------------------------------------
 
+# Helper to format output for lcmm methods
+.format_lcmm_output <- function(model, value, colname, component, ...) {
+  component <- insight::validate_argument(
+    component,
+    c("all", "conditional", "membership", "longitudinal", "beta", "splines", "linear")
+  )
+
+  out <- insight::get_parameters(model, component = "all", ...)
+  out[[colname]] <- as.vector(value)
+
+  if (component != "all") {
+    out <- out[out$Component == component, , drop = FALSE]
+  }
+
+  # clean up
+  out$Estimate <- NULL
+  out <- out[intersect(c("Parameter", colname, "Component", "Group"), colnames(out))]
+
+  insight::text_remove_backticks(out, verbose = FALSE)
+}
+
+
 #' @export
 p_value.lcmm <- function(model, component = "all", ...) {
   id <- seq_along(model$best)
@@ -75,24 +97,7 @@ p_value.lcmm <- function(model, component = "all", ...) {
   p <- 2 * stats::pt(abs(statistic), df = Inf, lower.tail = FALSE)
 
   p <- p[!startsWith(names(model$best), "cholesky ") & !startsWith(names(model$best), "varcov ")]
-
-  component <- insight::validate_argument(
-    component,
-    c("all", "conditional", "membership", "longitudinal", "beta", "splines", "linear")
-  )
-
-  out <- insight::get_parameters(model, component = "all", ...)
-  out$p <- as.vector(p)
-
-  if (component != "all") {
-    out <- out[out$Component == component, , drop = FALSE]
-  }
-
-  # clean up
-  out$Estimate <- NULL
-  out <- out[intersect(c("Parameter", "p", "Component", "Group"), colnames(out))]
-
-  insight::text_remove_backticks(out, verbose = FALSE)
+  .format_lcmm_output(model, p, "p", component, ...)
 }
 
 #' @export
@@ -111,24 +116,7 @@ standard_error.lcmm <- function(model, component = "all", ...) {
   se <- sqrt(model$V[indice])
 
   se <- se[!startsWith(names(model$best), "cholesky ") & !startsWith(names(model$best), "varcov ")]
-
-  component <- insight::validate_argument(
-    component,
-    c("all", "conditional", "membership", "longitudinal", "beta", "splines", "linear")
-  )
-
-  out <- insight::get_parameters(model, component = "all", ...)
-  out$SE <- as.vector(se)
-
-  if (component != "all") {
-    out <- out[out$Component == component, , drop = FALSE]
-  }
-
-  # clean up
-  out$Estimate <- NULL
-  out <- out[intersect(c("Parameter", "SE", "Component", "Group"), colnames(out))]
-
-  insight::text_remove_backticks(out, verbose = FALSE)
+  .format_lcmm_output(model, se, "SE", component, ...)
 }
 
 #' @export
