a67c5b1f52235c8996f72551eff857c39653388e
Daniel <mail@danielluedecke.de>
2025-10-15 15:27:16 +0200

    fix

--- R/standard_error_kenward.R
+++ R/standard_error_kenward.R
@@ -1,12 +1,12 @@
 #' @rdname p_value_kenward
 #' @export
-se_kenward <- function(model) {
+se_kenward <- function(model, component = "conditional") {
   if (!.check_REML_fit(model)) {
     model <- stats::update(model, . ~ ., REML = TRUE)
   }
 
   vcov_adjusted <- insight::get_varcov(model, vcov = "kenward-roger")
-  params <- insight::get_parameters(model, effects = "fixed", component)
+  params <- insight::get_parameters(model, effects = "fixed", component = component)
 
   .data_frame(Parameter = params$Parameter, SE = abs(sqrt(diag(vcov_adjusted))))
 }
