092f6e09ab0a11888b50e9e74b820d97de75bf3b
Daniel <mail@danielluedecke.de>
2026-02-05 10:58:32 +0100

    support `lavaan.mi` models (#1188)
    * support `lavaan.mi` models
    Fixes #1187
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * minor

---
+++ tests/testthat/test-lavaan.mi.R
@@ -0,0 +1,24 @@
+skip_on_cran()
+skip_if_not_installed("lavaan.mi")
+skip_if_not_installed("mice")
+
+test_that("model_parameters.lavaan.mi", {
+  # required for lavaan.mi to work
+  library(lavaan.mi)
+
+  set.seed(20260205)
+  mtcars_amp <- suppressWarnings(mice::ampute(mtcars)$amp)
+  imp <- mice::mice(mtcars_amp, printFlag = FALSE)
+
+  mod <- "
+  mpg ~ hp + vs
+  hp ~ am
+  "
+
+  fit.mi <- lavaan.mi::sem.mi(mod, data = imp)
+
+  mp <- model_parameters(fit.mi)
+  expect_s3_class(mp, "parameters_model")
+  expect_equal(nrow(mp), 5)
+  expect_equal(mp$Coefficient[1], -0.05, tolerance = 1e-2)
+})
